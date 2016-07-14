25c41ae87c1252e09e2fa19a5e6daf756b4ac486
fevega1 <fevega@gmail.com>
2016-07-14 09:31:43 -0500

    AWS CodePipeline Test 3 using Express

--- client/components/contact/contact.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from "ng-semantic";
-
-@Component({
-    directives: [SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES],
-    selector: "home",
-    templateUrl: `client/components/contact/contact.component.html`
-})
-export class ContactComponent {}
