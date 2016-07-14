6667f8026b2b0e8dbe902d2aab61b593376a3162
fevega1 <fevega@gmail.com>
2016-07-14 12:44:57 -0500

    Testing deployment

---
+++ public/app/main.ts
@@ -0,0 +1,8 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+import { enableProdMode } from '@angular/core';
+
+// enable production mode and thus disable debugging information
+// enableProdMode();
+
+bootstrap(AppComponent);
