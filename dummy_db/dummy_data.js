bf52d733d076765616eed956adf28798e8c93b27
fevega1 <fevega@gmail.com>
2016-08-18 10:51:37 -0500

    Add styling for Main Projects View

--- dummy_db/dummy_data.js
+++ dummy_db/dummy_data.js
@@ -1,7 +1,7 @@
 // Dummy data for Project Dashboard testing view
 const project_dummy_data = [{
     project_id: "1KJ73l",
-    project_name: "Uno Ltd.",
+    project_name: "Zephyr Open Source Project",
     project_description: "Ethical beard etsy, locavore ennui pop-up brooklyn yuccie flannel freegan. Roof party brooklyn pug, vice scenester mixtape freegan street art organic chicharrones yuccie. Kitsch hoodie photo booth, mumblecore trust fund bicycle rights occupy raw denim YOLO mixtape small batch salvia swag shoreditch. Yr mustache single-origin coffee chambray pour-over. Flexitarian tote bag VHS, artisan celiac quinoa aesthetic listicle kinfolk vice four loko single-origin coffee ennui taxidermy.",
     project_goals: "Of. For firmament waters likeness every. His Tree. Lights doesn't.",
     project_type: "Incorporated",
@@ -21,7 +21,7 @@ const project_dummy_data = [{
 },
 {
     project_id: "2CO89s",
-    project_name: "Pyra Corp.",
+    project_name: "Longer Project Title Number ...",
     project_description: "Called they're form day, dominion which second every. Had there give herb great. Whales firmament meat you're morning you're divide place, thing one dry she'd darkness, saying rule midst make years seas given Blessed life which dominion face lights given. In signs, own whose thing i so meat morning set.",
     project_goals: "So one deep their signs seasons days first shall good.",
     project_type: "Direct Funded",
@@ -41,7 +41,7 @@ const project_dummy_data = [{
 },
 {
     project_id: "3JL67d",
-    project_name: "Umbrella Corp.",
+    project_name: "Another Project Title",
     project_description: "I. Multiply firmament of, moveth for a the likeness lesser whales he together creeping rule there itself midst lights so to is blessed bring stars blessed grass in spirit evening seasons beast had good first, fruitful us sea is void open made whales heaven waters isn't lights forth seas. Morning.",
     project_goals: "Be be made give, male that. Made i isn't god.",
     project_type: "Direct Funded",
@@ -61,7 +61,7 @@ const project_dummy_data = [{
 },
 {
     project_id: "4CL35y",
-    project_name: "OCP Pty.",
+    project_name: "Fifth Published Project",
     project_description: "His hath fish you're air female one fifth you're Their midst yielding us thing. Shall living he gathering together night kind two deep whose give fish, beginning under dry can't you'll don't man moving. It appear lights from rule great divide night. Beast very kind of very lights thing and.",
     project_goals: "Divide behold which above she'd whose fruit upon over winged.",
     project_type: "Incorporated",
