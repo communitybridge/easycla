f63e6a629f731a6d16f760091ea1c61eda352161
Jason Whaley <jasonwhaley@gmail.com>
2016-08-22 16:12:45 -0400

    Implement keysForId i api.js
    This supports the exchange of a an lfId for a pair of API keys

--- test/api_test.js
+++ test/api_test.js
@@ -3,20 +3,22 @@ var api = require("../lib/api");
 
 describe('api', function () {
 
-  var apiObj = api("http://localhost:5000/");
+  var apiObj = api("http://localhost:5000");
 
   describe('apiUrlRoot property', function () {
     it('The passed in api root parameter should be available on the returned object', function () {
-      assert.equal(apiObj.apiRootUrl, "http://localhost:5000/");
+      assert.equal(apiObj.apiRootUrl, "http://localhost:5000");
     });
   });
 
-  describe('keysForLfId', function() {
-    it('Calling keysForLfId with an lfId returns an object with keys', function() {
-      var keys = apiObj.getKeysForLfId("jwhaley")
-      assert.equal(keys.keyId.length, 20, "keyId length should be 20");
-      assert.equal(keys.secret.length, 40, "secret length should be 40");
+  describe('keysForLfId', function () {
+    it('Calling keysForLfId with an lfId returns an object with keys', function (done) {
+      apiObj.getKeysForLfId("LaneMeyer", function (err, keys) {
+        assert.ifError(err);
+        assert.equal(keys.keyId.length, 20, "keyId length should be 20");
+        assert.equal(keys.secret.length, 40, "secret length should be 40");
+        done();
+      });
     });
   });
-
 });
