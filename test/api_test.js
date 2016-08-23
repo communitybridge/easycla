1191b337f1e8a1777c54413393f0f117d5ff9cdd
Jason Whaley <jasonwhaley@gmail.com>
2016-08-23 12:39:54 -0400

    Add top level describes to api_test.js

--- test/api_test.js
+++ test/api_test.js
@@ -5,20 +5,25 @@ describe('api', function () {
 
   var apiObj = api("http://localhost:5000");
 
-  describe('apiUrlRoot property', function () {
-    it('The passed in api root parameter should be available on the returned object', function () {
-      assert.equal(apiObj.apiRootUrl, "http://localhost:5000");
-    });
-  });
-
-  describe('keysForLfId', function () {
-    it('Calling keysForLfId with an lfId returns an object with keys', function (done) {
-      apiObj.getKeysForLfId("LaneMeyer", function (err, keys) {
-        assert.ifError(err);
-        assert.equal(keys.keyId.length, 20, "keyId length should be 20");
-        assert.equal(keys.secret.length, 40, "secret length should be 40");
-        done();
+  describe('Properties', function () {
+    describe('apiUrlRoot', function () {
+      it('The passed in api root parameter should be available on the returned object', function () {
+        assert.equal(apiObj.apiRootUrl, "http://localhost:5000");
       });
     });
   });
+
+  describe('Trusted Auth Endpoints', function () {
+    describe('keysForLfId', function () {
+      it('Calling keysForLfId with an lfId returns an object with keys', function (done) {
+        apiObj.getKeysForLfId("LaneMeyer", function (err, keys) {
+          assert.ifError(err);
+          assert.equal(keys.keyId.length, 20, "keyId length should be 20");
+          assert.equal(keys.secret.length, 40, "secret length should be 40");
+          done();
+        });
+      });
+    });
+  });
+
 });
