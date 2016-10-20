416b4056c13ee3bc5978dd4cacb786a150b5568d
fevega1 <fevega@gmail.com>
2016-10-20 15:59:01 -0500

    Add method to remove a participant from an email alias

--- test/api_test.js
+++ test/api_test.js
@@ -378,7 +378,7 @@ describe('api', function () {
         });
       });
 
-      it('POST /projects/{projectId}/emailaliases/{aliasid}/participants', function (done) {
+      it('POST /projects/{projectId}/emailaliases/{aliasId}/participants', function (done) {
         var newParticipant = {
           "address": "newFoo@bar.com"
         };
@@ -398,6 +398,35 @@ describe('api', function () {
             projManagerClient.addParticipantToEmailAlias(projectId, aliasId.aliasId, newParticipant, function (err, created, response) {
               assert(created);
               done();
+            });
+          });
+        });
+      });
+
+      it('DELETE /projects/{projectId}/emailaliases/{aliasId}/participants/{participantAddress}', function (done) {
+        var participantTBR = "participantToBeRemoved@bar.com";
+        var newParticipant = {
+          "address": participantTBR
+        };
+        var sampleAlias = {
+          "address": "anotherEmailAlias@myDomain.org",
+          "participants": [
+            {
+              "address": "someFoo@bar.com"
+            }
+          ]
+        };
+        projManagerClient.getMyProjects(function (err, projects) {
+          assert.ifError(err);
+          var projectId = projects[0].id;
+          projManagerClient.createEmailAliases(projectId, sampleAlias, function (err, created, aliasId) {
+            assert.ifError(err);
+            projManagerClient.addParticipantToEmailAlias(projectId, aliasId.aliasId, newParticipant, function (err, created, response) {
+              assert.ifError(err);
+              projManagerClient.removeParticipantFromEmailAlias(projectId, aliasId.aliasId, participantTBR, function (err, removed) {
+                assert(removed);
+                done();
+              });
             });
           });
         });
