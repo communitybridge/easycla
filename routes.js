85f44ae1a16b53751c2f3f9eab9a1261cb23a521
fevega1 <fevega@gmail.com>
2016-10-20 15:18:32 -0500

    Add participants to an email alias

--- routes.js
+++ routes.js
@@ -158,9 +158,9 @@ router.get('/aliases/:id', require('connect-ensure-login').ensureLoggedIn('/logi
     projManagerClient.getProject(id, function (err, project) {
       // TODO: Create 404 page for when project doesn't exist
       if (err) return res.redirect('/');
-      project.domain = project.url.replace('http://www.','').replace('https://www.','').replace('/','');
+      project.domain = "";
+      if(project.url) project.domain = project.url.replace('http://www.','').replace('https://www.','').replace('/','');
       projManagerClient.getEmailAliases(id, function (err, emailAliases) {
-        
         var isAlias = {
           "isContactAlias": false,
           "isEventsAlias": false,
@@ -168,11 +168,11 @@ router.get('/aliases/:id', require('connect-ensure-login').ensureLoggedIn('/logi
           "isLegalAlias": false,
           "isMembershipAlias": false,
         };
-
         if( emailAliases.length >= 1 ){
           for (var i = 0; i < emailAliases.length; i++) {
             var email_alias = emailAliases[i].address;
-            var alias_name = email_alias.match(/^([^@]*)@/)[1];
+            var alias_name;
+            if(email_alias) alias_name = email_alias.replace(/@.*$/,"");
             if(alias_name == "contact") isAlias.isContactAlias = true;
             if(alias_name == "events") isAlias.isEventsAlias = true;
             if(alias_name == "pr") isAlias.isPrAlias = true;
@@ -201,6 +201,21 @@ router.post('/aliases/:id', require('connect-ensure-login').ensureLoggedIn('/log
       ]
     };
     projManagerClient.createEmailAliases(projectId, newAlias, function (err, created, aliasId) {
+      return res.redirect('/aliases/' + projectId);
+    });
+  }
+});
+
+router.post('/addParticipantToEmailAlias/:projectId/', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
+  if(req.session.user.isAdmin || req.session.user.isProjectManager){
+    var projManagerClient = cinco.client(req.session.user.cinco_keys);
+    var projectId = req.params.projectId;
+    var aliasId = req.body.alias_id;
+    var participant_email = req.body.participant_email;
+    var newParticipant = {
+      "address": participant_email
+    };
+    projManagerClient.addParticipantToEmailAlias(projectId, aliasId, newParticipant, function (err, created, response) {
       return res.redirect('/aliases/' + projectId);
     });
   }
@@ -522,7 +537,10 @@ router.post('/create_project', require('connect-ensure-login').ensureLoggedIn('/
     var projManagerClient = cinco.client(req.session.user.cinco_keys);
     var now = new Date().toISOString();
     var url = req.body.url;
-    if (!/^(?:f|ht)tps?\:\/\//.test(url)) url = "http://" + url;
+    if(url){
+      if (!/^(?:f|ht)tps?\:\/\//.test(url)) url = "http://" + url;
+    }
+    console.log(url);
     var logoFileName = "";
     var agreementFileName = "";
     if(req.files){
@@ -567,7 +585,11 @@ router.get('/edit_project/:id', require('connect-ensure-login').ensureLoggedIn('
     projManagerClient.getProject(id, function (err, project) {
       // TODO: Create 404 page for when project doesn't exist
       if (err) return res.redirect('/');
-      return res.render('edit_project', {project: project});
+      project.domain = "";
+      if(project.url) project.domain = project.url.replace('http://www.','').replace('https://www.','').replace('/','');
+      projManagerClient.getEmailAliases(id, function (err, emailAliases) {
+        return res.render('edit_project', {project: project, emailAliases: emailAliases });
+      });
     });
   }
 });
