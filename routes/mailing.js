909aed2b86c0eee36b316fc4ea7237da66676478
fevega1 <fevega@gmail.com>
2016-11-29 17:41:00 -0500

    Get and create mailing lists from frontend

--- routes/mailing.js
+++ routes/mailing.js
@@ -22,4 +22,45 @@ router.get('/mailing', require('connect-ensure-login').ensureLoggedIn('/login'),
   });
 });
 
+router.get('/mailing/:projectId', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
+  if(req.session.user.isAdmin || req.session.user.isProjectManager){
+    var projectId = req.params.projectId;
+    var projManagerClient = cinco.client(req.session.user.cinco_keys);
+    projManagerClient.getProject(projectId, function (err, project) {
+      projManagerClient.getMailingLists(projectId, function (err, mailingList) {
+        console.log(mailingList);
+        res.render('mailing', { mailingList: mailingList, project:project });
+      });
+    });
+  }
+});
+
+router.post('/mailing/:projectId', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
+  if(req.session.user.isAdmin || req.session.user.isProjectManager){
+    var projManagerClient = cinco.client(req.session.user.cinco_keys);
+    var projectId = req.params.projectId;
+
+    var mailingName = req.body.mailing_name;
+    var mailingEmailAdmin = req.body.mailing_email_admin;
+    var mailingPassword = req.body.mailing_password;
+    // var mailingSubsribePolicy = req.body.mailing_subsribe_policy;
+    // var mailingArchivePolicy = req.body.mailing_archive_policy;
+
+    var newMailingList = {
+      "name": mailingName,
+      "admin": mailingEmailAdmin,
+      "password": mailingPassword
+      // ,
+      // "subsribePolicy": mailingSubsribePolicy,
+      // "archivePolicy": mailingArchivePolicy
+    };
+    projManagerClient.createMailingList(projectId, newMailingList, function (err, created, mailingListId) {
+      console.log("mailing list created: " + created);
+      console.log("mailingListId: " + mailingListId);
+      return res.redirect('/mailing/' + projectId);
+    });
+
+  }
+});
+
 module.exports = router;
