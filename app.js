5fee2a0bf98765155634bc09269facee2e111f85
fevega1 <fevega@gmail.com>
2016-12-09 18:33:11 -0500

    Comment out Gulp functions

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var passport = require('passport');
 var CasStrategy = require('passport-cas').Strategy;
 var path = require('path');
 var bodyParser = require('body-parser');
-var gulp = require('gulp');
-require('./gulpfile');
+// var gulp = require('gulp');
+// require('./gulpfile');
 
 // if (gulp.tasks.styles) {
 //     console.log('Concatenating and minifying CSS files from /public/assets/src/css to /public/assets/dist');
