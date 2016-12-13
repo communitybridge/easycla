ef6b769150fafb5057fbccbf4747f4a451615b74
fevega1 <fevega@gmail.com>
2016-12-13 16:24:06 -0500

    Add Node.js New Relic Agent

---
+++ newrelic.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+
+var newRelicLicenseKey = process.env['NEW_RELIC_API_KEY'];
+
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['CINCO Integration Platform'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: newRelicLicenseKey,
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
