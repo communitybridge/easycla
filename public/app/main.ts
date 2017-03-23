635e5f9f7e2397496afd294b2bff0696e17e7ef4
fvega <fvega@linuxfoundation.org>
2017-03-23 09:01:16 -0500

    Remove unused files

--- public/app/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-import { enableProdMode } from '@angular/core';
-
-// enable production mode and thus disable debugging information
-// enableProdMode();
-
-bootstrap(AppComponent);
