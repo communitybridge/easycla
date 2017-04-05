9cf3683dbe3718c48f0416d91c38437a92596a5a
fvega <fvega@linuxfoundation.org>
2017-04-04 18:16:28 -0700

    Add structure ‘Member View’ files

---
+++ app/src/pages/member/member.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'member',
+  templateUrl: 'member.html'
+})
+export class MemberPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
