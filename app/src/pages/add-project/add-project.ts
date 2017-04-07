38cb326ae6f23049b0e1c6cbf5f21800f60705ab
fvega <fvega@linuxfoundation.org>
2017-04-07 00:03:04 -0700

    Add ability to Create a Project from new UI

--- app/src/pages/add-project/add-project.ts
+++ app/src/pages/add-project/add-project.ts
@@ -2,14 +2,32 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { CincoService } from '../../app/services/cinco.service'
+import { ProjectPage } from '../project/project';
+
 @Component({
   selector: 'add-project',
   templateUrl: 'add-project.html'
 })
 export class AddProjectPage {
+  newProject;
+  project_name: String;
+  project_type: String;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private cincoService: CincoService) {
+    this.newProject = {};
+  }
 
+  submitNewProject() {
+    this.newProject = {
+      project_name: this.project_name,
+      project_type: this.project_type
+    };
+    this.cincoService.postProject(this.newProject).subscribe(response => {
+      this.navCtrl.push(ProjectPage, {
+        projectId: response
+      });
+    });
   }
 
 }
