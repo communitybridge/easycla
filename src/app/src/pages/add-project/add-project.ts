42c4a004904f90d267b4d38d2ca9a076acb90f00
fvega <fvega@linuxfoundation.org>
2017-04-27 12:19:26 -0500

    Cleaning code and remove errors from browser console

--- src/app/src/pages/add-project/add-project.ts
+++ src/app/src/pages/add-project/add-project.ts
@@ -5,7 +5,6 @@ import { NavController, IonicPage } from 'ionic-angular';
 import { CincoService } from '../../app/services/cinco.service'
 import { ProjectPage } from '../project/project';
 
-
 @IonicPage({
   segment: 'project-add'
 })
@@ -28,7 +27,6 @@ export class AddProjectPage {
       project_type: this.project_type
     };
     this.cincoService.postProject(this.newProject).subscribe(response => {
-      console.log(response)
       this.navCtrl.push(ProjectPage, {
         projectId: response
       });
