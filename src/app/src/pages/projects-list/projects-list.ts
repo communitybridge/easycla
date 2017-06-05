79d0c599fe3f638416f728d19ef5d33fdb34e530
fvega <fvega@linuxfoundation.org>
2017-06-05 14:57:46 -0500

    Remove Add Project functionality.

--- src/app/src/pages/projects-list/projects-list.ts
+++ src/app/src/pages/projects-list/projects-list.ts
@@ -13,7 +13,7 @@ import { Chart } from 'chart.js';
 export class ProjectsListPage {
   loading: boolean;
   allProjects: any;
-  pushAddProjectPage;
+
   numberOfContracts: {
     new: number,
     renewal: number,
@@ -50,7 +50,6 @@ export class ProjectsListPage {
   invoicesChart: any;
 
   constructor(public navCtrl: NavController, private cincoService: CincoService) {
-    this.pushAddProjectPage = 'AddProjectPage';
     this.getDefaults();
     this.loading = true;
   }
