05985f6b4e1d9eb9b2c2804f7d442284d3316f08
Philippe Labat <plabat@linuxfoundation.org>
2017-06-05 11:12:00 -0400

    First Cleanup

--- modules/pritunl-link/terraform.tf
+++ modules/pritunl-link/terraform.tf
@@ -11,8 +11,16 @@ variable "project" {
   description = "Project Name"
 }
 
+variable "key_pair" {
+  description = "The key-pair to use for the EC2 Instance"
+}
+
 variable "pritunl_link" {
   description = "The URL from Pritunl for this Link"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR of the VPC in which you are deploying to"
 }
 
 data "template_file" "installation-bash" {
@@ -20,6 +28,7 @@ data "template_file" "installation-bash" {
 
   vars {
     PRITUNL_LINK = "${var.pritunl_link}"
+    DNS_SERVER   = "${replace(var.vpc_cidr, ".0/24", ".2")}"
   }
 }
 
@@ -29,14 +38,14 @@ resource "aws_instance" "pritunl" {
   source_dest_check      = false
   instance_type          = "t2.small"
   subnet_id              = "${element(var.external_subnets, 0)}"
-  key_name               = "production-${var.project}"
+  key_name               = "${var.key_pair}"
   iam_instance_profile   = "PritunlLink"
   vpc_security_group_ids = ["${var.vpn_sg}"]
   user_data              = "${data.template_file.installation-bash.rendered}"
   monitoring             = true
 
   tags {
-    Name        = "Pritunl - IPsec Link"
+    Name        = "${var.project} - IPsec Link"
     Team        = "Engineering"
     Environment = "Production"
   }
--- production-tools/base/pritunl/terraform.tf
+++ production-tools/base/pritunl/terraform.tf
@@ -7,8 +7,6 @@ variable "vpn_sg" {
   description = "Security Group for the VPN Server"
 }
 
-variable "region_identifier" {}
-
 data "aws_ami" "pritunl" {
   provider    = "aws.local"
   most_recent = true
@@ -25,17 +23,18 @@ data "aws_ami" "pritunl" {
 }
 
 resource "aws_instance" "pritunl" {
+  count                  = "${length(var.external_subnets)}"
   provider               = "aws.local"
   ami                    = "${data.aws_ami.pritunl.id}"
   source_dest_check      = false
   instance_type          = "t2.small"
-  subnet_id              = "${element(var.external_subnets, 0)}"
+  subnet_id              = "${element(var.external_subnets, count.index)}"
   key_name               = "production-shared-tools"
   vpc_security_group_ids = ["${var.vpn_sg}"]
   monitoring             = true
 
   tags {
-    Name        = "Pritunl - VPN Instance"
+    Name        = "Pritunl - Node #${count.index}"
     Team        = "Engineering"
     Environment = "Production"
   }
@@ -43,14 +42,58 @@ resource "aws_instance" "pritunl" {
 
 resource "aws_eip" "pritunl" {
   provider = "aws.local"
-  instance = "${aws_instance.pritunl.id}"
+  instance = "${element(aws_instance.pritunl.*.id, count.index)}"
   vpc      = true
 }
 
-resource "aws_route53_record" "pritunl" {
+# Create a new load balancer
+resource "aws_elb" "pritunl-cluster" {
+  name               = "pritunl-cluster"
+  subnets            = ["${var.external_subnets}"]
+  security_groups    = ["${var.vpn_sg}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port      = 9700
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "arn:aws:acm:us-west-2:433610389961:certificate/bb946be4-a4f4-4f91-a786-60eddbd055b6"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:9700/ping"
+    interval            = 30
+  }
+
+  instances                   = ["${aws_instance.pritunl.*.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "pritunl-cluster"
+  }
+}
+
+resource "aws_route53_record" "pritunl-cluster" {
   zone_id = "Z2MDT77FL23F9B"
-  name    = "${var.region_identifier}.vpn"
+  name    = "vpn"
   type    = "A"
-  ttl     = "300"
-  records = ["${aws_eip.pritunl.public_ip}"]
+
+  alias {
+    name                   = "${aws_elb.pritunl-cluster.dns_name}"
+    zone_id                = "${aws_elb.pritunl-cluster.zone_id}"
+    evaluate_target_health = true
+  }
 }
