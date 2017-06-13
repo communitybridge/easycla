e46d0a668f7ee3b5cae67849c5cc611730564845
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-06-13 17:42:48 -0400

    add calendar parsing, user updating, calendar rendering

--- src/routes/users.js
+++ src/routes/users.js
@@ -25,6 +25,16 @@ var cpUpload = upload.fields([{ name: 'logo', maxCount: 1 }, { name: 'agreement'
   Resources to manage internal LF users and roles
  */
 
+ router.get('/user', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
+   if(req.session.user.isAdmin || req.session.user.isProjectManager){
+     var userId = req.session.user.user;
+     var projManagerClient = cinco.client(req.session.user.cinco_keys);
+     projManagerClient.getUser(userId, function (err, user) {
+       res.send(user);
+     });
+   }
+ });
+
 router.get('/users', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
   if(req.session.user.isAdmin || req.session.user.isProjectManager){
     var projManagerClient = cinco.client(req.session.user.cinco_keys);
@@ -39,6 +49,21 @@ router.get('/users/:userId', require('connect-ensure-login').ensureLoggedIn('/lo
     var userId = req.params.userId;
     var projManagerClient = cinco.client(req.session.user.cinco_keys);
     projManagerClient.getUser(userId, function (err, user) {
+      res.send(user);
+    });
+  }
+});
+
+router.put('/users/:userId', require('connect-ensure-login').ensureLoggedIn('/login'), cpUpload, function(req, res){
+  if(req.session.user.isAdmin || (req.session.user.isProjectManager && req.session.user.user == req.params.userId)){
+    var userId = req.params.userId;
+    var user = {
+      userId: req.body.userId,
+      email: req.body.email,
+      calendar: req.body.calendar,
+    };
+    var projManagerClient = cinco.client(req.session.user.cinco_keys);
+    projManagerClient.updateUser(userId, user, function (err, user) {
       res.send(user);
     });
   }
