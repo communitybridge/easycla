3e4e1e601ddeb0cf1d2f9011a5d719a7f8c2d50b
fvega <fvega@linuxfoundation.org>
2017-06-16 19:09:19 -0500

    Convert POST /edit_project/{projectId} to PUT /projects/{projectId}. Refactor /projects routes.

--- src/app/src/app/services/cinco.service.ts
+++ src/app/src/app/services/cinco.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 import 'rxjs/Rx';
 
 @Injectable()
-export class CincoService{
+export class CincoService {
   http: any;
   baseUrl: String;
 
@@ -13,94 +13,88 @@ export class CincoService{
     this.baseUrl = '';
   }
 
-  /*
-    Projects:
-    Resources to expose and manipulate details of projects
-   */
-   getProjectStatuses() {
-     return this.http.get(this.baseUrl + '/project/status')
-             .map(res => res.json());
-   }
+  /**
+  * Projects
+  * Resources to expose and manipulate details of projects
+  **/
 
-   getProjectCategories() {
-     return this.http.get(this.baseUrl + '/project/categories')
-             .map(res => res.json());
-   }
+  getProjectStatuses() {
+    return this.http.get(this.baseUrl + '/project/status')
+      .map(res => res.json());
+  }
 
-   getProjectSectors() {
-     return this.http.get(this.baseUrl + '/project/sectors')
-             .map(res => res.json());
-   }
+  getProjectCategories() {
+    return this.http.get(this.baseUrl + '/project/categories')
+      .map(res => res.json());
+  }
 
-   getAllProjects() {
-     return this.http.get(this.baseUrl + '/projects')
-             .map(res => res.json());
-   }
+  getProjectSectors() {
+    return this.http.get(this.baseUrl + '/project/sectors')
+      .map(res => res.json());
+  }
 
-   getProject(projectId, getMembers) {
-     if (getMembers) { projectId = projectId + '?members=true' ; }
-     return this.http.get(this.baseUrl + '/projects/' + projectId)
-             .map(res => res.json());
-   }
+  getAllProjects() {
+    return this.http.get(this.baseUrl + '/projects')
+      .map(res => res.json());
+  }
 
-   postProject(newProject) {
-     var headers = new Headers();
-     headers.append("Accept", 'application/json');
-     headers.append('Content-Type', 'application/json' );
-     let options = new RequestOptions({ headers: headers });
-     return this.http.post('/projects', newProject, options)
-                 .map((res) => res.json());
-   }
+  //  Disabled for PMC v1.0
+  //  createProject(newProject) {
+  //    return this.http.post('/projects', newProject)
+  //            .map((res) => res.json());
+  //  }
 
-   editProject(projectId, editProject) {
-     var headers = new Headers();
-     headers.append("Accept", 'application/json');
-     headers.append('Content-Type', 'application/json' );
-     let options = new RequestOptions({ headers: headers });
-     return this.http.post('/edit_project/' + projectId, editProject, options)
-                 .map((res) => res.json());
-   }
+  getProject(projectId, getMembers) {
+    if (getMembers) { projectId = projectId + '?members=true'; }
+    return this.http.get(this.baseUrl + '/projects/' + projectId)
+      .map(res => res.json());
+  }
 
-   getProjectConfig(projectId) {
-     return this.http.get(this.baseUrl + '/projects/' + projectId + '/config')
-             .map(res => res.json());
-   }
+  editProject(projectId, editProject) {
+    return this.http.put('/projects/' + projectId, editProject)
+      .map((res) => res.json());
+  }
 
-   updateProjectManagers(projectId, updatedManagers) {
-     return this.http.put('/projects/' + projectId + '/managers', updatedManagers)
-             .map((res) => res.json());
-   }
+  getProjectConfig(projectId) {
+    return this.http.get(this.baseUrl + '/projects/' + projectId + '/config')
+      .map(res => res.json());
+  }
 
-  /*
-    Projects - Members:
-    Resources for getting details about project members
-   */
+  updateProjectManagers(projectId, updatedManagers) {
+    return this.http.put('/projects/' + projectId + '/managers', updatedManagers)
+      .map((res) => res.json());
+  }
+
+  /**
+  * Projects - Members
+  * Resources for getting details about project members
+  **/
 
   getProjectMembers(projectId) {
     return this.http.get(this.baseUrl + '/projects/' + projectId + '/members')
-            .map(res => res.json());
+      .map(res => res.json());
   }
 
   getMember(projectId, memberId) {
     var response = this.http.get(this.baseUrl + '/projects/' + projectId + '/members/' + memberId)
-            .map(res => res.json());
+      .map(res => res.json());
     return response;
   }
 
-  /*
-    Projects - Members - Contacts:
-    Resources for getting and manipulating contacts of project members
-   */
+  /**
+  * Projects - Members - Contacts:
+  * Resources for getting and manipulating contacts of project members
+  **/
 
   getMemberContactRoles() {
     var response = this.http.get(this.baseUrl + '/project/members/contacts/types')
-            .map(res => res.json());
+      .map(res => res.json());
     return response;
   }
 
   getMemberContacts(projectId, memberId) {
     var response = this.http.get(this.baseUrl + '/projects/' + projectId + '/members/' + memberId + '/contacts')
-            .map(res => res.json());
+      .map(res => res.json());
     return response;
   }
 
@@ -122,14 +116,14 @@ export class CincoService{
     body.append('contactHeadshotRef', contact.contact.headshotRef);
     body.append('contactType', contact.contact.type);
     return this.http.post('/projects/' + projectId + '/members/' + memberId + '/contacts/' + contactId, body, headers)
-                .map((res) => res.json());
+      .map((res) => res.json());
   }
 
   removeMemberContact(projectId, memberId, contactId, roleId) {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let body = new FormData();
     return this.http.delete('/projects/' + projectId + '/members/' + memberId + '/contacts/' + contactId + '/roles/' + roleId, body, headers)
-                .map((res) => res.json());
+      .map((res) => res.json());
   }
 
   updateMemberContact(projectId, memberId, contactId, roleId, contact) {
@@ -150,7 +144,7 @@ export class CincoService{
     body.append('contactHeadshotRef', contact.contact.headshotRef);
     body.append('contactType', contact.contact.type);
     return this.http.put('/projects/' + projectId + '/members/' + memberId + '/contacts/' + contactId + '/roles/' + roleId, body, headers)
-                .map((res) => res.json());
+      .map((res) => res.json());
   }
 
   /*
@@ -160,13 +154,13 @@ export class CincoService{
 
   getOrganizationContactTypes() {
     var response = this.http.get(this.baseUrl + '/organizations/contacts/types')
-            .map(res => res.json());
+      .map(res => res.json());
     return response;
   }
 
   getOrganizationContacts(organizationId) {
     var response = this.http.get(this.baseUrl + '/organizations/' + organizationId + '/contacts')
-            .map(res => res.json());
+      .map(res => res.json());
     return response;
   }
 
@@ -181,12 +175,12 @@ export class CincoService{
     body.append('email', contact.email);
     body.append('phone', contact.phone);
     return this.http.post('/organizations/' + organizationId + '/contacts', body, headers)
-                .map((res) => res.json());
+      .map((res) => res.json());
   }
 
   getOrganizationContact(organizationId, contactId) {
     var response = this.http.get(this.baseUrl + '/organizations/' + organizationId + '/contacts/' + contactId)
-            .map(res => res.json());
+      .map(res => res.json());
     return response;
   }
 
@@ -201,38 +195,38 @@ export class CincoService{
     body.append('email', contact.email);
     body.append('phone', contact.phone);
     return this.http.put('/organizations/' + organizationId + '/contacts/' + contactId, body, headers)
-                .map((res) => res.json());
+      .map((res) => res.json());
   }
 
-  /*
-    Organizations - Projects:
-    Resources for getting details about an organizations project membership
-   */
+  /**
+  * Organizations - Projects:
+  * Resources for getting details about an organizations project membership
+  **/
 
   getOrganizationProjectMemberships(organizationId) {
     var response = this.http.get(this.baseUrl + '/organizations/' + organizationId + '/projects_member')
-            .map(res => res.json());
+      .map(res => res.json());
     return response;
   }
 
-  /*
-    Users:
-    Resources to manage internal LF users and roles
-   */
+  /**
+  * Users
+  * Resources to manage internal LF users and roles
+  **/
 
   getCurrentUser() {
     return this.http.get(this.baseUrl + '/user')
-            .map(res => res.json());
+      .map(res => res.json());
   }
 
   getAllUsers() {
     return this.http.get(this.baseUrl + '/users')
-            .map(res => res.json());
+      .map(res => res.json());
   }
 
   getUser(userId) {
     return this.http.get(this.baseUrl + '/users/' + userId)
-            .map(res => res.json());
+      .map(res => res.json());
   }
 
   updateUser(userId, user) {
@@ -242,7 +236,7 @@ export class CincoService{
     body.append('email', user.email);
     body.append('calendar', user.calendar);
     return this.http.put('/users/' + userId, body, headers)
-            .map(res => res.json());
+      .map(res => res.json());
   }
 
 }
