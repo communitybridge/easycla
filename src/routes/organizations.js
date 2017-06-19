d930186edc6f64f738d5a049b2040df260816a96
fvega <fvega@linuxfoundation.org>
2017-06-19 14:38:17 -0500

    Refactor Organizations Router

--- src/routes/organizations.js
+++ src/routes/organizations.js
@@ -1,48 +1,27 @@
 if (process.env['NEWRELIC_LICENSE']) require('newrelic');
 var express = require('express');
-var passport = require('passport');
-var request = require('request');
-var multer  = require('multer');
-var async = require('async');
+var router = express.Router();
 
 var cinco = require("../lib/api");
 
-var upload = multer(); // for parsing multipart/form-data
+/**
+* Organizations:
+* Resources to expose and manipulate organizations
+**/
 
-var router = express.Router();
-
-var storageLogoCompany = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
+/**
+* GET /organizations/{orgId}
+* Get a single organization by Id
+**/
+router.get('/organizations/:organizationId', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res) {
+  if (req.session.user.isAdmin || req.session.user.isProjectManager) {
+    var organizationId = req.params.organizationId;
+    var projManagerClient = cinco.client(req.session.user.cinco_keys);
+    projManagerClient.getOrganization(organizationId, function(err, organization) {
+      if (err) return res.send('');
+      res.send(organization);
+    });
   }
 });
-var uploadLogoCompany = multer({ storage: storageLogoCompany });
-var cpUploadLogoCompany = uploadLogoCompany.fields([
-  {name: 'logoCompany', maxCount: 1},
-  {name: 'board_headshot', maxCount: 1 },
-  {name: 'technical_headshot', maxCount: 1 },
-  {name: 'marketing_headshot', maxCount: 1 },
-  {name: 'finance_headshot', maxCount: 1 },
-  {name: 'other_headshot', maxCount: 1 }
-]);
-
-/*
-  Organizations:
-  Resources to expose and manipulate organizations
- */
- 
- router.get('/organizations/:organizationId', require('connect-ensure-login').ensureLoggedIn('/login'), function(req, res){
-   if(req.session.user.isAdmin || req.session.user.isProjectManager){
-     var organizationId = req.params.organizationId;
-     var projManagerClient = cinco.client(req.session.user.cinco_keys);
-     projManagerClient.getOrganization(organizationId, function (err, organization) {
-       if (err) return res.send('');
-       res.send(organization);
-     });
-   }
- });
 
 module.exports = router;
