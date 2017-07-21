9c4355f0c5f0cc1264fa71090b9cec95e12e634e
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-07-21 10:49:04 -0400

    add modals and domain whitelist templating

---
+++ src/app/src/modals/cla-corporate-member-options-modal/cla-corporate-member-options-modal.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ClaCorporateMemberOptionsModal } from './cla-corporate-member-options-modal';
+import { LoadingSpinnerComponentModule } from '../../components/loading-spinner/loading-spinner.module';
+import { LoadingDisplayDirectiveModule } from '../../directives/loading-display/loading-display.module';
+
+@NgModule({
+  declarations: [
+    ClaCorporateMemberOptionsModal
+  ],
+  imports: [
+    LoadingSpinnerComponentModule,
+    LoadingDisplayDirectiveModule,
+    IonicPageModule.forChild(ClaCorporateMemberOptionsModal)
+  ],
+  entryComponents: [
+    ClaCorporateMemberOptionsModal
+  ]
+})
+export class ClaCorporateMemberOptionsModalModule {}
