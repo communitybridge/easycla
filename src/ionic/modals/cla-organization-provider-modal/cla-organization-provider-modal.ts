95d6ef896d14f9991513a256209d96ca29df185f
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-08-29 18:12:00 -0400

    add org app modal. add org popover. add validation starter

--- src/ionic/modals/cla-organization-provider-modal/cla-organization-provider-modal.ts
+++ src/ionic/modals/cla-organization-provider-modal/cla-organization-provider-modal.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController, IonicPage, } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { OrganizationValidator } from '../../validators/organization';
-import { CincoService } from '../../services/cinco.service'
+import { FormControl } from '@angular/forms';
+import { CincoService } from '../../services/cinco.service';
+import { Http } from '@angular/http';
 
 @IonicPage({
   segment: 'cla-organization-provider-modal'
@@ -24,14 +25,23 @@ export class ClaOrganizationProviderModal {
     public navParams: NavParams,
     public viewCtrl: ViewController,
     private formBuilder: FormBuilder,
+    public http: Http,
   ) {
     this.getDefaults();
     // this.uploadInfo = this.navParams.get('uploadInfo');
     this.form = formBuilder.group({
       provider: ['', Validators.required],
-      organization: ['', Validators.compose([Validators.required, OrganizationValidator.isValid])],
+      organization: ['', Validators.compose([Validators.required])/*, this.urlCheck.bind(this)*/],
     });
   }
+
+  // urlCheck(control: FormControl) {
+  //   let url = 'https://api.github.com/users/' + control.value;
+  //   return this.http.get(url).map(res => {
+  //     let json = res.json();
+  //     return json;
+  //   });
+  // }
 
   ngOnInit() {
 
@@ -56,6 +66,8 @@ export class ClaOrganizationProviderModal {
     //   // call any success messaging
     //   // navigate to previous page, root, or destination
     // });
+
+    // After submitting for the organization
   }
 
   dismiss() {
