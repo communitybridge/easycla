fb5a0b8153975020c83a47b29e63ddf848e1c77d
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-09-01 18:32:53 -0400

    make console a front end only app served by cla

--- infra/hooks.cli.py
+++ infra/hooks.cli.py
@@ -9,18 +9,19 @@ def local_init_docker_compose_file(containers, config, dependencies, envs, mode,
         platform_instance = dependencies[0]
         docker_config = lf.utils.loadYaml(os.path.join(platform_instance.path, 'docker-compose.yml'))
 
+        envs = docker_config['services']['workspace']['environment']
+        kc_endpoint = [x for k, x in envs.items() if k == 'KEYCLOAK_SERVER_URL'][0]
+        kc_instance = platform_instance.dependencies.get('keycloak')
+
+        kc_port = kc_endpoint.split(':')[2]
+        containers['workspace']['environment']['KEYCLOAK_SERVER_URL'] = 'http://' + kc_instance.containers.bridge_ip + ':' + kc_port
+        lf.logger.info('Setting KEYCLOAK_SERVER_URL to ' + containers['workspace']['environment']['KEYCLOAK_SERVER_URL'])
+
         for key, port in enumerate(docker_config['services']['workspace']['ports']):
             p = port.split(':')
             if p[1] == '5000':
-                envs['CINCO_SERVER_URL'] = 'http://' + platform_instance.containers.bridge_ip + ':' + p[0] + '/'
-                lf.logger.info('Setting CINCO_SERVER_URL to ' + envs['CINCO_SERVER_URL'])
-
-    for key, port in enumerate(containers['workspace']['ports']):
-        p = port.split(':')
-        if p[1] == '8081':
-            envs['CINCO_CONSOLE_URL'] = 'http://' + lf.storage.config['hostname'] + ':' + p[0] + '/'
-
-    lf.logger.info('Setting CINCO_CONSOLE_URL to ' + envs['CINCO_CONSOLE_URL'])
+                containers['workspace']['environment']['CINCO_SERVER_URL'] = 'http://' + platform_instance.containers.bridge_ip + ':' + p[0]
+                lf.logger.info('Setting CINCO_SERVER_URL to ' + containers['workspace']['environment']['CINCO_SERVER_URL'])
 
 
 @gossip.register('preprod_instance_task_build', tags=['cla-console'])
@@ -28,14 +29,18 @@ def preprod_instance_task_build(containers, instance_config, dependencies, domai
     if len(dependencies) >= 1:
         platform = dependencies.get('cinco')
 
+        task = platform.artifacts.get('ECSPreprodTask')
+        workspace = [x for x in task.containers if x['name'] == 'workspace'][0]
+        kc_endpoint = [x['value'] for x in workspace['environment'] if x['name'] == 'KEYCLOAK_SERVER_URL'][0]
+
         envs.append({
             'name': 'CINCO_SERVER_URL',
-            'value': 'https://' + platform.domain + '/'
+            'value': 'https://' + platform.domain
         })
         envs.append({
-            'name': 'CINCO_CONSOLE_URL',
-            'value': 'https://' + domains['primary'] + '/'
+            'name': 'KEYCLOAK_SERVER_URL',
+            'value': kc_endpoint
         })
 
-        lf.logger.info('Setting CINCO_CONSOLE_URL to ' + 'https://' + platform.domains['primary'] + '/')
-        lf.logger.info('Setting CINCO_SERVER_URL to ' + 'https://' + domains['primary'] + '/')
+        lf.logger.info('Setting CINCO_SERVER_URL to ' + 'https://' + domains['primary'])
+        lf.logger.info('Setting KEYCLOAK_SERVER_URL to ' + kc_endpoint)
---
+++ src/ionic/components/upload-button/upload-button.ts
@@ -0,0 +1,200 @@
+import { Component, ElementRef, Input, Output, ViewChild, Renderer, EventEmitter } from '@angular/core';
+import { PopoverController, ToastController } from 'ionic-angular';
+
+/*
+  Generated class for the UploadButton component.
+
+  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+  for more info on Angular 2 Components.
+*/
+@Component({
+  selector: 'upload-button',
+  templateUrl: 'upload-button.html'
+})
+export class UploadButtonComponent {
+
+  /**
+   * The fileList maintained as files are added and removed
+   */
+  fileList: Array<any>;
+
+  /**
+   * The text used for the upload label
+   */
+  @Input()
+  private files: Array<any>;
+
+  /**
+   * The text used for the upload label
+   */
+  @Input()
+  private uploadText: String;
+
+  /**
+   * Comma separated array of allowed file extensions
+   */
+  @Input()
+  private uploadTypes: String;
+
+  /**
+   * If multiple files can be uploaded
+   * @TODO: implement this feature so it affects the input multiple attribute
+   *        and also build in validation for number of files allowed (1 or many)
+   */
+  @Input()
+  private multiple: boolean;
+
+  /**
+   * Native upload button (hidden)
+   */
+  @ViewChild('input')
+  private nativeInputBtn: ElementRef;
+
+  /**
+   * Event emitted after FileList is modified
+   */
+  @Output() notify: EventEmitter<Array<any>> = new EventEmitter<Array<any>>();
+
+  constructor(
+    private renderer: Renderer,
+    private popoverCtrl: PopoverController,
+    public toastCtrl: ToastController,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.fileList = this.files;
+
+    if (typeof this.multiple == 'undefined') {
+      this.multiple = false;
+    }
+
+    if(!this.uploadText) {
+      this.uploadText = "Upload";
+    }
+  }
+
+  /**
+  * Callback executed when the visible button is pressed
+  * @param  {Event}  event should be a mouse click event
+  */
+  public callback(event: Event): void {
+
+    // trigger click event of hidden input
+    let clickEvent: MouseEvent = new MouseEvent("click", {bubbles: true});
+    this.renderer.invokeElementMethod(
+      this.nativeInputBtn.nativeElement, "dispatchEvent", [clickEvent]
+    );
+  }
+
+  /**
+  * Callback which is executed after files from native popup are selected.
+  * @param  {Event}    event change event containing selected files
+  */
+  filesAdded(event: Event): void {
+    let addedFiles: FileList = this.nativeInputBtn.nativeElement.files;
+
+    for(let i=0; i< addedFiles.length; i++) {
+      let file = addedFiles.item(i);
+      let valid = this.validateFile(file);
+      if(valid) {
+        // merge files from the input with the fileList
+        if(!this.fileList) {
+          this.fileList = [];
+        }
+        this.fileList.push(file);
+      }
+    }
+    this.notify.emit(this.fileList);
+  }
+
+  validateFile(file) {
+    if(typeof this.uploadTypes == 'undefined') {
+      return true;
+    }
+    // Validate extension by checking extension in filename against uploadTypes
+    var validTypes = this.uploadTypes.split(',');
+    var extensionValid = false;
+    for (var i = 0; i < validTypes.length; i++) {
+        var currentType = validTypes[i];
+        if (file.name.substr(file.name.length - currentType.length, currentType.length).toLowerCase() == currentType.toLowerCase()) {
+            extensionValid = true;
+            return extensionValid;
+        }
+    }
+    if (!extensionValid) {
+      this.uploadError("Sorry, " + file.name + " is invalid, allowed extensions are: " + validTypes.join(", "));
+      return false;
+    }
+  }
+
+  uploadError(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      showCloseButton: true,
+      closeButtonText: 'Dismiss',
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  presentPopover(ev, index) {
+    let popoverData = {
+      items: [
+        {
+          label: 'Download file',
+          callback: 'fileDownload',
+          callbackData: {
+            index: index,
+          }
+        },
+        {
+          label: 'Delete file',
+          callback: 'fileDelete',
+          callbackData: {
+            index: index,
+          }
+        },
+      ]
+    };
+
+    let popover = this.popoverCtrl.create(
+      'ActionPopoverComponent',
+      popoverData,
+    );
+
+    popover.present({
+      ev: ev
+    });
+
+    popover.onDidDismiss((popoverData) => {
+      if(popoverData) {
+        this.popoverResponse(popoverData);
+      }
+    });
+  }
+
+  /**
+   * Called if popover dismissed with data. Passes data to a callback function
+   * @param  {object} popoverData should contain .callback and .callbackData
+   */
+  popoverResponse(popoverData) {
+    let callback = popoverData.callback;
+    if(this[callback]) {
+      this[callback](popoverData.callbackData);
+    }
+  }
+
+  fileDelete(data) {
+    this.fileList.splice(data.index, 1);
+    this.notify.emit(this.fileList);
+  }
+
+  fileDownload(data) {
+    // File download:
+    // data.index
+    // this.fileList
+  }
+
+}
