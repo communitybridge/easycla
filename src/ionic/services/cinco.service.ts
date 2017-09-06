553336f095726de6c440f4c24ad461858548b5d9
root <email@example.com>
2017-09-06 15:39:15 -0500

    Add methods to obtain and upload documents to S3. Refactor methods to buildHeaders for CINCO and S3

--- src/ionic/services/cinco.service.ts
+++ src/ionic/services/cinco.service.ts
@@ -80,7 +80,17 @@ export class CincoService {
   }
 
   /**
-  * PUT /projects/{projectId}/logos/{classifier}
+  * This endpoint will return an array of all documents for a project.
+  * Each object contains a url property indicating a temporary URL where the actual document can be retrieved with a GET request.
+  * The expiresOn property is an ISO-8601 timestamp indicating the last millisecond the URL will be valid to issue a GET request to.
+  **/
+  getProjectDocuments(projectId) {
+    return this.http.get(this.cincoApiUrl + '/projects/' + projectId + '/documents')
+      .map(res => res.json());
+  }
+
+  /**
+  * PUT /projects/{projectId}/logos/{classifier}/
   * For logos, any program manager/admin should be able to submit a PUT to `/projects/{projectId}/logos/{classifier}`.
   * The request must have a Content-Type header of `image/**` (e.g. `image/png` or `image/jpeg`)
   * and can optionally include the image bytes in the body.
@@ -89,23 +99,30 @@ export class CincoService {
   * Location header (and body) will contain a URL to S3 that the client can resubmit the PUT to.
   * That will actually upload the image.
   **/
-  obtainS3URL(projectId, classifier, image) {
-    return this.http.putS3URL(this.cincoApiUrl + '/projects/' + projectId + '/logos/' + classifier, image, image.contentType)
+  obtainLogoS3URL(projectId, classifier, image) {
+    return this.http.put(this.cincoApiUrl + '/projects/' + projectId + '/logos/' + classifier, image, image.contentType)
       .map(res => res);
   }
 
-  uploadLogo(S3URL, file, contentType) {
-    // let headers = new Headers();
-    // headers.append('Content-Type', contentType);
-    // return this.http.put(S3URL, file, {headers: headers})
-    //TODO: Change headers in httpClient to ('Content-Type', contentType);
-    return this.http.putUploadS3URL(S3URL, file, contentType)
-      // .catch((e) => {
-      //   console.log(e)
-      //   return e;
-      // })
+  /**
+  * PUT /projects/{projectId}/documents/{classifier}/{filename}/
+  * Accepts a request whose body is optionally blank (the body will be ignored).
+  * The request headers must specify the Content-Type of the document.
+  * The classifier path parameter corresponds as a single word class of this logo.
+  * The values of the classifier path are limited to known and expected values, such as "minutes", or "bylaws".
+  * A successful request will result in a 307, indicating that the client should retry the request with
+  * the returned URI found in the responses location header and the binary contents of the logo in the request body
+  * The response body contains details about the request. The putUrl.url property is where the real upload request should occur.
+  * The putUrl.expiresOn property is an ISO-8601 timestamp indicating the last millisecond the putURL will be valid.
+  **/
+  obtainDocumentS3URL(projectId, classifier, filename) {
+    return this.http.put(this.cincoApiUrl + '/projects/' + projectId + '/documents/' + classifier + '/' + filename, null, filename.contentType)
+      .map(res => res);
+  }
+
+  uploadToS3(S3URL, file, contentType) {
+    return this.http.putS3(S3URL, file, contentType)
       .map((res) => res );
-      // .map(response => response.ok)
   }
 
   //////////////////////////////////////////////////////////////////////////////
