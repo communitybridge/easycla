a011ad5f666ab87b502ffc715263dc6fa690508e
Edouard Poitras <epoitras@linuxfoundation.org>
2017-09-11 13:34:10 -0400

    Helper script can be used again
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- helpers/create_database.py
+++ helpers/create_database.py
@@ -7,9 +7,7 @@ Based on the database storage configuration found in config.py and config_instan
 # Temporary for testing purposes - create the Project and Document objects.
 TEST_DOCUMENT_URL = 'https://github.com/cncf/cla/raw/master/individual-cla.pdf'
 # The GitHub user/org used for testing purposes.
-GITHUB_PROJECT_NAME = 'edouardpoitras'
-# The GitLab user used for testing purposes.
-GITLAB_PROJECT_NAME = 'eddie'
+GITHUB_PROJECT_NAME = 'cla-test'
 
 import sys
 sys.path.append('../')
@@ -28,13 +26,11 @@ from cla.utils import get_project_instance, get_document_instance
 ## Project
 cla.log.info('Creating new project: %s', 'Test Project')
 github_project = get_project_instance()
-github_project.set_project_id(GITHUB_PROJECT_NAME)
+github_project.set_project_id((uuid.uuid4()))
 github_project.set_project_name('Test GitHub Project')
+github_project.set_project_external_id(GITHUB_PROJECT_NAME)
+github_project.set_project_ccla_requires_icla_signature(False)
 github_project.save()
-gitlab_project = get_project_instance()
-gitlab_project.set_project_id(GITLAB_PROJECT_NAME)
-gitlab_project.set_project_name('Test GitLab Project')
-gitlab_project.save()
 # Document
 # Slower as the document is fetched every time a document signature is initiated.
 #document = Document(str(uuid.uuid4()), 'Test Document', 'url+pdf', TEST_DOCUMENT_URL)
@@ -45,10 +41,9 @@ document.set_document_name('Test Document')
 document.set_document_file_id(str(uuid.uuid4()))
 document.set_document_content_type('storage+pdf')
 document.set_document_content(data)
-document.set_document_revision(1)
+document.set_document_major_version(1)
+document.set_document_minor_version(0)
 cla.log.info('Adding CLA document to new project: %s', TEST_DOCUMENT_URL)
 github_project.add_project_individual_document(document)
 github_project.save()
-gitlab_project.add_project_individual_document(document)
-gitlab_project.save()
 cla.log.info('Done')
