7876c31e31c9f7ab203e11f15361025d4b1fb6e9
Philippe Labat <phil@Pheonix.local>
2017-09-25 14:48:45 -0400

    Changed deps

--- scripts/generate-constants.sh
+++ scripts/generate-constants.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "export const CINCO_API_URL: string = \"${CINCO_SERVER_URL}\";" > /srv/app/src/ionic/services/constants.ts
+echo "export const CLA_API_URL: string = \"${CLA_SERVER_URL}\";" > /srv/app/src/ionic/services/constants.ts
 echo "Wrote /srv/app/src/ionic/services/constants.ts"
