c387f069b71dc5617460859157ff0c8960085188
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-10-02 16:37:42 -0400

    add the /auth suffix for kc endpoint

--- scripts/generate-constants.sh
+++ scripts/generate-constants.sh
@@ -5,7 +5,7 @@ echo "Wrote /srv/app/src/ionic/services/constants.ts"
 
 echo "{" > /srv/app/src/ionic/assets/keycloak.json
 echo "  \"realm\": \"LinuxFoundation\"," >> /srv/app/src/ionic/assets/keycloak.json
-echo "  \"auth-server-url\": \"${KEYCLOAK_SERVER_URL}\"," >> /srv/app/src/ionic/assets/keycloak.json
+echo "  \"auth-server-url\": \"${KEYCLOAK_SERVER_URL}/auth\"," >> /srv/app/src/ionic/assets/keycloak.json
 echo "  \"ssl-required\": \"external\"," >> /srv/app/src/ionic/assets/keycloak.json
 echo "  \"resource\": \"pmc\"," >> /srv/app/src/ionic/assets/keycloak.json
 echo "  \"public-client\": true" >> /srv/app/src/ionic/assets/keycloak.json
