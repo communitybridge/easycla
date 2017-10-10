3c02f19cdf1e835a2dd26db1e9467f9fe3f971a6
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-10-10 14:00:23 -0400

    remove unused elements and move shared elements to packages

--- src/ionic/modals/cla-new-company-modal/cla-new-company-modal.module.ts
+++ src/ionic/modals/cla-new-company-modal/cla-new-company-modal.module.ts
@@ -3,7 +3,6 @@ import { IonicPageModule } from 'ionic-angular';
 import { ClaNewCompanyModal } from './cla-new-company-modal';
 import { LoadingSpinnerComponentModule } from '../../components/loading-spinner/loading-spinner.module';
 import { LoadingDisplayDirectiveModule } from '../../directives/loading-display/loading-display.module';
-import { SortingDisplayComponentModule } from '../../components/sorting-display/sorting-display.module';
 
 // import { ClipboardModule } from 'ngx-clipboard';
 
@@ -14,7 +13,6 @@ import { SortingDisplayComponentModule } from '../../components/sorting-display/
   imports: [
     LoadingSpinnerComponentModule,
     LoadingDisplayDirectiveModule,
-    SortingDisplayComponentModule,
     // ClipboardModule,
     IonicPageModule.forChild(ClaNewCompanyModal)
   ],
