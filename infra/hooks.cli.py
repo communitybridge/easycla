31d0ef1b17e2d59ac0ea85eccc285151703b9819
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-10-10 14:37:33 -0400

    remove unused hook

--- infra/hooks.cli.py
+++ infra/hooks.cli.py
@@ -12,8 +12,3 @@ def local_init_docker_compose_file(containers, config, dependencies, envs, mode,
         cla_endpoint = cla.endpoints.containers.get('workspace', 5000).formatted
         containers['workspace']['environment']['CLA_SERVER_URL'] = cla_endpoint
         lf.logger.info('Setting CLA_SERVER_URL to ' + containers['workspace']['environment']['CLA_SERVER_URL'])
-
-
-@gossip.register('preprod_instance_task_build', tags=['cla-console'])
-def preprod_instance_task_build(containers, instance_config, dependencies, domains, envs):
-    if len(dependencies) >= 1:
