bb25ac11267720e1ef4445cd91425d155be9a39b
Edouard Poitras <epoitras@linuxfoundation.org>
2017-10-16 11:28:09 -0400

    Issue #CLA-102 add CLA dependency to CINCO
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- infra/hooks.cli.py
+++ infra/hooks.cli.py
@@ -22,9 +22,27 @@ def host_port(containers, container_name, idx=0):
 
 @gossip.register('local.instance.init.docker-compose', tags=['cla'])
 def local_init_docker_compose_file(containers, config, dependencies, envs, mode, path):
+    """
+    All necessary steps to integrate CINCO and Keycloak into the CLA system.
 
-    # If dev mode, we don't provision a workspace container
+    If dev mode, we don't provision a workspace container and assume the user has manually
+    configured the appropriate dependencies.
+    """
     if mode == 'dev':
+        lf.logger.info('No workspace container provisioned in dev mode')
+        lf.logger.info('Ensure the cla_config.py file has the appropriate configuration for your \
+                       external dependencies')
         del containers['workspace']
-
-        # We need to write the configuration file
+    else:
+        # Should we use CINCO's DynamoDB instance here?
+        lf.logger.info('Automatically setting CLA dependency endpoints')
+        platform = dependencies.get('cinco')
+        if platform:
+            cinco_endpoint = platform.endpoints.containers.get('workspace', 5000).formatted
+            lf.logger.info('CINCO: %s', cinco_endpoint)
+            containers['workspace']['environment']['CLA_CINCO_ENDPOINT'] = cinco_endpoint
+            keycloak = platform.instance.dependencies.get('keycloak')
+            if keycloak:
+                kc_endpoint = keycloak.endpoints.containers.get('workspace', 8080).formatted
+                lf.logger.info('Keycloak: %s', kc_endpoint)
+                containers['workspace']['environment']['CLA_KEYCLOAK_ENDPOINT'] = kc_endpoint
