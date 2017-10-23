12b3ffa089672b603edc05a7524a972b409fe388
fvega <fvega@linuxfoundation.org>
2017-10-23 11:49:35 -0500

    Add sorting for All Logos Table

--- src/ionic/pages/all-projects-logos/all-projects-logos.module.ts
+++ src/ionic/pages/all-projects-logos/all-projects-logos.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
 import { AllProjectsLogosPage } from './all-projects-logos';
+import { SortingDisplayComponentModule } from '../../components/sorting-display/sorting-display.module';
 
 @NgModule({
   declarations: [
     AllProjectsLogosPage
   ],
   imports: [
+    SortingDisplayComponentModule,
     IonicPageModule.forChild(AllProjectsLogosPage)
   ],
 })
