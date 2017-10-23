14aef01d56fe4cb9cdca4a69cf96f912d6899719
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-10-23 12:00:49 -0400

    remove missing references to removed modules

--- src/ionic/modals/cla-select-company-modal/cla-select-company-modal.module.ts
+++ src/ionic/modals/cla-select-company-modal/cla-select-company-modal.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ClaSelectCompanyModal } from './cla-select-company-modal';
-import { LoadingSpinnerComponentModule } from '../../components/loading-spinner/loading-spinner.module';
-import { LoadingDisplayDirectiveModule } from '../../directives/loading-display/loading-display.module';
-import { SortingDisplayComponentModule } from '../../components/sorting-display/sorting-display.module';
 
 @NgModule({
   declarations: [
     ClaSelectCompanyModal,
   ],
   imports: [
-    LoadingSpinnerComponentModule,
-    LoadingDisplayDirectiveModule,
-    SortingDisplayComponentModule,
     IonicPageModule.forChild(ClaSelectCompanyModal)
   ],
   entryComponents: [
