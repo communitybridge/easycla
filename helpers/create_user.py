afcad69f8fb289d617113785df0249cf3f846cc6
Edouard Poitras <epoitras@linuxfoundation.org>
2017-11-03 15:31:04 -0400

    Added whitelisted user in testing data
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- helpers/create_user.py
+++ helpers/create_user.py
@@ -9,10 +9,17 @@ import cla
 from cla.utils import get_user_instance
 
 # Test User.
-cla.log.info('Creating new user')
+cla.log.info('Creating first user')
 user = get_user_instance()
 user.set_user_id(str(uuid.uuid4()))
 user.set_user_name('***REMOVED*** ***REMOVED***')
 user.set_user_email('***REMOVED***@linuxfoundation.org')
 user.set_user_github_id(123)
+user.save()
+cla.log.info('Creating second user')
+user = get_user_instance()
+user.set_user_id(str(uuid.uuid4()))
+user.set_user_name('Whitelisted User')
+user.set_user_email('white@listed.org')
+user.set_user_github_id(234)
 user.save()
