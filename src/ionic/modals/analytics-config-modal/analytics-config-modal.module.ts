c68b023085d9611b73f65eb7ed766d68f159b371
fvega <fvega@linuxfoundation.org>
2017-11-07 18:22:22 -0500

    Add Analytics Config Modal

---
+++ src/ionic/modals/analytics-config-modal/analytics-config-modal.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AnalyticsConfigModal } from './analytics-config-modal';
+import { LoadingSpinnerComponentModule } from '../../components/loading-spinner/loading-spinner.module';
+import { LoadingDisplayDirectiveModule } from '../../directives/loading-display/loading-display.module';
+
+@NgModule({
+  declarations: [
+    AnalyticsConfigModal
+  ],
+  imports: [
+    LoadingSpinnerComponentModule,
+    LoadingDisplayDirectiveModule,
+    IonicPageModule.forChild(AnalyticsConfigModal)
+  ],
+  entryComponents: [
+    AnalyticsConfigModal
+  ]
+})
+export class AnalyticsConfigModalModule {}
