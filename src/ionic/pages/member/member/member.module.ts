1b0532da12ce34b6751b689562bb03d20c273822
zaclittleberry <zlittleberry@linuxfoundation.org>
2017-11-15 10:23:11 -0500

    refactor to use member-header and member-navigation

---
+++ src/ionic/pages/member/member/member.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MemberPage } from './member';
+import { LoadingSpinnerComponentModule } from '../../../components/loading-spinner/loading-spinner.module';
+import { LoadingDisplayDirectiveModule } from '../../../directives/loading-display/loading-display.module';
+import { SortingDisplayComponentModule } from '../../../components/sorting-display/sorting-display.module';
+import { MemberHeaderComponentModule } from '../../../components/member-header/member-header.module';
+import { MemberNavigationComponentModule } from '../../../components/member-navigation/member-navigation.module';
+
+@NgModule({
+  declarations: [
+    MemberPage
+  ],
+  imports: [
+    LoadingSpinnerComponentModule,
+    LoadingDisplayDirectiveModule,
+    SortingDisplayComponentModule,
+    MemberHeaderComponentModule,
+    MemberNavigationComponentModule,
+    IonicPageModule.forChild(MemberPage)
+  ],
+  entryComponents: [
+    MemberPage
+  ]
+})
+export class MemberPageModule {}
