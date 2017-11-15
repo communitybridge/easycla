f6de8dd49bc049d9be667ab5223463a6ac3d9be7
Philippe Labat <plabat@linuxfoundation.org>
2017-11-15 12:18:09 -0500

    Nov 15th Update

--- modules/sandbox/terraform.tf
+++ modules/sandbox/terraform.tf
@@ -59,7 +59,7 @@ resource "aws_alb_target_group" "sandbox-main-port" {
 
 resource "aws_alb" "sandbox-main" {
   name               = "${var.project_name}-${var.instance_name}"
-  subnets            = ["${join(",", var.subnets)}"]
+  subnets            = ["${split(",", var.subnets)}"]
   security_groups    = ["${var.public == true ? data.terraform_remote_state.sandbox-env.external_elb_sg : data.terraform_remote_state.sandbox-env.internal_elb_sg}"]
   internal           = "${var.public}"
 }
