b414608896a06c2518e043de8c37c8dd97f06bc4
Fernando Vega <fvega@linuxfoundation.org>
2017-12-04 18:25:05 -0500

    Merge develop into master. (#94)
    * Make LF Users link clickable
    
    * Add Project Analytics Page
    
    * Add editProjectConfig method in Cinco Service. Display Bitergia iframe if config exists.
    
    * Add Analytics Config Modal
    
    * Fixed Typo in KC Url, Whoops!
    
    * Removing "Waiting for CINCO" Step from CI Procedures
    
    * Fix for CLI 3.0
    
    * refactor to use member-header and member-navigation
    
    * change field requirements
    
    * fix user calendar recursive embedding
    
    * Testing latest KC and CINCO roles
    
    * save WIP re Analytics Charts
    
    * get cla service connected with pmc
    
    * prepare to connect signatures page
    
    * Disable Analytics Tab as its API is not online yet
    
    * fix npmrc to point to the group, not just the proxy
    
    * fix spacing; kick jenkins
    
    * add member details page (currently project details page)
    
    * project details -> member details. add project id to member navigation.
    
    * Upgrade KC libraries

---
+++ src/ionic/pages/project/project-analytics/project-analytics.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController, NavParams, IonicPage } from 'ionic-angular';
+import { CincoService } from '../../../services/cinco.service';
+import { KeycloakService } from '../../../services/keycloak/keycloak.service';
+import { DomSanitizer} from '@angular/platform-browser';
+
+@IonicPage({
+  segment: 'project/:projectId/analytics'
+})
+@Component({
+  selector: 'project-analytics',
+  templateUrl: 'project-analytics.html',
+  providers: [CincoService]
+})
+export class ProjectAnalyticsPage {
+
+  projectId: string;
+
+  hasBitergia: boolean;
+  analyticsUrl: any;
+  sanitizedBitergiaUrl: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private cincoService: CincoService,
+    private keycloak: KeycloakService,
+    private domSanitizer : DomSanitizer,
+    public modalCtrl: ModalController
+  ) {
+    this.projectId = navParams.get('projectId');
+    this.getDefaults();
+  }
+
+  ionViewCanEnter() {
+    if(!this.keycloak.authenticated())
+    {
+      this.navCtrl.setRoot('LoginPage');
+      this.navCtrl.popToRoot();
+    }
+    return this.keycloak.authenticated();
+  }
+
+  ionViewWillEnter() {
+    if(!this.keycloak.authenticated())
+    {
+      this.navCtrl.push('LoginPage');
+    }
+  }
+
+  ngOnInit() {
+    this.getProjectConfig(this.projectId);
+  }
+
+  getDefaults() {
+
+  }
+
+  getProjectConfig(projectId) {
+    this.cincoService.getProjectConfig(projectId).subscribe(response => {
+      if (response) {
+        let projectConfig = response;
+        if(projectConfig.analyticsUrl) {
+          this.analyticsUrl = projectConfig.analyticsUrl;
+          this.sanitizedBitergiaUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.analyticsUrl);
+          this.hasBitergia = true;
+        }
+        else{
+          this.hasBitergia = true;
+        }
+      }
+    });
+  }
+
+  openAnaylticsConfigModal(projectId) {
+    let modal = this.modalCtrl.create('AnalyticsConfigModal', {
+      projectId: projectId,
+    });
+    modal.onDidDismiss(analyticsUrl => {
+      if(analyticsUrl){
+        this.analyticsUrl = analyticsUrl;
+        this.hasBitergia = true;
+        this.sanitizedBitergiaUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.analyticsUrl);
+      }
+    });
+    modal.present();
+  }
+
+}
