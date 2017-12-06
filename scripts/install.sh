daa658108744547142ecf1d3c131159d31513c80
Edouard Poitras <epoitras@linuxfoundation.org>
2017-12-06 10:01:35 -0500

    Now compatible with latest LF CLI tool (>3.1)
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,15 @@
 
 yum install libxslt-devel libxml2-devel gcc python36u python36u-pip python36u-devel python36u-dev -y
 
-pip3.6 install -r /srv/app/requirements.txt
+pip3.6 install -r /srv/app/requirements.txt --user
 
-python3.6 /srv/app/setup.py install
+python3.6 /srv/app/setup.py install --user
 
-echo "Creating /root/.aws/credentials file"
-mkdir -p /root/.aws
+echo "Creating /home/www-data/.aws/credentials file"
+mkdir -p /home/www-data/.aws
 echo '[default]
 aws_access_key_id=""
-aws_secret_access_key=""' > /root/.aws/credentials
+aws_secret_access_key=""' > /home/www-data/.aws/credentials
 
 #python3.6 /srv/app/helpers/create_test_environment.py
 python3.6 /srv/app/helpers/create_database.py
