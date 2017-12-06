2a6d9ce8e75e055efb053fb907c6b567201df71f
Edouard Poitras <epoitras@linuxfoundation.org>
2017-12-06 10:57:45 -0500

    Issue CLA-156 cleaned up the hooks and configuration with the --dev flag
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-yum install libxslt-devel libxml2-devel gcc python36u python36u-pip python36u-devel python36u-dev -y
-
 pip3.6 install -r /srv/app/requirements.txt --user
 
 python3.6 /srv/app/setup.py install --user
