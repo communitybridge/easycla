3ce54538c831fb0c8a5cf6ff4586c246b1a3e69f
zaclittleberry <zlittleberry@linuxfoundation.org>
2018-01-18 19:04:24 -0500

    add activity page

--- src/ionic/pages/activity-log/activity-log.ts
+++ src/ionic/pages/activity-log/activity-log.ts
@@ -15,7 +15,12 @@ import { Restricted } from '../../decorators/restricted';
   templateUrl: 'activity-log.html'
 })
 export class ActivityLogPage {
+  allProjects: any;
+  selectedProject: any;
   events: any;
+  loading: any;
+  users: any;
+  expand: any;
 
   constructor(
     public navCtrl: NavController,
@@ -27,18 +32,61 @@ export class ActivityLogPage {
 
   getDefaults() {
     this.events = [];
+    this.loading = {
+      events: true,
+    };
+    this.expand = {};
+    this.users = {};
   }
 
   ngOnInit(){
     this.getEvents();
+    this.getProjects();
   }
 
-  getEvents() {
-    this.cincoService.getEventsForProject('a0941000002wByYAAU').subscribe(response => {
+  getProjects(){
+    this.cincoService.getAllProjects().subscribe(response => {
+      this.allProjects = response;
+      this.selectedProject = this.allProjects[0];
+      this.projectSelectChanged();
+    });
+  }
+
+  projectSelectChanged() {
+    let projectId = this.selectedProject.id;
+    this.events = [];
+    this.loading.events = true;
+    this.getEvents(projectId);
+  }
+
+  getEvents(projectId) {
+    this.cincoService.getEventsForProject(projectId).subscribe(response => {
       if (response) {
         console.log(response);
+        this.events = response;
+        for (let event of this.events) {
+          this.getUser(event.userId);
+        }
+        this.loading.events = false;
       }
     });
+  }
+
+  getUser(userId) {
+    if(!this.users[userId]) {
+      this.users[userId] = true; //placeholder
+      this.cincoService.getUser(userId).subscribe(response => {
+        if (response) {
+          this.users[userId] = response;
+          console.log(this.users);
+        }
+      });
+    }
+  }
+
+  toggle(index) {
+    this.expand[index] = !this.expand[index];
+    console.log(this.expand);
   }
 
 }
