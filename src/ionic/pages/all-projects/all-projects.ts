1e541dfb5e484c1045a6bb9c6e057568cdb6ae33
zaclittleberry <zlittleberry@linuxfoundation.org>
2018-01-18 19:55:56 -0500

    more fully comment out chart and calendar scripts

--- src/ionic/pages/all-projects/all-projects.ts
+++ src/ionic/pages/all-projects/all-projects.ts
@@ -1,9 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
-import { DomSanitizer} from '@angular/platform-browser';
+// import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+// import { DomSanitizer} from '@angular/platform-browser';
 import { NavController, IonicPage } from 'ionic-angular';
-import { KeycloakService } from '../../services/keycloak/keycloak.service';
 import { CincoService } from '../../services/cinco.service'
-import { Chart } from 'chart.js';
+// import { Chart } from 'chart.js';
 import { FilterService } from '../../services/filter.service'
 import { RolesService } from '../../services/roles.service';
 import { Restricted } from '../../decorators/restricted';
@@ -21,21 +21,21 @@ import { Restricted } from '../../decorators/restricted';
 })
 export class AllProjectsPage {
   loading: any;
-  userHasCalendar: boolean;
+  // userHasCalendar: boolean;
   allProjects: any;
   allFilteredProjects: any;
-  numberOfContracts: {
-    new: number,
-    renewal: number,
-  }
-  user: any;
-  numberOfInvoices: {
-    fewerThan60Days: number,
-    fewerThan30Days: number,
-    sent: number,
-    late: number,
-    paidLast30Days: number,
-  }
+  // numberOfContracts: {
+  //   new: number,
+  //   renewal: number,
+  // }
+  // user: any;
+  // numberOfInvoices: {
+  //   fewerThan60Days: number,
+  //   fewerThan30Days: number,
+  //   sent: number,
+  //   late: number,
+  //   paidLast30Days: number,
+  // }
   projects: Array<{
     icon: string,
     title: string,
@@ -59,17 +59,16 @@ export class AllProjectsPage {
 
   userRoles: any;
 
-  @ViewChild('contractsCanvas') contractsCanvas;
-  contractsChart: any;
-
-  @ViewChild('invoicesCanvas') invoicesCanvas;
-  invoicesChart: any;
+  // @ViewChild('contractsCanvas') contractsCanvas;
+  // contractsChart: any;
+  //
+  // @ViewChild('invoicesCanvas') invoicesCanvas;
+  // invoicesChart: any;
 
   constructor(
     public navCtrl: NavController,
     private cincoService: CincoService,
-    private sanitizer: DomSanitizer,
-    private keycloak: KeycloakService,
+    // private sanitizer: DomSanitizer,
     private rolesService: RolesService,
     private filterService: FilterService,
   ) {
@@ -78,7 +77,7 @@ export class AllProjectsPage {
 
   async ngOnInit() {
     this.getIndustries();
-    this.getCurrentUser();
+    // this.getCurrentUser();
     this.getAllProjects();
   }
 
@@ -112,15 +111,15 @@ export class AllProjectsPage {
     });
   }
 
-  getCurrentUser() {
-    this.cincoService.getCurrentUser().subscribe(response => {
-      this.user = response;
-      if (response.hasOwnProperty('calendar') && response.calendar) {
-        this.userHasCalendar = true;
-        this.user.calendar = this.sanitizer.bypassSecurityTrustResourceUrl(response.calendar);
-      }
-    });
-  }
+  // getCurrentUser() {
+  //   this.cincoService.getCurrentUser().subscribe(response => {
+  //     this.user = response;
+  //     if (response.hasOwnProperty('calendar') && response.calendar) {
+  //       this.userHasCalendar = true;
+  //       this.user.calendar = this.sanitizer.bypassSecurityTrustResourceUrl(response.calendar);
+  //     }
+  //   });
+  // }
 
   viewProject(projectId) {
     this.navCtrl.setRoot('ProjectPage', {
@@ -134,121 +133,121 @@ export class AllProjectsPage {
     });
   }
 
-  ionViewDidLoad() {
-    let barOptions = this.getBarOptions();
-    this.contractsChart = new Chart(this.contractsCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ["NEW", "RENEWAL"],
-        datasets: [{
-             label: '# of Contracts',
-             data: [this.numberOfContracts.new, this.numberOfContracts.renewal],
-             backgroundColor: [
-                 'rgba(163,131,107,1)',
-                 'rgba(225,170,128,1)',
-             ]
-         }]
-      },
-      options: barOptions
-    });
+  // ionViewDidLoad() {
+  //   let barOptions = this.getBarOptions();
+  //   this.contractsChart = new Chart(this.contractsCanvas.nativeElement, {
+  //     type: 'bar',
+  //     data: {
+  //       labels: ["NEW", "RENEWAL"],
+  //       datasets: [{
+  //            label: '# of Contracts',
+  //            data: [this.numberOfContracts.new, this.numberOfContracts.renewal],
+  //            backgroundColor: [
+  //                'rgba(163,131,107,1)',
+  //                'rgba(225,170,128,1)',
+  //            ]
+  //        }]
+  //     },
+  //     options: barOptions
+  //   });
+  //
+  //   this.invoicesChart = new Chart(this.invoicesCanvas.nativeElement, {
+  //     type: 'bar',
+  //     data: {
+  //       labels: ["<60 Days", "<30 Days", "SENT", "LATE", "PAID"],
+  //       datasets: [{
+  //            label: '# of Invoices',
+  //            data: [
+  //              this.numberOfInvoices.fewerThan60Days,
+  //              this.numberOfInvoices.fewerThan30Days,
+  //              this.numberOfInvoices.sent,
+  //              this.numberOfInvoices.late,
+  //              this.numberOfInvoices.paidLast30Days],
+  //            backgroundColor: [
+  //                'rgba(196,221,140,1)',
+  //                'rgba(171,206,92,1)',
+  //                'rgba(136,186,22,1)',
+  //                'rgba(245,166,35,1)',
+  //                'rgba(65,117,5,1)',
+  //            ]
+  //        }]
+  //     },
+  //     options: barOptions
+  //   });
+  //
+  // }
 
-    this.invoicesChart = new Chart(this.invoicesCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ["<60 Days", "<30 Days", "SENT", "LATE", "PAID"],
-        datasets: [{
-             label: '# of Invoices',
-             data: [
-               this.numberOfInvoices.fewerThan60Days,
-               this.numberOfInvoices.fewerThan30Days,
-               this.numberOfInvoices.sent,
-               this.numberOfInvoices.late,
-               this.numberOfInvoices.paidLast30Days],
-             backgroundColor: [
-                 'rgba(196,221,140,1)',
-                 'rgba(171,206,92,1)',
-                 'rgba(136,186,22,1)',
-                 'rgba(245,166,35,1)',
-                 'rgba(65,117,5,1)',
-             ]
-         }]
-      },
-      options: barOptions
-    });
-
-  }
-
-  getBarOptions(){
-    return {
-      layout:{
-        padding: 20
-      },
-      responsive: true,
-      tooltips: {
-          enabled: true
-      },
-      hover :{
-          animationDuration:0
-      },
-      scales: {
-          xAxes: [{
-              ticks: {
-                  beginAtZero:true,
-                  fontSize:11
-              },
-              scaleLabel:{
-                  display:false
-              },
-              gridLines: {
-                display:false,
-              },
-              stacked: true
-          }],
-          yAxes: [{
-              gridLines: {
-                  display:false,
-                  color: "#fff",
-                  zeroLineColor: "#fff",
-                  zeroLineWidth: 0
-              },
-              ticks: {
-                  beginAtZero: true,
-                  fontSize:11
-              },
-              stacked: true,
-              display: false,
-              barThickness: 300,
-          }]
-      },
-      legend:{
-          display: false
-      }
-    }
-  }
+  // getBarOptions(){
+  //   return {
+  //     layout:{
+  //       padding: 20
+  //     },
+  //     responsive: true,
+  //     tooltips: {
+  //         enabled: true
+  //     },
+  //     hover :{
+  //         animationDuration:0
+  //     },
+  //     scales: {
+  //         xAxes: [{
+  //             ticks: {
+  //                 beginAtZero:true,
+  //                 fontSize:11
+  //             },
+  //             scaleLabel:{
+  //                 display:false
+  //             },
+  //             gridLines: {
+  //               display:false,
+  //             },
+  //             stacked: true
+  //         }],
+  //         yAxes: [{
+  //             gridLines: {
+  //                 display:false,
+  //                 color: "#fff",
+  //                 zeroLineColor: "#fff",
+  //                 zeroLineWidth: 0
+  //             },
+  //             ticks: {
+  //                 beginAtZero: true,
+  //                 fontSize:11
+  //             },
+  //             stacked: true,
+  //             display: false,
+  //             barThickness: 300,
+  //         }]
+  //     },
+  //     legend:{
+  //         display: false
+  //     }
+  //   }
+  // }
 
   getDefaults(){
 
     this.userRoles = this.rolesService.userRoleDefaults;
 
-    this.userHasCalendar = false;
+    // this.userHasCalendar = false;
 
     this.loading = {
-      charts: true,
+      // charts: true,
       projects: true,
     }
 
-    this.numberOfContracts = {
-      new: 15,
-      renewal: 50
-    };
-
-    this.numberOfInvoices = {
-      fewerThan60Days: 50,
-      fewerThan30Days: 50,
-      sent: 60,
-      late: 15,
-      paidLast30Days: 15,
-    };
+    // this.numberOfContracts = {
+    //   new: 15,
+    //   renewal: 50
+    // };
+    //
+    // this.numberOfInvoices = {
+    //   fewerThan60Days: 50,
+    //   fewerThan30Days: 50,
+    //   sent: 60,
+    //   late: 15,
+    //   paidLast30Days: 15,
+    // };
 
     this.user = {
       userId: "",
@@ -377,9 +376,9 @@ export class AllProjectsPage {
     ];
   }
 
-  openAccountSettings() {
-    this.navCtrl.setRoot('AccountSettingsPage');
-  }
+  // openAccountSettings() {
+  //   this.navCtrl.setRoot('AccountSettingsPage');
+  // }
 
   getIndustries() {
     this.cincoService.getProjectSectors().subscribe(response => {
