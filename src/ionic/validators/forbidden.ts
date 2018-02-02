c8583524bb16307c41a27bf4b4e65beeebf4fad3
zaclittleberry <zlittleberry@linuxfoundation.org>
2018-02-02 18:40:38 -0500

    standardize project save form

---
+++ src/ionic/validators/forbidden.ts
@@ -0,0 +1,9 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+/** A hero's name can't match the given regular expression */
+export function forbiddenValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? {'forbidden': {value: control.value}} : null;
+  };
+}
