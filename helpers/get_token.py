ada0e4119bf85c08d4f265fae0fb51cec784bfab
Edouard Poitras <epoitras@linuxfoundation.org>
2018-02-13 13:52:25 -0500

    Issue #CLA-93 using proper KC endpoint in sandbox
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- helpers/get_token.py
+++ helpers/get_token.py
@@ -9,5 +9,5 @@ kc = KeycloakOpenID(cla.conf['KEYCLOAK_ENDPOINT'],
                     cla.conf['KEYCLOAK_REALM'],
                     cla.conf['KEYCLOAK_CLIENT_SECRET'])
 certs = kc.certs()
-token = kc.token('password', '***REMOVED***', '***REMOVED***')
+token = kc.token('password', '***REMOVED***', '<password-here>') # Password is same as username for sandbox.
 print(token)
