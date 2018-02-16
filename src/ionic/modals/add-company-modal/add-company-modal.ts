067b9e28f2b0ab0d9ac23478fe15eb72b61f3651
zaclittleberry <zlittleberry@linuxfoundation.org>
2018-02-16 11:56:20 -0500

    modify add company modal to also be able to edit company

--- src/ionic/modals/add-company-modal/add-company-modal.ts
+++ src/ionic/modals/add-company-modal/add-company-modal.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 import { NavController, NavParams, ModalController, ViewController, AlertController, IonicPage } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ClaService } from 'cla-service';
+import { ClaCompanyModel } from '../../models/cla-company';
 
 @IonicPage({
   segment: 'add-company-modal'
@@ -16,7 +17,11 @@ export class AddCompanyModal {
   submitAttempt: boolean = false;
   currentlySubmitting: boolean = false;
 
+  mode: string;
+  company: ClaCompanyModel;
+
   constructor(
+    public navParams: NavParams,
     public viewCtrl: ViewController,
     public formBuilder: FormBuilder,
     private claService: ClaService,
@@ -25,8 +30,15 @@ export class AddCompanyModal {
   }
 
   getDefaults() {
+    this.company = this.navParams.get('company');
+    this.mode = this.company
+      ? 'edit'
+      : 'add';
+    let companyName = this.company
+      ? this.company.company_name
+      : '';
     this.form = this.formBuilder.group({
-      name:['', Validators.compose([Validators.required])],
+      name:[companyName, Validators.compose([Validators.required])],
     });
   }
 
@@ -42,6 +54,14 @@ export class AddCompanyModal {
       // prevent submit
       return;
     }
+    if (this.mode === 'add') {
+      this.addCompany();
+    } else {
+      this.updateCompany();
+    }
+  }
+
+  addCompany() {
     let company = {
       company_name: this.form.value.name,
       company_whitelist: [],
@@ -49,18 +69,31 @@ export class AddCompanyModal {
     };
     this.claService.postCompany(company).subscribe(
       (response) => {
-        console.log('response');
-        console.log(response);
         this.currentlySubmitting = false;
         this.dismiss();
       },
       (error) => {
-        console.log('error');
-        console.log(error);
         this.currentlySubmitting = false;
       }
     );
+  }
 
+  updateCompany() {
+    let company = {
+      company_id: this.company.company_id,
+      company_name: this.form.value.name,
+      company_whitelist: this.company.company_whitelist,
+      company_whitelist_patterns: this.company.company_whitelist_patterns,
+    };
+    this.claService.putCompany(company).subscribe(
+      (response) => {
+        this.currentlySubmitting = false;
+        this.dismiss();
+      },
+      (error) => {
+        this.currentlySubmitting = false;
+      }
+    );
   }
 
   dismiss() {
