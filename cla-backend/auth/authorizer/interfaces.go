676b75742dde603e7a8e456f785bcf3ab1123d5f
Runze <runze@twobulls.com>
2018-08-08 10:59:26 -0400

    Add custom authorizer for API gateway. Include execute bin file.

---
+++ cla-backend/auth/authorizer/interfaces.go
@@ -0,0 +1,71 @@
+package authorizer
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+// Interfaces defines an interface to interact with the Interfaces layer
+type Interfaces interface {
+	Handler(context.Context, events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error)
+}
+
+type interfacesContainer struct {
+	usecases Usecases
+}
+
+// NewInterfaces creates an InterfacesInteractor
+func NewInterfaces(usecases Usecases) Interfaces {
+	return &interfacesContainer{
+		usecases: usecases,
+	}
+}
+
+// Handler authorizes a request
+func (ic *interfacesContainer) Handler(
+	ctx context.Context,
+	event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+	token := event.AuthorizationToken
+	parsedToken, err := ic.usecases.ValidateToken(token)
+	if err != nil {
+		fields := map[string]interface{}{}
+		log.Print(fields, err.Error())
+		return events.APIGatewayCustomAuthorizerResponse{}, err
+	}
+
+	return generateAuthResponse(event.MethodArn, &parsedToken), nil
+}
+
+func generateAuthResponse(methodArn string, token *TokenInfo) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: token.Subject}
+
+	authResponse.PolicyDocument = generatePolicy(methodArn)
+	authResponse.Context = map[string]interface{}{
+		"subject":       token.Subject,
+		"email":         token.Email,
+		"emailVerified": token.EmailVerified,
+	}
+	return authResponse
+}
+
+func generatePolicy(methodARN string) events.APIGatewayCustomAuthorizerPolicy {
+	resource := getWildcardFunctionARN(methodARN)
+	return events.APIGatewayCustomAuthorizerPolicy{
+		Version: "2012-10-17",
+		Statement: []events.IAMPolicyStatement{
+			{
+				Action:   []string{"execute-api:Invoke"},
+				Effect:   "Allow",
+				Resource: []string{resource},
+			},
+		},
+	}
+}
+
+func getWildcardFunctionARN(methodARN string) string {
+	paths := strings.Split(methodARN, "/")
+	return paths[0] + "/" + paths[1] + "/*"
+}
---
+++ cla-backend/auth/authorizer/interfaces_test.go
@@ -0,0 +1,52 @@
+package authorizer
+
+import (
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateAuthResponseHasContext(t *testing.T) {
+	token := TokenInfo{
+		Email:         "user@example.com",
+		EmailVerified: true,
+		Subject:       "google-oauth2|1111111111",
+	}
+	methodArn := "arn:aws:execute-api:us-east-1:xxxxx:xxxxx/stage/some-method"
+	result := generateAuthResponse(methodArn, &token)
+	expected := map[string]interface{}{
+		"subject":       token.Subject,
+		"email":         token.Email,
+		"emailVerified": token.EmailVerified,
+	}
+	assert.Equal(t, expected, result.Context)
+}
+
+func TestGenerateAuthResponseHasPrincipleID(t *testing.T) {
+	token := TokenInfo{
+		Email:         "user@example.com",
+		EmailVerified: true,
+		Subject:       "google-oauth2|1111111111",
+	}
+	methodArn := "arn:aws:execute-api:us-east-1:xxxxx:xxxxx/stage/some-method"
+	result := generateAuthResponse(methodArn, &token)
+	expected := "google-oauth2|1111111111"
+	assert.Equal(t, expected, result.PrincipalID)
+}
+
+func TestGeneratePolicy(t *testing.T) {
+	methodArn := "arn:aws:execute-api:us-east-1:xxxxx:xxxxx/stage/some-method"
+	result := generatePolicy(methodArn)
+	expected := events.APIGatewayCustomAuthorizerPolicy{
+		Version: "2012-10-17",
+		Statement: []events.IAMPolicyStatement{
+			{
+				Action:   []string{"execute-api:Invoke"},
+				Effect:   "Allow",
+				Resource: []string{"arn:aws:execute-api:us-east-1:xxxxx:xxxxx/stage/*"},
+			},
+		},
+	}
+	assert.Equal(t, expected, result)
+}
