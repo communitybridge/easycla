3bec14d379abacb92fc253a12d63e4a255930ae3
Runze <runze@twobulls.com>
2018-08-13 14:57:19 -0400

    Create auth service with Auth0. Swith keycloak service with Auth0 service. But haven't fully swapped.

---
+++ frontend-project-management-console/src/ionic/services/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: "51e1ydDRWNcG63eS0XRbkVXj6gR2mjl3",
+  domain: "***REMOVED***.auth0.com",
+  callbackURL: "http://localhost:8100/auth"
+};
