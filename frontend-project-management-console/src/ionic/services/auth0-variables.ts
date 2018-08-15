e3b4b6b2b845e2665bf2e8856d94626d75dab33e
Runze <runze@twobulls.com>
2018-08-15 15:26:23 -0400

    Update user role service with Auth0. Phase out keycloak.

--- frontend-project-management-console/src/ionic/services/auth0-variables.ts
+++ frontend-project-management-console/src/ionic/services/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: "51e1ydDRWNcG63eS0XRbkVXj6gR2mjl3",
   domain: "***REMOVED***.auth0.com",
-  callbackURL: "http://localhost:8100/auth"
+  callbackURL: "http://localhost:8100/#AllProjectsPage"
 };
