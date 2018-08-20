1dca4dc556899e7fa7dc5f8f3b4e2a22071cceeb
Runze <runze@twobulls.com>
2018-08-20 11:45:05 -0400

    Switch docraptor to twobulls dev created by Ernest.

--- cla-backend/cla/models/docraptor_models.py
+++ cla-backend/cla/models/docraptor_models.py
@@ -15,12 +15,12 @@ class DocRaptor(PDFService):
         self.javascript = False
 
     def initialize(self, config):
-        self.api_key = config['DOCRAPTOR_API_KEY']
+        self.api_key = "zIHdSVf0zOh9bQPBGxo"
         docraptor.configuration.username = self.api_key
-        self.debug_mode = config['DOCRAPTOR_DEBUG_MODE']
+        self.debug_mode = True
         docraptor.configuration.debug = self.debug_mode
-        self.test_mode = config['DOCRAPTOR_TEST_MODE']
-        self.javascript = config['DOCRAPTOR_JAVASCRIPT']
+        self.test_mode = True
+        self.javascript = True
 
     def generate(self, content, external_resource=False):
         doc_api = docraptor.DocApi()
