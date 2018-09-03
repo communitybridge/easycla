2c54c39afafe3ab37dd98e230e3b33c47bc14cea
Runze <runze@twobulls.com>
2018-09-03 15:29:30 -0400

    Generate auth page

---
+++ frontend-project-management-console/src/ionic/pages/auth/auth.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the AuthPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+ @IonicPage({
+   name: "AuthPage",
+   segment: "auth"
+ })
+
+ @Component({
+  selector: 'page-auth',
+  templateUrl: 'auth.html',
+})
+export class AuthPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AuthPage');
+  }
+
+}
