b1423b97995440c6e20275bb3ea918c8aac9020f
Runze <runze@twobulls.com>
2018-09-04 15:30:21 -0400

    Clean up Login URL path
    - Remove nav parameter passing to login page when redirecting

--- frontend-project-management-console/src/ionic/pages/login/login.ts
+++ frontend-project-management-console/src/ionic/pages/login/login.ts
@@ -6,7 +6,7 @@ import { AuthService } from "../../services/auth.service";
 
 @IonicPage({
   name: "LoginPage",
-  segment: "login/:return"
+  segment: "login"
 })
 @Component({
   selector: "login",
@@ -25,43 +25,36 @@ export class LoginPage {
     public rolesService: RolesService,
     public authService: AuthService
   ) {
-    let dataString = this.navParams.get("return");
-    try {
-      this.data = JSON.parse(dataString);
-      this.returnData = true;
-    } catch (e) {
-      this.returnData = false;
-    }
+    // let dataString = this.navParams.get("return");
+    // try {
+    //   this.data = JSON.parse(dataString);
+    //   this.returnData = true;
+    // } catch (e) {
+      // this.returnData = false;
+    // }
     this.userRoles = this.rolesService.userRoles;
     this.rolesService.getUserRolesPromise().then(userRoles => {
       this.userRoles = userRoles;
-      this.checkPageReturn();
+      this.canAccess = this.hasAccess();
+      // this.checkPageReturn();
     });
   }
 
-  checkPageReturn() {
-    this.canAccess = this.hasAccess();
-    if (this.canAccess && this.returnData) {
-      if (this.data.page) {
-        if (this.data.params) {
-          this.navCtrl.setRoot(this.data.page, this.data.params);
-        } else {
-          this.navCtrl.setRoot(this.data.page);
-        }
-      }
-    }
-  }
+  // checkPageReturn() {
+  //   this.canAccess = this.hasAccess();
+  //   if (this.canAccess && this.returnData) {
+  //     if (this.data.page) {
+  //       if (this.data.params) {
+  //         this.navCtrl.setRoot(this.data.page, this.data.params);
+  //       } else {
+  //         this.navCtrl.setRoot(this.data.page);
+  //       }
+  //     }
+  //   }
+  // }
 
   hasAccess() {
-    console.log("check if hasAccess");
-    if (this.data && this.data.roles) {
-      for (let role of this.data.roles) {
-        if (!this.userRoles[role]) {
-          return false;
-        }
-      }
-    }
-    return true;
+    return this.userRoles.isAuthenticated && this.userRoles.isPmcUser;
   }
 
   login() {
