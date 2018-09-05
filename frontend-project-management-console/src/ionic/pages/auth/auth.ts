81524ac0a6ad92e4d6fff1c43023cc704144124e
Runze <runze@twobulls.com>
2018-09-04 21:08:25 -0400

    Make Auth page as entry page

--- frontend-project-management-console/src/ionic/pages/auth/auth.ts
+++ frontend-project-management-console/src/ionic/pages/auth/auth.ts
@@ -8,11 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
- @IonicPage({
-   name: "AuthPage",
-   segment: "auth"
- })
-
  @Component({
   selector: 'page-auth',
   templateUrl: 'auth.html',
