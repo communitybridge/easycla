72d7a9d5ed18d74876b5043cee70ca4bc0b756ca
Runze <runze@twobulls.com>
2018-09-10 12:53:04 -0400

    Route user based on authentication state from AuthPage
    1. rewrite roles.service to check user role
    2. check auth state and route user at AuthPage

--- cla-frontend-corporate-console/src/ionic/services/roles.service.ts
+++ cla-frontend-corporate-console/src/ionic/services/roles.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { KeycloakService } from './keycloak/keycloak.service';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { KeycloakService } from "./keycloak/keycloak.service";
+import { AuthService } from "./auth.service";
 
 @Injectable()
 export class RolesService {
-
   public userAuthenticated: boolean;
   public userRoleDefaults: any;
   public userRoles: any;
@@ -12,19 +12,24 @@ export class RolesService {
   public getData: any;
   private rolesFetched: boolean;
 
-  constructor (
+  private LF_CUSTOM_CLAIM = "https://sso.linuxfoundation.org/claims/roles";
+  private CLA_PROJECT_ADMIN = "cla-system-admin";
+  private projectSet = new Set(["cla-admin-project-mvp"]); // Here we may need to generate this array from Salesforce API ??
+
+  constructor(
     private keycloak: KeycloakService,
+    private authService: AuthService
   ) {
     this.rolesFetched = false;
     this.userRoleDefaults = {
-      isAuthenticated: this.keycloak.authenticated(),
+      isAuthenticated: this.authService.isAuthenticated(),
       isPmcUser: false,
       isStaffInc: false,
       isDirectorInc: false,
       isStaffDirect: false,
       isDirectorDirect: false,
       isExec: false,
-      isAdmin: false,
+      isAdmin: false
     };
     this.userRoles = this.userRoleDefaults;
   }
@@ -32,14 +37,49 @@ export class RolesService {
   //////////////////////////////////////////////////////////////////////////////
 
   /**
-  * This service should ONLY contain methods for user roles
-  **/
+   * This service should ONLY contain methods for user roles
+   **/
 
   //////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
 
-  isInArray(roles, role) {
-    for(let i=0; i<roles.length; i++) {
+  getUserRolesPromise() {
+    console.log("Get UserRole Promise.");
+    if (this.authService.isAuthenticated()) {
+      return this.authService
+        .getIdToken()
+        .then(token => {
+          return this.authService.parseIdToken(token);
+        })
+        .then(tokenParsed => {
+          if (tokenParsed && tokenParsed[this.LF_CUSTOM_CLAIM]) {
+            let customRules = tokenParsed[this.LF_CUSTOM_CLAIM];
+            this.userRoles = {
+              isAuthenticated: this.authService.isAuthenticated(),
+              isPmcUser: this.isInProjectSet(customRules, this.projectSet),
+              isStaffInc: false,
+              isDirectorInc: false,
+              isStaffDirect: false,
+              isDirectorDirect: false,
+              isExec: false,
+              isAdmin: this.isInArray(customRules, this.CLA_PROJECT_ADMIN)
+            };
+            console.log(this.userRoles);
+            return this.userRoles;
+          }
+          return this.userRoleDefaults;
+        })
+        .catch(error => {
+          return Promise.resolve(this.userRoleDefaults);
+        });
+    } else {
+      // not authenticated. can't decode token. just return defaults
+      return Promise.resolve(this.userRoleDefaults);
+    }
+  }
+
+  private isInArray(roles, role) {
+    for (let i = 0; i < roles.length; i++) {
       if (roles[i].toLowerCase() === role.toLowerCase()) {
         return true;
       }
@@ -47,29 +87,14 @@ export class RolesService {
     return false;
   }
 
-  getUserRolesPromise() {
-    if (this.keycloak.authenticated()) {
-      return this.keycloak.getTokenParsed().then((tokenParsed) => {
-        if (tokenParsed && tokenParsed.realm_access && tokenParsed.realm_access.roles) {
-          this.userRoles = {
-            isAuthenticated: this.keycloak.authenticated(),
-            isPmcUser: this.isInArray(tokenParsed.realm_access.roles, 'PMC_LOGIN'),
-            isStaffInc: this.isInArray(tokenParsed.realm_access.roles, 'STAFF_STAFF_INC'),
-            isDirectorInc: this.isInArray(tokenParsed.realm_access.roles, 'STAFF_DIRECTOR_INC'),
-            isStaffDirect: this.isInArray(tokenParsed.realm_access.roles, 'STAFF_STAFF_DIRECT'),
-            isDirectorDirect: this.isInArray(tokenParsed.realm_access.roles, 'STAFF_DIRECTOR_DIRECT'),
-            isExec: this.isInArray(tokenParsed.realm_access.roles, 'STAFF_EXEC'),
-            isAdmin: this.isInArray(tokenParsed.realm_access.roles, 'STAFF_SUPER_ADMIN'),
-          };
-          return this.userRoles;
-        }
-        return this.userRoleDefaults;
-      });
-    } else { // not authenticated. can't decode token. just return defaults
-      return Promise.resolve(this.userRoleDefaults);
+  private isInProjectSet(roles, projectSet) {
+    for (let i = 0; i < roles.length; i++) {
+      if (projectSet.has(roles[i])) {
+        return true;
+      }
     }
+    return false;
   }
 
   //////////////////////////////////////////////////////////////////////////////
-
 }
