193f266be052eee035d5243450ad3eb7dd17d20b
sruti <sruti@twobulls.com>
2018-09-26 12:28:08 -0400

    Add stages in config file. Cleanup params.

--- cla-backend/helpers/get_token.py
+++ cla-backend/helpers/get_token.py
@@ -4,14 +4,14 @@ sys.path.append('../')
 from keycloak import KeycloakOpenID
 import cla
 
-kc = KeycloakOpenID(cla.conf['KEYCLOAK_ENDPOINT'],
-                    cla.conf['KEYCLOAK_CLIENT_ID'],
-                    cla.conf['KEYCLOAK_REALM'],
-                    cla.conf['KEYCLOAK_CLIENT_SECRET'])
-certs = kc.certs()
-token = kc.token('password', '***REMOVED***', '***REMOVED***') # Password is same as username for sandbox.
-print(token)
-print(kc.decode_token(token['access_token'], certs))
-token = kc.token('client_credentials')
-print(token)
-print(kc.decode_token(token['access_token'], certs))
+# kc = KeycloakOpenID(cla.conf['KEYCLOAK_ENDPOINT'],
+#                     cla.conf['KEYCLOAK_CLIENT_ID'],
+#                     cla.conf['KEYCLOAK_REALM'],
+#                     cla.conf['KEYCLOAK_CLIENT_SECRET'])
+# certs = kc.certs()
+# token = kc.token('password', '***REMOVED***', '***REMOVED***') # Password is same as username for sandbox.
+# print(token)
+# print(kc.decode_token(token['access_token'], certs))
+# token = kc.token('client_credentials')
+# print(token)
+# print(kc.decode_token(token['access_token'], certs))
