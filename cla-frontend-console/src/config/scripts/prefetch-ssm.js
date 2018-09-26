eb758a7582941b75773517a3821aaedf72dea745
Runze <runze@twobulls.com>
2018-09-26 15:22:53 -0400

    Redirect to corporate console if not company matched
    - Provision corporate console per stage from SSM
    - Change rediect link to corp console at cla-new-company-modal
    - Give more information when landing  cla-new-company-modal

--- cla-frontend-console/src/config/scripts/prefetch-ssm.js
+++ cla-frontend-console/src/config/scripts/prefetch-ssm.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const RetrieveSSMValues = require('./read-ssm');
-const configVarArray = ['auth0-clientId', 'auth0-domain', 'cla-api-url'];
+const configVarArray = ['auth0-clientId', 'auth0-domain', 'cla-api-url', 'corp-console-link'];
 const region = 'us-east-1';
 const profile = 'lf-cla';
 const stageEnv = process.env.STAGE_ENV;
