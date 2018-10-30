35b418c00934b12dafdfa2ddb0b4a0e74a6e5395
Danny Suarez <dsuarezlogans@gmail.com>
2018-10-30 14:49:35 -0500

    added aws profile creation on install:dev

--- cla-backend/dev.sh
+++ cla-backend/dev.sh
@@ -2,31 +2,35 @@
 
 if [ $1 = 'install' ]; then
   echo '======> installing npm dependencies..'
-  npm install &&\
+    npm install &&\
+    npm i -g serverless &&\
   echo '======> installing python virtualenv..'
-  pip3 install virtualenv &&\
+    pip3 install virtualenv &&\
   echo '======> creating virtual enviroment..'
-  virtualenv ~/.env/lf-cla &&\
+    virtualenv ~/.env/lf-cla &&\
   echo '======> activating virtual enviroment'
-  . ~/.env/lf-cla/bin/activate &&\
+    . ~/.env/lf-cla/bin/activate &&\
   echo '======> installing python dependencies..'
-  pip install -r requirements.txt
-  cat cla/config.py > cla_config.py
+    pip install -r requirements.txt
+    cat cla/config.py > cla_config.py
+  echo '======> setting up aws profile..'
+    cd .. &&\
+    serverless config credentials --provider aws --profile lf-cla --key ' ' --secret ' ' -s devS
   echo '======> installation has done, now run `npm run start:dev` to run local dev enviroment'
 
 elif [ $1 = 'start:lambda' ]; then
   echo '======> activating virtual enviroment'
-  . ~/.env/lf-cla/bin/activate &&\
+    . ~/.env/lf-cla/bin/activate &&\
   echo '======> running local lambda server'
-  sls wsgi serve -s dev
+    sls wsgi serve -s dev
 
 elif [ $1 = 'start:dynamodb' ]; then
   echo '======> running local dynamodb server'
-  sls dynamodb start -s dev
+    sls dynamodb start -s dev
 
 elif [ $1 = 'start:s3' ]; then
   echo '======> running local s3 server'
-  sls s3 start -s dev
+    sls s3 start -s dev
   
 else
   echo "option not valid"
