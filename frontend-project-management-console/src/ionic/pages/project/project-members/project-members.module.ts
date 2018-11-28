917f38287501d073eace215dc636cf9487df7fe4
Luis Suarez <lsuarez@cofebe.com>
2018-11-28 18:54:10 -0500

    WIP 284

--- frontend-project-management-console/src/ionic/pages/project/project-members/project-members.module.ts
+++ frontend-project-management-console/src/ionic/pages/project/project-members/project-members.module.ts
@@ -4,7 +4,7 @@ import { ProjectMembersPage } from './project-members';
 import { LoadingSpinnerComponentModule } from '../../../components/loading-spinner/loading-spinner.module';
 import { LoadingDisplayDirectiveModule } from '../../../directives/loading-display/loading-display.module';
 import { SortingDisplayComponentModule } from '../../../components/sorting-display/sorting-display.module';
-import { ProjectHeaderComponentModule } from '../../../components/project-header/project-header.module';
+import { ProjectHeaderComponentModule } from '../../../components/section-header/section-header.module';
 import { ProjectNavigationComponentModule } from '../../../components/project-navigation/project-navigation.module';
 
 @NgModule({
