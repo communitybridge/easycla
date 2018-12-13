771d21bd70fa8cb361a404a137462e009450bba3
Charlie Oh <mun5424@gmail.com>
2018-12-13 15:09:24 -0800

    add login authentication. needs to configure so that auth only is for gerrit

--- cla-frontend-console/src/ionic/app/app.module.ts
+++ cla-frontend-console/src/ionic/app/app.module.ts
@@ -6,19 +6,26 @@ import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { ClaService } from "../services/cla.service";
+import { AuthService } from "../services/auth.service";
+import { RolesService } from "../services/roles.service";
+import { HttpClient } from "../services/http-client";
+import { AuthPage } from "../pages/auth/auth";
 import { MyApp } from "./app.component";
 
 @NgModule({
-  declarations: [MyApp],
+  declarations: [MyApp, AuthPage],
   imports: [BrowserModule, HttpModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp],
+  entryComponents: [MyApp, AuthPage],
   providers: [
     StatusBar,
     SplashScreen,
     CurrencyPipe,
+    HttpClient,
     ClaService,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    AuthService,
+    RolesService,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
   ]
 })
 export class AppModule {}
