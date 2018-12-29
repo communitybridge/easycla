ac2b9f78dea39959fb9a1ee491957fc3d0aa721a
Charlie Oh <mun5424@gmail.com>
2018-12-28 16:45:38 -0800

    added UI Components for Gerrit Instance addition. adjusted endpoints for error handling

---
+++ frontend-project-management-console/src/ionic/modals/cla-gerrit-modal/cla-gerrit-modal.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ClaGerritModal } from './cla-gerrit-modal';
+import { ModalHeaderComponentModule } from "../../components/modal-header/modal-header.module";
+
+@NgModule({
+  declarations: [
+    ClaGerritModal
+  ],
+  imports: [
+    ModalHeaderComponentModule,
+    IonicPageModule.forChild(ClaGerritModal)
+  ],
+  entryComponents: [
+    ClaGerritModal
+  ]
+})
+export class ClaGerritModalModule {}
