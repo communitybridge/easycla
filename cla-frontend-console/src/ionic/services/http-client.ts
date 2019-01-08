c3058bcb9fa2f53f40f9467ae9d1a3a998fdcde7
Charlie Oh <mun5424@gmail.com>
2019-01-07 17:00:14 -0800

    secured endpoint for retrieving userIds for Gerrit Users

--- cla-frontend-console/src/ionic/services/http-client.ts
+++ cla-frontend-console/src/ionic/services/http-client.ts
@@ -1,21 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { KeycloakService } from './keycloak/keycloak.service';
 import { Observable } from 'rxjs/Rx';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class HttpClient {
 
   constructor(
-    private http: Http,
-  ) {
+    public http: Http,
+    private keycloak: KeycloakService,
+    private authService: AuthService) {}
+
+  buildAuthHeaders(contentType: string = 'application/json') {
+    let headers = new Headers({
+      'Accept': 'application/json',
+      'Content-Type': contentType
+    });
+
+    if (this.authService.isAuthenticated()) {
+      return this.authService.getIdToken().then(
+        (token) => {
+          if (token){
+            headers.append('Authorization', 'Bearer ' + token);
+            return headers;
+          }
+        }
+      );
+    } else {
+      return Promise.resolve(headers);
+    }
 
   }
 
-  public setHttp(http: Http) {
+  setHttp(http: Http) {
     this.http = http; // allow alternate http library
   }
 
-  private buildHeaders(contentType: string = 'application/json') {
+  buildHeaders(contentType: string = 'application/json') {
     let headers = new Headers({
       'Accept': 'application/json',
       'Content-Type': contentType
@@ -51,6 +73,37 @@ export class HttpClient {
   delete(url) {
     return Observable
         .fromPromise(this.buildHeaders())
+        .switchMap((headers) => this.http.delete(url, { headers: headers }));
+  }
+
+
+  securedGet(url) {
+    return Observable
+        .fromPromise(this.buildAuthHeaders())
+        .switchMap((headers) => this.http.get(url, { headers: headers }));
+  }
+
+  securedPost(url, data) {
+    return Observable
+        .fromPromise(this.buildAuthHeaders())
+        .switchMap((headers) => this.http.post(url, data, { headers: headers }));
+  }
+
+  securedPut(url, data, contentType: string = 'application/json') {
+    return Observable
+        .fromPromise(this.buildAuthHeaders(contentType))
+        .switchMap((headers) => this.http.put(url, data, { headers: headers }));
+  }
+
+  securedPatch(url, data, contentType: string = 'application/json') {
+    return Observable
+        .fromPromise(this.buildAuthHeaders(contentType))
+        .switchMap((headers) => this.http.patch(url, data, { headers: headers }));
+  }
+
+  securedDelete(url) {
+    return Observable
+        .fromPromise(this.buildAuthHeaders())
         .switchMap((headers) => this.http.delete(url, { headers: headers }));
   }
 
