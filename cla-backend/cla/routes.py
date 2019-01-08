3fa4849febdf376d81f610d6e2f55e11d0a076ab
James Heller <jaheller5@gmail.com>
2019-01-07 21:50:04 -0800

    Add Tungsten Fabric CCLA

--- cla-backend/cla/routes.py
+++ cla-backend/cla/routes.py
@@ -859,7 +859,11 @@ def post_project_document_template(user: cla_user,
                                    document_name: hug.types.text,
                                    document_preamble: hug.types.text,
                                    document_legal_entity_name: hug.types.text,
-                                   template_name: hug.types.one_of(['CNCFTemplate', 'OpenBMCTemplate']),
+                                   template_name: hug.types.one_of([
+                                       'CNCFTemplate',
+                                       'OpenBMCTemplate',
+                                       'TungstenFabricTemplate'
+                                    ]),
                                    new_major_version=None):
     """
     POST: /project/{project_id}/document/template/{document_type}
