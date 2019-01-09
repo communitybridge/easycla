22639bd39b85462c8cb1f618780ac23a620c171b
James Heller <jaheller5@gmail.com>
2019-01-09 00:58:46 -0800

    Add LF LDAP vars to ssm. Update LFGroup()

--- cla-backend/cla/controllers/lf_group.py
+++ cla-backend/cla/controllers/lf_group.py
@@ -4,19 +4,25 @@ import json
 class LFGroup():
     def __init__(self, lf_base_url, client_id, client_secret, refresh_token):
         self.lf_base_url = lf_base_url
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.refresh_token = refresh_token
+
+    def _get_access_token(self):
         data = {
             'grant_type': 'refresh_token',
-            'refresh_token': refresh_token,
+            'refresh_token': self.refresh_token,
             'scope': 'manage_groups'
         }
         oauth_url = self.lf_base_url + "oauth2/token"
-        response = requests.post(oauth_url, data=data, auth=(client_id, client_secret)).json()
-        self.access_token = response['access_token']
-    
+        response = requests.post(oauth_url, data=data, auth=(self.client_id, self.client_secret)).json()
+
+        return response['access_token']
 
     # get LDAP group 
     def get_group(self, group_id):
-        headers = { 'Authorization': 'bearer ' + self.access_token } 
+        access_token = self._get_access_token()
+        headers = { 'Authorization': 'bearer ' + access_token } 
         response = requests.get(self.lf_base_url + 'rest/auth0/og/' + str(group_id), headers=headers)
         if response.status_code == 200:
             return response.json()
@@ -25,8 +31,9 @@ class LFGroup():
 
     # add user to LDAP group
     def add_user_to_group(self, group_id, username): 
+        access_token = self._get_access_token()
         headers = {
-            'Authorization': 'bearer ' + self.access_token,
+            'Authorization': 'bearer ' + access_token,
             'Content-Type': 'application/json',
             'cache-control': 'no-cache',
         }
