e3a3ce31e4b696c4ea544d45d1399c179dfa6246
James Heller <jaheller5@gmail.com>
2019-01-12 17:24:55 -0800

    Fix Gerrit creation
    Fixed issues where the LDAP client threw errors if unexpected credentials are
    provided. Fixed issues creating a Gerrit. Updated get_gerrit_by_project_id()
    to only return gerrits for a particular project.

--- cla-backend/cla/controllers/gerrit.py
+++ cla-backend/cla/controllers/gerrit.py
@@ -6,7 +6,7 @@ import uuid
 import cla.hug_types
 import os
 import cla 
-from cla.utils import get_gerrit_instance
+from cla.models.dynamo_models import Gerrit
 from cla.models import DoesNotExist
 from cla.controllers.lf_group import LFGroup
 
@@ -17,23 +17,26 @@ lf_group_refresh_token = os.environ.get('LF_GROUP_REFRESH_TOKEN', '')
 lf_group = LFGroup(lf_group_client_url, lf_group_client_id, lf_group_client_secret, lf_group_refresh_token)
 
 def get_gerrit_by_project_id(project_id):
-    gerrit = get_gerrit_instance()
+    gerrit = Gerrit()
     try:
         gerrits = gerrit.get_gerrit_by_project_id(project_id)
-    except DoesNotExist as err:
-        return {'errors': {'a gerrit instance does not exist with the given project ID. ': str(err)}}
+    except DoesNotExist:
+        return []
+    except Exception as e:
+        return {'errors': {'a gerrit instance does not exist with the given project ID. ': str(e)}}
 
     if gerrits is None:
         return []
 
-    return [gerrit.to_dict() for gerrit in get_gerrit_instance().all()] 
+    return [gerrit.to_dict() for gerrit in gerrits]
 
 def get_gerrit(gerrit_id):
-    gerrit = get_gerrit_instance()
+    gerrit = Gerrit()
     try:
         gerrit.load(gerrit_id)
     except DoesNotExist as err:
         return {'errors': {'a gerrit instance does not exist with the given Gerrit ID. ': str(err)}}
+
     return gerrit.to_dict()
 
 
@@ -56,29 +59,34 @@ def create_gerrit(project_id,
     :param group_id_ccla: The id of the LDAP group for CCLA. 
     :type group_id_ccla: string
     """
-    
-    gerrit = get_gerrit_instance()
+
+    gerrit = Gerrit()
+
+    # Check if ICLA exists
+    if group_id_icla is not None:
+        ldap_group_icla = lf_group.get_group(group_id_icla)
+        if ldap_group_icla.get('error') is not None:
+            return {'error_icla': 'The specified LDAP group for ICLA does not exist. '}
+
+        gerrit.set_group_name_icla(ldap_group_icla.get('title'))
+        gerrit.set_group_id_icla(str(group_id_icla))
+
+    # Check if CCLA exists
+    if group_id_ccla is not None:
+        ldap_group_ccla = lf_group.get_group(group_id_ccla)
+        if ldap_group_ccla.get('error') is not None:
+            return {'error_ccla': 'The specified LDAP group for CCLA does not exist. '}
+        gerrit.set_group_name_ccla(ldap_group_ccla.get('title'))
+        gerrit.set_group_id_ccla(str(group_id_ccla))
+
+    # Save Gerrit Instance
     gerrit.set_gerrit_id(str(uuid.uuid4()))
     gerrit.set_project_id(str(project_id))
     gerrit.set_gerrit_url(gerrit_url)
     gerrit.set_gerrit_name(gerrit_name)
-    
-    #check if LDAP group exists     
-    # returns 'error' if the LDAP group does not exist
-    ldap_group_icla = lf_group.get_group(group_id_icla)
-    if ldap_group_icla.get('error') is not None:
-        return {'error_icla': 'The specified LDAP group for ICLA does not exist. '}
-    gerrit.set_group_name_icla(ldap_group_icla.get('title'))
-    gerrit.set_group_id_icla(str(group_id_icla))
-
-    ldap_group_ccla = lf_group.get_group(group_id_ccla)
-    if ldap_group_ccla.get('error') is not None:
-        return {'error_ccla': 'The specified LDAP group for CCLA does not exist. '}
-    gerrit.set_group_name_ccla(ldap_group_ccla.get('title'))
-    gerrit.set_group_id_ccla(str(group_id_ccla))
-
     gerrit.save()
-    return [gerrit.to_dict()]
+
+    return gerrit.to_dict()
 
 def delete_gerrit(gerrit_id):
     """
@@ -86,7 +94,7 @@ def delete_gerrit(gerrit_id):
 
     :param gerrit_id: The ID of the gerrit instance.
     """
-    gerrit = get_gerrit_instance()
+    gerrit = Gerrit()
     try:
         gerrit.load(str(gerrit_id))
     except DoesNotExist as err:
