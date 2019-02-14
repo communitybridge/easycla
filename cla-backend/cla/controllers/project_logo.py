e789ca68e17dfd9a1090f5ed8fe85da29f96a813
James Heller <jaheller5@gmail.com>
2019-02-14 11:46:36 -0800

    Generate signed upload url for logo upload

--- cla-backend/cla/controllers/project_logo.py
+++ cla-backend/cla/controllers/project_logo.py
@@ -4,24 +4,29 @@ from urllib.parse import urlsplit, urljoin
 import boto3
 
 import cla
-from cla.models.s3_storage import S3Storage
 from cla.auth import AuthUser, admin_list
 
 cla_logo_url = os.environ.get('CLA_BUCKET_LOGO_URL', '')
 logo_bucket_parts = urlsplit(cla_logo_url)
 logo_bucket = logo_bucket_parts.path.replace('/', '')
 
-s3_client = S3Storage()
-s3_client.initialize(None, bucket_name=logo_bucket)
+s3_client = boto3.client('s3')
 
-def upload_logo(auth_user: AuthUser, project_sfdc_id: str, data):
+def create_signed_logo_url(auth_user: AuthUser, project_sfdc_id: str):
     if auth_user.username not in admin_list:
         return {'error': 'unauthorized'}
 
-    cla.log.info('logo ({}) uploaded by {}'.format(project_sfdc_id, auth_user.username))
+    cla.log.info('signed url ({}) created by {}'.format(project_sfdc_id, auth_user.username))
 
     file_path = '{}.png'.format(project_sfdc_id)
+
+    params = {
+        'Bucket': logo_bucket,
+        'Key': file_path
+    }
+
     try:
-        s3_client.store(file_path, data)
+        signed_url = s3_client.generate_presigned_url('put_object', Params=params, ExpiresIn=100)
+        return {'signed_url': signed_url}
     except Exception as err:
         return {'error': err}
