a45ef8c30a4b9be91182e31fbd686909c88b829e
Jordano Moscoso <jor_notes@outlook.com>
2019-03-13 16:31:06 -0500

    Adds api routes for adding and removing company managers.

--- cla-backend/cla/routes.py
+++ cla-backend/cla/routes.py
@@ -597,6 +597,30 @@ def get_company(company_id: hug.types.text):
     """
     return cla.controllers.company.get_company(company_id)
 
+@hug.post('/company/{company_id}/manager', versions=1,
+          examples=" - {'lfid': 'user_lfid'}")
+def post_company_manager(auth_user: check_auth, 
+                         company_id: hug.types.text,
+                         lfid: hug.types.text):
+    """
+    GET: /company/{company_id}/add_manager
+
+    Returns the CLA company with the new manager added
+    """
+    return cla.controllers.company.add_manager(auth_user.username, company_id, lfid)
+
+@hug.delete('/company/{company_id}/manager', versions=1,
+          examples=" - {'lfid': 'user_lfid'}")
+def delete_company_manager(auth_user: check_auth, 
+                         company_id: hug.types.text,
+                         lfid: hug.types.text):
+    """
+    GET: /company/{company_id}/manager
+
+    Returns the CLA company with the new manager removed
+    """
+    return cla.controllers.company.remove_manager(auth_user.username, company_id, lfid)
+
 @hug.get('/company/{company_id}/project/unsigned', versions=1)
 def get_unsigned_projects_for_company(company_id: hug.types.text):
     """
