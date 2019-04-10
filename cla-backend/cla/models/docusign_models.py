45061fde7a35c5bf764f5b711aabbf6dee074bbd
Charlie Oh <mun5424@gmail.com>
2019-04-10 11:43:28 -0700

    remove space

--- cla-backend/cla/models/docusign_models.py
+++ cla-backend/cla/models/docusign_models.py
@@ -383,7 +383,7 @@ class DocuSign(signing_service_interface.SigningService):
         # Set signature ACL (user already validated in 'check_and_prepare_employee_signature')
         user = User()
         user.load(str(user_id))
-        new_signature.set_signature_acl('github: {}'.format(user.get_user_github_id()))
+        new_signature.set_signature_acl('github:{}'.format(user.get_user_github_id()))
 
         # Save signature
         new_signature.save()
