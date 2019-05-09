13a02e9ed2de849ea7ef0ca25a758efffc0c92a8
Charlie Oh <mun5424@gmail.com>
2019-05-09 16:41:57 -0700

    use swagger to generate template properties

--- cla-backend-go/template/handlers.go
+++ cla-backend-go/template/handlers.go
@@ -3,19 +3,20 @@ package template
 import (
 	"github.com/LF-Engineering/cla-monorepo/cla-backend-go/gen/models"
 	"github.com/LF-Engineering/cla-monorepo/cla-backend-go/gen/restapi/operations"
+	"github.com/LF-Engineering/cla-monorepo/cla-backend-go/gen/restapi/operations/template"
 
 	"github.com/go-openapi/runtime/middleware"
 )
 
-func Configure(api *operations.ClaAPI, service Service) {
+func Configure(api *operations.ClaAPI, service service) {
 	// Retrieve a list of available templates
-	api.TemplateGetTemplateHandler = template.GetTemplateHandlerFunc(func(template template.GetTemplateParams) middleware.Responder {
+	api.TemplateGetTemplateHandler = template.GetTemplateHandlerFunc(func(template template.NewGetTemplatesParams) middleware.Responder {
 
 		template, err := service.getTemplate(template.HTTPRequest.Context(), params)
 		if err != nil {
 			return template.NewGetTemplateBadRequest().WithPayload(errorResponse(err))
 		}
-
+		NewGetTemplatesParams
 		return template.NewGetTemplateOK().WithPayload(template)
 	})
 }
