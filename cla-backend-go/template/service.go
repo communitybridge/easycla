b7141f18a39f25402ab89b149fc30641fa3b84bf
Jarrett Dixon <jarrett@cofebe.com>
2019-05-14 15:14:02 -0700

    Added post endpoint for clagroup template api

--- cla-backend-go/template/service.go
+++ cla-backend-go/template/service.go
@@ -2,6 +2,7 @@ package template
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/LF-Engineering/cla-monorepo/cla-backend-go/gen/models"
 )
@@ -18,6 +19,15 @@ func NewService(templateRepo Repository) service {
 	return service{
 		templateRepo: templateRepo,
 	}
+}
+
+func (s service) CreateCLAGroupTemplate(ctx context.Context, claGroupID string, claGroupFields *models.CreateClaTemplateGroup) error {
+	// template, err := s.templateRepo.CreateCLAGroupTemplate(ctx)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	fmt.Println("CreateCLAGroupTemplate Method")
+	return nil
 }
 
 func (s service) GetTemplates(ctx context.Context) ([]models.Template, error) {
