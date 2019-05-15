6af701183775e3ed75fdf71938e29258a178f3e4
Jarrett Dixon <jarrett@cofebe.com>
2019-05-15 11:03:07 -0700

    change name of method to CreatePDF in docraptor client, moved creation of url string to docraptor client constructor, moved InjectProjectInformationIntoTemplate to template service.go

--- cla-backend-go/template/service.go
+++ cla-backend-go/template/service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/LF-Engineering/cla-monorepo/cla-backend-go/gen/models"
+	"github.com/aymerick/raymond"
 )
 
 type Service interface {
@@ -19,6 +20,26 @@ func NewService(templateRepo Repository) service {
 	return service{
 		templateRepo: templateRepo,
 	}
+}
+
+func (s service) InjectProjectInformationIntoTemplate(projectName, shortProjectName, documentType, majorVersion, minorVersion, contactEmail string) string {
+	// DocRaptor API likes HTML in single line
+	templateBefore := `<html><body><p style=\"text-align: center\">{{projectName}}<br />{{documentType}} Contributor License Agreement (\"Agreement\")v{{majorVersion}}.{{minorVersion}}</p><p>Thank you for your interest in {{projectName}} project (“{{shortProjectName}}”) of The Linux Foundation (the “Foundation”). In order to clarify the intellectual property license granted with Contributions from any person or entity, the Foundation must have a Contributor License Agreement (“CLA”) on file that has been signed by each Contributor, indicating agreement to the license terms below. This license is for your protection as a Contributor as well as the protection of {{shortProjectName}}, the Foundation and its users; it does not change your rights to use your own Contributions for any other purpose.</p><p>If you have not already done so, please complete and sign this Agreement using the electronic signature portal made available to you by the Foundation or its third-party service providers, or email a PDF of the signed agreement to {{contactEmail}}. Please read this document carefully before signing and keep a copy for your records.</p></body></html>`
+	fieldsMap := map[string]string{
+		"projectName":      projectName,
+		"shortProjectName": shortProjectName,
+		"documentType":     documentType,
+		"majorVersion":     majorVersion,
+		"minorVersion":     minorVersion,
+		"contactEmail":     contactEmail,
+	}
+
+	templateAfter, err := raymond.Render(templateBefore, fieldsMap)
+	if err != nil {
+		fmt.Println("Failed to enter fields into HTML", err)
+	}
+
+	return templateAfter
 }
 
 func (s service) CreateCLAGroupTemplate(ctx context.Context, claGroupID string, claGroupFields *models.CreateClaTemplateGroup) error {
