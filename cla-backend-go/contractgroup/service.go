77187e9aa1931dca233191f56d2cc0bb5417484a
Charlie Oh <mun5424@gmail.com>
2019-05-17 11:47:43 -0700

    remove template endpoints from contractGroup

--- cla-backend-go/contractgroup/service.go
+++ cla-backend-go/contractgroup/service.go
@@ -36,87 +36,6 @@ func NewService(contractGroupRepo Repository) service {
 	}
 }
 
-// func (s Service) SaveTemplateToDynamoDB(template Template, templateName, tableName, contractGroupID, region string) {
-// 	// Initialize a session in us-west-2 that the SDK will use to load
-// 	// credentials from the shared credentials file ~/.aws/credentials.
-// 	sess, err := session.NewSession(&aws.Config{
-// 		Region: aws.String(region)},
-// 	)
-
-// 	// Create DynamoDB client
-// 	svc := dynamodb.New(sess)
-
-// 	item := dynamodbattribute.MarshalMap(template)
-
-// 	// Create item in table Movies
-// 	input := &dynamodb.PutItemInput{
-// 		Template:  item,
-// 		TableName: aws.String(tableName),
-// 	}
-
-// 	result, err = svc.PutItem(input)
-
-// 	if err != nil {
-// 		fmt.Println("Error putting item in database: ", err)
-// 		return err
-// 	}
-
-// 	fmt.Println("Successfully put item in database.")
-// 	return nil
-
-// }
-
-// func (s Service) SaveFileToS3Bucket(file io.ReadCloser, bucketName, fileName, region string) error {
-// 	sess := session.Must(session.NewSession(&aws.Config{
-// 		Region: aws.String(region),
-// 	},
-// 	))
-// 	// Create an uploader with the session and default options
-// 	uploader := s3manager.NewUploader(sess)
-
-// 	// Upload the file to S3.
-// 	result, err := uploader.Upload(&s3manager.UploadInput{
-// 		Bucket: aws.String(bucketName),
-// 		Key:    aws.String(fileName),
-// 		Body:   file,
-// 	})
-// 	if err != nil {
-// 		return fmt.Errorf("failed to upload file to S3 Bucket, %v", err)
-// 	}
-// 	fmt.Printf("file uploaded to, %s\n", result.Location)
-
-// 	defer file.Close()
-
-// 	return nil
-
-// }
-// func (s Service) SendHTMLToDocRaptor(HTML string) io.Reader {
-// 	DocRaptorAPIURL := "https://JMgaW58AX1CHmVmbKZCn@docraptor.com/docs"
-
-// 	document := `{
-//   		"type": "pdf",
-//   		"document_content": "%s",
-//   		"test":true
-// 	}`
-// 	document = fmt.Sprintf(document, HTML)
-
-// 	req, err := http.NewRequest(http.MethodPost, DocRaptorAPIURL, bytes.NewBufferString(document))
-// 	if err != nil {
-// 		fmt.Printf("failed to create request to submit data to API: %s", err)
-// 	}
-
-// 	req.Header.Set("Content-Type", "application/json")
-
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		fmt.Printf("failed to submit data to DocRaptorAPI: %s", err)
-// 	}
-
-// 	fmt.Printf("API Response Status Code: %s\n", resp.Status)
-
-// 	return resp.Body
-// }
-
 func (s service) InjectProjectInformationIntoTemplate(projectName, shortProjectName, documentType, majorVersion, minorVersion, contactEmail string) string {
 	// DocRaptor API likes HTML in single line
 	templateBefore := `<html><body><p style=\"text-align: center\">{{projectName}}<br />{{documentType}} Contributor License Agreement (\"Agreement\")v{{majorVersion}}.{{minorVersion}}</p><p>Thank you for your interest in {{projectName}} project (“{{shortProjectName}}”) of The Linux Foundation (the “Foundation”). In order to clarify the intellectual property license granted with Contributions from any person or entity, the Foundation must have a Contributor License Agreement (“CLA”) on file that has been signed by each Contributor, indicating agreement to the license terms below. This license is for your protection as a Contributor as well as the protection of {{shortProjectName}}, the Foundation and its users; it does not change your rights to use your own Contributions for any other purpose.</p><p>If you have not already done so, please complete and sign this Agreement using the electronic signature portal made available to you by the Foundation or its third-party service providers, or email a PDF of the signed agreement to {{contactEmail}}. Please read this document carefully before signing and keep a copy for your records.</p></body></html>`
@@ -237,14 +156,4 @@ func (s service) GetContractGroupSignatures(ctx context.Context, projectSFDCID s
 	}
 
 	return contractGoupSignatures, nil
-}
-
-func (s service) CreateContractTemplates(ctx context.Context, contractGroupID string, template models.Template) error {
-	err := s.contractGroupRepo.AddContractGroupTemplates(ctx, contractGroupID, template)
-	if err != nil {
-		return err
-	}
-
-	return nil
-
 }
