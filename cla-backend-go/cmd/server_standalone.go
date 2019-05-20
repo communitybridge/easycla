9b2412b908d157d14dc4a4719150377ab98bf502
Nima Jalali <nima@jalali.net>
2019-05-19 21:10:33 -0700

    go build automation

---
+++ cla-backend-go/cmd/server_standalone.go
@@ -0,0 +1,30 @@
+// +build !aws_lambda
+
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func runServer(cmd *cobra.Command, args []string) {
+	handler := server()
+
+	errs := make(chan error, 2)
+	go func() {
+		errs <- http.ListenAndServe(fmt.Sprintf(":%d", viper.GetInt("PORT")), handler)
+	}()
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Println("terminated", <-errs)
+}
