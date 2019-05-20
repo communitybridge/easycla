8588fa516f807b9ee180b7c8d1260c6a472d3de3
James Heller <jaheller5@gmail.com>
2019-05-20 02:33:19 -0700

    Read template config vars from SSM

--- cla-backend-go/config/ssm.go
+++ cla-backend-go/config/ssm.go
@@ -1,1 +1,80 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+func loadSSMConfig(awsSession *session.Session, stage string) (Config, error) {
+	config := Config{}
+
+	ssmClient := ssm.New(awsSession)
+
+	// Auth0
+	auth0Domain, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(fmt.Sprintf("cla-auth0-domain-%s", stage)),
+		WithDecryption: aws.Bool(false),
+	})
+	if err != nil {
+		return Config{}, err
+	}
+
+	auth0ClientID, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(fmt.Sprintf("cla-auth0-clientId-%s", stage)),
+		WithDecryption: aws.Bool(false),
+	})
+	if err != nil {
+		return Config{}, err
+	}
+
+	auth0Username, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(fmt.Sprintf("cla-auth0-username-claim-%s", stage)),
+		WithDecryption: aws.Bool(false),
+	})
+	if err != nil {
+		return Config{}, err
+	}
+
+	auth0Algorithm, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(fmt.Sprintf("cla-auth0-algorithm-%s", stage)),
+		WithDecryption: aws.Bool(false),
+	})
+	if err != nil {
+		return Config{}, err
+	}
+
+	config.Auth0 = Auth0{
+		Domain:        *auth0Domain.Parameter.Value,
+		ClientID:      *auth0ClientID.Parameter.Value,
+		UsernameClaim: *auth0Username.Parameter.Value,
+		Algorithm:     *auth0Algorithm.Parameter.Value,
+	}
+
+	// SFDC
+
+	// GitHub
+
+	// Docusign
+
+	// Docraptor
+	docraptorAPIKey, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String(fmt.Sprintf("cla-doc-raptor-api-key-%s", stage)),
+		WithDecryption: aws.Bool(false),
+	})
+	if err != nil {
+		return Config{}, err
+	}
+
+	config.Docraptor.APIKey = *docraptorAPIKey.Parameter.Value
+	config.Docraptor.TestMode = stage != "prod" && stage != "staging"
+
+	// LF Identity
+
+	// AWS
+	config.AWS.Region = "us-east-1"
+
+	return config, nil
+}
