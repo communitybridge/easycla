ed3fa4319656c03b53d9e7ac42a7abb0f3b79361
James Heller <jaheller5@gmail.com>
2019-05-20 01:54:34 -0700

    Update error checks and pdf url model

--- frontend-project-management-console/src/config/scripts/prefetch-ssm.js
+++ frontend-project-management-console/src/config/scripts/prefetch-ssm.js
@@ -10,7 +10,7 @@ async function prefetchSSM () {
   console.log(`Start to fetch SSM values at ${stageEnv}...`);
   result = await RetrieveSSMValues(configVarArray, stageEnv, region, profile);
 
-  result['cla-api-url'] = 'https://api.dev.lfcla.com';
+  // result['cla-api-url'] = 'https://api.dev.lfcla.com';
   fs.writeFile ('./config/cla-env-config.json', JSON.stringify(result), function(err) {
     if (err) throw new Error(`Couldn't save SSM paramters to disk with error ${err}`);
     console.log('Fetching completed...');});
