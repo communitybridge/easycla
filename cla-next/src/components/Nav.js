f61c44799d0fc582f02627e618399325e0ef58af
Ngozi Ekekwe <11598255+ngozi-ekekwe@users.noreply.github.com>
2019-07-10 18:34:04 +0100

    [WIP] - Create UI for Landing page (#19)
    * add initial commit for UI components
    * fix styles and add animation to links
    * add social links
    * fix styles and add animation to links
    
    Signed-off-by: ngozi-ekekwe <rose.ekekwe@gmail.com>

---
+++ cla-next/src/components/Nav.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import Link from 'next/link'
+import HamburgerIcon from './HamburgerIcon';
+
+const leftLinks = [
+  {
+    href: 'https://funding.communitybridge.org/', label: 'Funding'
+  },
+  {
+    href: 'https://people.communitybridge.org/', label: 'Mentorship'
+  },
+  {
+    href: 'https://docs.linuxfoundation.org/', label: 'Docs'
+  },
+  {
+    href: 'https://support.communitybridge.org/', label: 'Support'
+  }
+].map(link => {
+  link.key = `nav-link-${link.href}-${link.label}`
+  return link
+})
+
+const rightLinks = [
+  {
+    href: 'https://twitter.com/linuxfoundation', label: 'twitter'
+  },
+  {
+    href: 'https://www.facebook.com/TheLinuxFoundation', label: 'facebook'
+  },
+  {
+    href: 'https://www.linkedin.com/company/208777', label: 'linkedin'
+  },
+  {
+    href: 'https://www.youtube.com/user/TheLinuxFoundation', label: 'youtube-play'
+  }
+].map(link => {
+  link.key = `nav-link-${link.href}-${link.label}`
+  return link
+})
+
+
+
+
+const Nav = () => (
+  <header>
+    <div className="row header-row">
+      <div className="container">
+        <div className="header-wrapper">
+          <div className="header-left-column">
+            <nav>
+              <ul>
+                <li>
+                  <Link prefetch href="/">
+                    <a className="header-brand">
+                      <img src="../static/logo.svg" alt="" />
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+              <ul className="header-navs d-none d-lg-flex">
+                {leftLinks.map(({ key, href, label }) => (
+                  <li key={key}>
+                    <Link href={href}>
+                      <a>{label}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div className="header-right-column">
+            <nav className="d-none d-lg-flex">
+              <ul className="header-navs">
+                {rightLinks.map(({ key, href, label }) => (
+                  <li key={key}>
+                    <Link href={href}>
+                      <a>
+                        <i class={`fa fa-${label} faa-bounce animated-hover`}></i>
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <button className="linux-button">
+                <Link href="https://www.linuxfoundation.org/">
+                  <a> THE LINUX FOUNDATION</a>
+                </Link>
+              </button>
+            </nav>
+            <div className="d-flex d-md-none">
+              <HamburgerIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header >
+)
+
+export default Nav
---
+++ cla-next/src/components/shape-divider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShapeDivider = () => {
+  return (
+      <svg className="nectar-shape-divider" fill="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none"> <path d="M 0 14 s 88.64 3.48 300 36 c 260 40 514 27 703 -10 l 12 28 l 3 36 h -1018 z"></path> <path d="M 0 45 s 271 45.13 500 32 c 157 -9 330 -47 515 -63 v 86 h -1015 z"></path> <path d="M 0 58 s 188.29 32 508 32 c 290 0 494 -35 494 -35 v 45 h -1002 z"></path> </svg>
+  );
+};
+
+export default ShapeDivider;
