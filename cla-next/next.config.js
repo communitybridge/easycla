6d9751ead086666683d8dc9465e8314da65c2af2
David Deal <dealako@gmail.com>
2019-07-12 12:18:24 -0700

    Updated Security Headers
    * Added support community bridge and google fonts
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-next/next.config.js
+++ cla-next/next.config.js
@@ -8,26 +8,26 @@ const Dotenv = require('dotenv-webpack')
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass(
-  {
-    webpack: config => {
-      config.plugins = config.plugins || []
-  
-      config.plugins = [
-        ...config.plugins,
-  
-        // Read the .env file
-        new Dotenv({
-          path: path.join(__dirname, '.env'),
-          systemvars: true
-        })
-      ]
-  
-      return config
-    },
-    exportPathMap: function() {
-      return {
-        '/': { page: '/' }
-      }
+    {
+        webpack: config => {
+            config.plugins = config.plugins || []
+
+            config.plugins = [
+                ...config.plugins
+
+                // Read the .env file
+                // new Dotenv({
+                //   path: path.join(__dirname, '.env'),
+                //   systemvars: true
+                // })
+            ]
+
+            return config
+        },
+        exportPathMap: function() {
+            return {
+                '/': { page: '/home' }
+            }
+        }
     }
-  }
-)
+);
