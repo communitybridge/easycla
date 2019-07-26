3bb0011276da5292713c137c54c1797912dfaa88
ngozi-ekekwe <rose.ekekwe@gmail.com>
2019-07-26 20:57:18 +0100

    show cla agreement is user has signed agreement
    Signed-off-by: ngozi-ekekwe <rose.ekekwe@gmail.com>

--- cla-frontend-contributor-console/src/ionic/pages/cla-landing/cla-landing.ts
+++ cla-frontend-contributor-console/src/ionic/pages/cla-landing/cla-landing.ts
@@ -23,6 +23,9 @@ export class ClaLandingPage {
   hasIndividualCla: boolean;
   hasCorporateCla: boolean;
 
+  individualClaAgreement: string;
+  corporateClaAgreement: string;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -45,6 +48,8 @@ export class ClaLandingPage {
 
     this.hasCorporateCla = false;
     this.hasIndividualCla = false;
+    this.individualClaAgreement = "";
+    this.corporateClaAgreement = ""
   }
 
   ngOnInit() {
@@ -86,14 +91,32 @@ export class ClaLandingPage {
       if (!response.hasOwnProperty('errors')) {
         this.hasIndividualCla = true;
       }
+      this.individualClaAgreement = response.document_s3_url;
       this.loading.individualDoc = false;
     });
     this.claService.getProjectDocument(this.projectId, 'corporate').subscribe(response => {
       if (!response.hasOwnProperty('errors')) {
         this.hasCorporateCla = true;
       }
+      this.corporateClaAgreement = response.document_s3_url;
       this.loading.corporateDoc = false;
     });
+  }
+
+  getUserFriendlyID() {
+    if (this.user == null) {
+      return "user is undefined";
+    }
+
+    if (this.user.user_github_username != null) {
+      return this.user.user_github_username + " Github user id";
+    } else if (this.user.lf_username != null) {
+        return this.user.lf_username + " LF user id";
+    } else if (this.user.lf_email != null) {
+      return this.user.lf_email + " LF email";
+    } else {
+      return "no user information";
+    }
   }
 
 }
