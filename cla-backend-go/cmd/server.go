fa82320b86623fbae1b10d990e3d381962bab780
David Deal <dealako@gmail.com>
2019-08-06 10:32:11 -1000

    Resolved CORS Hostname Mismatch Logic (#121)
    - use url hostname instead of complete address
    - added additional unit tests
    - updated logging
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/cmd/server.go
+++ cla-backend-go/cmd/server.go
@@ -170,16 +170,16 @@ func setupGlobalMiddleware(handler http.Handler, allowedOrigins []string) http.H
 			}
 
 			if u.Scheme != "https" {
-				log.Warnf("non-https scheme - blocking origin: %s", origin)
+				log.Warnf("non-https scheme: %s - blocking origin: %s", u.Scheme, origin)
 				return false
 			}
 
 			// Ensure the origin is in our allowed list
-			allowedOrigin := stringInSlice(origin, allowedOrigins)
+			allowedOrigin := stringInSlice(u.Hostname(), allowedOrigins)
 			if allowedOrigin {
-				log.Debugf("origin %s is allowed", origin)
+				log.Debugf("origin %s is allowed", u.Hostname())
 			} else {
-				log.Warnf("origin %s is NOT allowed - not in allowed list: %v", origin, allowedOrigins)
+				log.Warnf("origin %s is NOT allowed - not in allowed list: %v", u.Hostname(), allowedOrigins)
 			}
 			return allowedOrigin
 		},
