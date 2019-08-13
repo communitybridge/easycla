c21e87d7f68b371a78e928a17b63564849a59722
David Deal <dealako@gmail.com>
2019-08-13 09:08:44 -0700

    Added Session Handler, Logging and UI Login Redirect (#138)
    - Added session handler to list of default http handlers
    - Added additional logging
    - Working to resolve session timeout/refresh issue
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-contributor-console/src/ionic/pages/auth/auth.ts
+++ cla-frontend-contributor-console/src/ionic/pages/auth/auth.ts
@@ -1,12 +1,12 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { AuthService } from "../../services/auth.service";
-import { RolesService } from "../../services/roles.service";
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {AuthService} from "../../services/auth.service";
+import {RolesService} from "../../services/roles.service";
 
- @Component({
+@Component({
   selector: 'page-auth',
   templateUrl: 'auth.html',
 })
@@ -16,10 +16,10 @@ export class AuthPage {
   gerritId: string;
   claType: string;
 
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams,
-    public authService: AuthService, 
-    public rolesService: RolesService) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public authService: AuthService,
+              public rolesService: RolesService) {
     this.gerritId = localStorage.getItem("gerritId");
     this.claType = localStorage.getItem("gerritClaType");
   }
@@ -27,24 +27,22 @@ export class AuthPage {
   ionViewDidEnter() {
     setTimeout(() => {
       this.rolesService.getUserRolesPromise().then((userRoles) => {
-        if (userRoles.isAuthenticated) { 
-            if(this.claType == "ICLA") {
-              this.navCtrl.setRoot('ClaGerritIndividualPage', {gerritId: this.gerritId});
-            }
-            else if(this.claType == "CCLA") { 
-              this.navCtrl.setRoot('ClaGerritCorporatePage', {gerritId: this.gerritId});
-            }
-            localStorage.removeItem("gerritId");
-            localStorage.removeItem("gerritClaType");
+        if (userRoles.isAuthenticated) {
+          if (this.claType == "ICLA") {
+            this.navCtrl.setRoot('ClaGerritIndividualPage', {gerritId: this.gerritId});
+          } else if (this.claType == "CCLA") {
+            this.navCtrl.setRoot('ClaGerritCorporatePage', {gerritId: this.gerritId});
+          }
+          localStorage.removeItem("gerritId");
+          localStorage.removeItem("gerritClaType");
 
-        }
-        else { 
+        } else {
           this.navCtrl.setRoot('loginPage');
         }
+      }).catch((error) => {
+        console.log("unable lookup user roles - possible session timeout: " + error);
+        this.navCtrl.setRoot("LoginPage")
       });
-    }, 2000); 
+    }, 2000);
   }
-
-  
-
 }
--- cla-frontend-corporate-console/src/ionic/pages/auth/auth.ts
+++ cla-frontend-corporate-console/src/ionic/pages/auth/auth.ts
@@ -1,10 +1,10 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AuthService } from '../../services/auth.service';
-import { RolesService } from "../../services/roles.service";
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {AuthService} from '../../services/auth.service';
+import {RolesService} from "../../services/roles.service";
 
 /**
  * Generated class for the AuthPage page.
@@ -12,7 +12,7 @@ import { RolesService } from "../../services/roles.service";
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
- @Component({
+@Component({
   selector: 'page-auth',
   templateUrl: 'auth.html',
 })
@@ -28,11 +28,14 @@ export class AuthPage {
 
     setTimeout(() => {
       this.rolesService.getUserRolesPromise().then((userRoles) => {
-        if(this.hasAccess(userRoles)) {
+        if (AuthPage.hasAccess(userRoles)) {
           this.navCtrl.setRoot("CompaniesPage");
         } else {
           this.navCtrl.setRoot("LoginPage");
         }
+      }).catch((error) => {
+        console.log("unable lookup user roles - possible session timeout: " + error);
+        this.navCtrl.setRoot("LoginPage")
       });
     }, 2000);
     // Artificial 2s delay isn't good, but the app may encoutner race condition between parse auth result and retrive user role
@@ -40,7 +43,7 @@ export class AuthPage {
     // Refactoring to Ionic 4.0+ with default Ng Route Module may resolve this problem but it's over work balance.
   }
 
-  private hasAccess(userRoles: any): boolean {
+  private static hasAccess(userRoles: any): boolean {
     return userRoles.isAuthenticated
   }
 
