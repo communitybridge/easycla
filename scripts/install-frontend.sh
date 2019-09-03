8c58d6d64a0ecce5c5df91419fe38abd01bb4e30
David Deal <dealako@gmail.com>
2019-09-03 16:08:12 -0700

    Cleaning up the build scripts and adding UI dependency.
    Signed-off-by: David Deal <dealako@gmail.com>

--- scripts/install-frontend.sh
+++ scripts/install-frontend.sh
@@ -3,10 +3,24 @@
 # Copyright The Linux Foundation and each contributor to CommunityBridge.
 # SPDX-License-Identifier: MIT
 
+###############################################################################
+# What to do if we die
+###############################################################################
+die() {
+  echo "$@"
+  exit 1
+}
+
+echo "Running yarn install in folder: $(pwd)"
 yarn install
-cd src
-npm install
-cd ..
-cd edge
-yarn install
-cd ..
+
+for dir in src edge; do
+  if [[ -d ${dir} ]]; then
+    pushd ${dir} || die "Unable to change to pushd to the ${dir} folder - exiting..."
+    echo "Running yarn install in folder: $(pwd)"
+    yarn install
+    popd || die "Unable to change to popd from the ${dir} folder - exiting..."
+  else
+    echo "Missing folder: ${dir} - skipping yarn install"
+  fi
+done
