70bc5ed331a075e5360affab19459989003a49f0
ngozi-ekekwe <rose.ekekwe@gmail.com>
2019-09-19 14:32:42 +0100

    add list for displaying existing companies
    Signed-off-by: ngozi-ekekwe <rose.ekekwe@gmail.com>

--- cla-frontend-corporate-console/src/ionic/modals/add-company-modal/add-company-modal.ts
+++ cla-frontend-corporate-console/src/ionic/modals/add-company-modal/add-company-modal.ts
@@ -34,6 +34,12 @@ export class AddCompanyModal {
   companyName: string;
   userEmail: string;
   userName: string;
+  companies: any[];
+  filteredComapnies: any[];
+  companySet: boolean = false;
+  joinExistingCompany: boolean = false;
+  addNewCompany: boolean = true;
+
 
   constructor(
     public navParams: NavParams,
@@ -48,7 +54,9 @@ export class AddCompanyModal {
 
   getDefaults() {
     this.company = this.navParams.get("company");
-    this.mode = this.company ? "edit" : "add";
+    this.mode = this.company ? "edit" : "find";
+    this.companies = [];
+    this.filteredComapnies = [];
 
     this.form = this.formBuilder.group({
       companyName: [this.companyName, Validators.compose([Validators.required])],
@@ -58,10 +66,17 @@ export class AddCompanyModal {
   }
 
   ngOnInit() {
+    this.getCompanies();
   }
 
   ionViewDidEnter() {
     this.updateUserInfoBasedLFID();
+  }
+
+  getCompanies() {
+    this.claService.getCompanies().subscribe((companies) => {
+      this.companies = companies;
+    })
   }
 
   submit() {
@@ -93,6 +108,15 @@ export class AddCompanyModal {
         this.currentlySubmitting = false;
       }
     );
+  }
+
+  joinCompany() {
+    let company = {
+      company_name: this.companyName,
+      company_manager_user_email: this.userEmail,
+      company_manager_user_name: this.userName
+    };
+    // TODO: add api call for joining organizations
   }
 
   updateCompany() {
@@ -143,6 +167,25 @@ export class AddCompanyModal {
     alert.present();
   }
 
+  findCompany(event) {
+    this.filteredComapnies = []
+    let compnayName = event.value;
+    if (compnayName.length > 0) {
+      this.companySet = false;
+      let filteredComapnies = this.companies.filter((company) => {
+        return company.company_name.toLowerCase().includes(compnayName.toLowerCase())
+      });
+      this.filteredComapnies = filteredComapnies;
+    }
+  }
+
+  setCompanyName(company) {
+    this.companySet = true;
+    this.companyName = company.company_name;
+    this.addNewCompany = false;
+    this.joinExistingCompany = true;
+  }
+
   private updateUserInfoBasedLFID() {
     if (this.authService.isAuthenticated()) {
       this.authService.getIdToken()
@@ -164,4 +207,6 @@ export class AddCompanyModal {
     }
     return;
   }
+
+  
 }
