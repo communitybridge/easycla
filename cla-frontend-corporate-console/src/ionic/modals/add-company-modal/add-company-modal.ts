2d326da3ecad514597299fae0b41316839a3ed48
ngozi-ekekwe <rose.ekekwe@gmail.com>
2019-09-19 15:10:47 +0100

    fix typo
    Signed-off-by: ngozi-ekekwe <rose.ekekwe@gmail.com>

--- cla-frontend-corporate-console/src/ionic/modals/add-company-modal/add-company-modal.ts
+++ cla-frontend-corporate-console/src/ionic/modals/add-company-modal/add-company-modal.ts
@@ -169,11 +169,11 @@ export class AddCompanyModal {
 
   findCompany(event) {
     this.filteredComapnies = []
-    let compnayName = event.value;
-    if (compnayName.length > 0) {
+    let companyName = event.value;
+    if (companyName.length > 0) {
       this.companySet = false;
       let filteredComapnies = this.companies.filter((company) => {
-        return company.company_name.toLowerCase().includes(compnayName.toLowerCase())
+        return company.company_name.toLowerCase().includes(companyName.toLowerCase())
       });
       this.filteredComapnies = filteredComapnies;
     }
