a9005d456e4b6a8a81716a72995bb640942030fa
David Deal <dealako@gmail.com>
2019-09-19 17:34:26 -0700

    Updated Corporate Console Projects View
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-project-console/src/ionic/modals/cla-contract-view-signatures-modal/cla-contract-view-signatures-modal.module.ts
+++ cla-frontend-project-console/src/ionic/modals/cla-contract-view-signatures-modal/cla-contract-view-signatures-modal.module.ts
@@ -1,16 +1,15 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { NgModule } from '@angular/core';
-import { Response } from '@angular/http';
-import { IonicPageModule } from 'ionic-angular';
-import { ClaContractViewSignaturesModal } from './cla-contract-view-signatures-modal';
-import { LoadingSpinnerComponentModule } from '../../components/loading-spinner/loading-spinner.module';
-import { LoadingDisplayDirectiveModule } from '../../directives/loading-display/loading-display.module';
-import { ModalHeaderComponentModule } from "../../components/modal-header/modal-header.module";
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {ClaContractViewSignaturesModal} from './cla-contract-view-signatures-modal';
+import {LoadingSpinnerComponentModule} from '../../components/loading-spinner/loading-spinner.module';
+import {LoadingDisplayDirectiveModule} from '../../directives/loading-display/loading-display.module';
+import {ModalHeaderComponentModule} from "../../components/modal-header/modal-header.module";
 import {SortingDisplayComponentModule} from "../../components/sorting-display/sorting-display.module";
 import {NgxPaginationModule} from 'ngx-pagination';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [
@@ -29,4 +28,5 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
     ClaContractViewSignaturesModal
   ]
 })
-export class ClaContractViewSignaturesModalModule {}
+export class ClaContractViewSignaturesModalModule {
+}
