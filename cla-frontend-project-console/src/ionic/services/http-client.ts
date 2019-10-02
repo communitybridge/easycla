c9d2dc872c1f42d3291e1a1216a457667491e617
David Deal <dealako@gmail.com>
2019-10-02 13:17:26 -0700

    Added cache-control to REST API requests
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-project-console/src/ionic/services/http-client.ts
+++ cla-frontend-project-console/src/ionic/services/http-client.ts
@@ -1,9 +1,9 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class HttpClient {
@@ -21,40 +21,40 @@ export class HttpClient {
   private buildHeaders(contentType: string = 'application/json') {
     let headers = new Headers({
       'Accept': 'application/json',
-      'Content-Type': contentType
+      'Content-Type': contentType,
+      'Cache-Control': 'no-cache',
     });
     return Promise.resolve(headers);
   }
 
   get(url) {
     return Observable
-        .fromPromise(this.buildHeaders())
-        .switchMap((headers) =>
-          this.http.get(url, { headers: headers }));
+      .fromPromise(this.buildHeaders())
+      .switchMap((headers) =>
+        this.http.get(url, {headers: headers}));
   }
 
   post(url, data) {
     return Observable
-        .fromPromise(this.buildHeaders())
-        .switchMap((headers) => this.http.post(url, data, { headers: headers }));
+      .fromPromise(this.buildHeaders())
+      .switchMap((headers) => this.http.post(url, data, {headers: headers}));
   }
 
   put(url, data, contentType: string = 'application/json') {
     return Observable
-        .fromPromise(this.buildHeaders(contentType))
-        .switchMap((headers) => this.http.put(url, data, { headers: headers }));
+      .fromPromise(this.buildHeaders(contentType))
+      .switchMap((headers) => this.http.put(url, data, {headers: headers}));
   }
 
   patch(url, data, contentType: string = 'application/json') {
     return Observable
-        .fromPromise(this.buildHeaders(contentType))
-        .switchMap((headers) => this.http.patch(url, data, { headers: headers }));
+      .fromPromise(this.buildHeaders(contentType))
+      .switchMap((headers) => this.http.patch(url, data, {headers: headers}));
   }
 
   delete(url) {
     return Observable
-        .fromPromise(this.buildHeaders())
-        .switchMap((headers) => this.http.delete(url, { headers: headers }));
+      .fromPromise(this.buildHeaders())
+      .switchMap((headers) => this.http.delete(url, {headers: headers}));
   }
-
 }
