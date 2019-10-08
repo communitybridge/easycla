e82f99aee7e2bc2a0036c401b1f3818b165f95f8
David Deal <dealako@gmail.com>
2019-10-08 16:26:15 -0700

    Contributor Console Erorr Handling (#267)
    - Updated spinner start/stop logic to prevent it from endlessly spinning
    - Added error message to UI when an error occurs
    - Added help desk link if error occurs
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-contributor-console/src/ionic/pages/cla-employee-company-confirm/cla-employee-company-confirm.ts
+++ cla-frontend-contributor-console/src/ionic/pages/cla-employee-company-confirm/cla-employee-company-confirm.ts
@@ -31,6 +31,9 @@ export class ClaEmployeeCompanyConfirmPage {
   submitAttempt: boolean = false;
   currentlySubmitting: boolean = false;
 
+  errorMessage: string = null;
+  helpDeskLink: URL = new URL('https://jira.linuxfoundation.org/servicedesk/customer/portal/4');
+
   constructor(
     public navCtrl: NavController,
     private modalCtrl: ModalController,
@@ -60,7 +63,8 @@ export class ClaEmployeeCompanyConfirmPage {
     };
     this.user = {
       user_name: '',
-    }
+    };
+    this.errorMessage = null;
     this.currentlySubmitting = false;
   }
 
@@ -89,8 +93,11 @@ export class ClaEmployeeCompanyConfirmPage {
   }
 
   submit() {
+    // Reset our status and error messages
     this.submitAttempt = true;
+    this.errorMessage = null;
     this.currentlySubmitting = true;
+
     if (!this.form.valid) {
       this.currentlySubmitting = false;
       // prevent submit
@@ -104,14 +111,20 @@ export class ClaEmployeeCompanyConfirmPage {
       return_url_type: this.signingType, //"Gerrit" / "Github"
     };
     this.claService.postEmployeeSignatureRequest(signatureRequest).subscribe(response => {
+      this.currentlySubmitting = false;
+
       let errors = response.hasOwnProperty('errors');
       if (errors) {
+        console.log('Signature request response:');
+        console.log(response.errors);
+        this.errorMessage = response.errors;
+
         if (response.errors.hasOwnProperty('ccla_whitelist')) {
           // When the user is not whitelisted with the company: return {'errors': {'ccla_whitelist': 'No user email whitelisted for this ccla'}}
           this.openClaEmployeeCompanyTroubleshootPage();
-          this.currentlySubmitting = false;
           return;
         }
+
         if (response.errors.hasOwnProperty('missing_ccla')) {
           // When the company does NOT have a CCLA with the project: {'errors': {'missing_ccla': 'Company does not have CCLA with this project'}}
           // The user shouldn't get here if they are using the console properly
@@ -119,12 +132,11 @@ export class ClaEmployeeCompanyConfirmPage {
         }
       } else {
         // No Errors, expect normal signature response
+        this.errorMessage = null;
         this.signature = response;
         this.openClaNextStepModal();
-        this.currentlySubmitting = false;
       }
     });
-
   }
 
   openClaNextStepModal() {
