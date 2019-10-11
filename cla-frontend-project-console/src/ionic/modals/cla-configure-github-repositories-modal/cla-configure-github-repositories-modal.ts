e5b0f6d097ccb73e78c9deca8a17931dbe9217e7
David Deal <dealako@gmail.com>
2019-10-11 16:18:25 -0700

    Project Console - GitHub Repository Add All (#274)
    - added repository add all feature
    
    Resolves: #210
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-project-console/src/ionic/modals/cla-configure-github-repositories-modal/cla-configure-github-repositories-modal.ts
+++ cla-frontend-project-console/src/ionic/modals/cla-configure-github-repositories-modal/cla-configure-github-repositories-modal.ts
@@ -1,9 +1,9 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component } from '@angular/core';
-import { NavParams, ViewController, IonicPage, Events } from 'ionic-angular';
-import { ClaService } from '../../services/cla.service';
+import {Component} from '@angular/core';
+import {Events, IonicPage, NavParams, ViewController} from 'ionic-angular';
+import {ClaService} from '../../services/cla.service';
 
 @IonicPage({
   segment: 'cla-configure-github-repositories-modal'
@@ -53,7 +53,7 @@ export class ClaConfigureGithubRepositoriesModal {
     this.assignedRepositories = data['repositories'];
     this.orgAndRepositories = data['orgs_and_repos']
       .map(organization => {
-        this.loading.activateSpinner = false
+        this.loading.activateSpinner = false;
         organization.repositories.map(repository => {
           repository.status = 'free';
           repository.repository_organization_name = organization.organization_name;
@@ -87,18 +87,16 @@ export class ClaConfigureGithubRepositoriesModal {
       if (String(assignedRepository.repository_external_id) === String(repository.repository_github_id)) {
         repository.repository_project_id = assignedRepository.repository_project_id;
         repository.repository_id = assignedRepository.repository_id;
-
         return true;
       }
-
       return false;
     });
   }
 
   assignRepository(repository) {
-    const payload = { ...repository };
+    const payload = {...repository};
     delete payload.status;
-    this.loading.activateSpinner = true
+    this.loading.activateSpinner = true;
 
     payload.repository_project_id = this.claProjectId;
     payload.repository_external_id = payload.repository_github_id;
@@ -109,9 +107,59 @@ export class ClaConfigureGithubRepositoriesModal {
   }
 
   removeRepository(repository) {
-    this.loading.activateSpinner = true
+    this.loading.activateSpinner = true;
     this.claService.removeProjectRepository(repository.repository_id)
       .subscribe(() => this.getOrgRepositories())
   }
 
+  addAll() {
+    console.log(this.orgAndRepositories);
+    for (const orgRepo in this.orgAndRepositories) {
+      const theOrg = this.orgAndRepositories[orgRepo];
+      // No data?
+      if (theOrg == null || theOrg.repositories == null || theOrg.repositories == 0) {
+        continue;
+      }
+
+      for (const repo in theOrg.repositories) {
+        const theRepo = theOrg.repositories[repo];
+        // No data? - move on
+        if (theRepo == null || theRepo.status == null) {
+          continue;
+        }
+
+        if (theRepo.status == 'free') {
+          console.log('Adding repo: ' + theRepo.repository_name);
+          this.assignRepository(theRepo);
+        } else {
+          console.log('Skipping repo: ' + theRepo.repository_name + ', status is: ' + theRepo.status);
+        }
+      }
+    }
+  }
+
+  anyAvailableRepos(): boolean {
+    let retVal: boolean = false;
+    for (const orgRepo in this.orgAndRepositories) {
+      const theOrg = this.orgAndRepositories[orgRepo];
+      // No data?
+      if (theOrg == null || theOrg.repositories == null || theOrg.repositories == 0) {
+        continue;
+      }
+
+      for (const repo in theOrg.repositories) {
+        const theRepo = theOrg.repositories[repo];
+        // No data? - move on
+        if (theRepo == null || theRepo.status == null) {
+          continue;
+        }
+
+        if (theRepo.status == 'free') {
+          retVal = true;
+        }
+      }
+    }
+
+    return retVal;
+  }
 }
