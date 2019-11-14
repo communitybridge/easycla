220dca14a395b048c4a472b3375c372a4a2640d0
David Deal <dealako@gmail.com>
2019-11-13 17:26:43 -0800

    Added Auth0 Logout
    - Fixed Redirect/ReturnTo URI
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-corporate-console/src/ionic/services/auth.service.ts
+++ cla-frontend-corporate-console/src/ionic/services/auth.service.ts
@@ -108,8 +108,10 @@ export class AuthService {
       redirectUri = 'http://localhost:8101'
     }
     console.log('Redirecting to: ' + redirectUri + '/#/login');
+    // This library seems to want the options to be `redirectUri`, but the API docs say to use: `returnTo`
+    // `returnTo` works
     this.auth0.logout({
-      redirectUri: redirectUri + '/#/login'
+      returnTo: redirectUri + '/#/login'
     });
   }
 
