bb9d5f54358644d38cd1b2fc16026931f60e32e7
David Deal <dealako@gmail.com>
2019-12-04 11:26:08 -0800

    Access Page Help Link Updates and Removed Duplicate Get Help Lin… (#391)
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-project-console/src/ionic/pages/all-projects/all-projects.ts
+++ cla-frontend-project-console/src/ionic/pages/all-projects/all-projects.ts
@@ -111,6 +111,6 @@ export class AllProjectsPage {
    * Opens the access page in a new window
    */
   openAccessPage() {
-    window.open('https://github.com/communitybridge/easycla/blob/master/docs/get-access-to-easycla.md', "_blank");
+    window.open('https://app.gitbook.com/@lf-docs-linux-foundation/s/easycla/getting-started/get-access-to-easycla', "_blank");
   }
 }
