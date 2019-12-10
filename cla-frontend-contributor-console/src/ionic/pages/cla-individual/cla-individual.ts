7236a61b690e62bc6d833242c484fda695e18b83
asithade <asithade@gmail.com>
2019-12-10 15:23:54 -0800

    Fix for Missing Buttons
    Signed-off-by: asithade <asithade@gmail.com>

--- cla-frontend-contributor-console/src/ionic/pages/cla-individual/cla-individual.ts
+++ cla-frontend-contributor-console/src/ionic/pages/cla-individual/cla-individual.ts
@@ -1,16 +1,21 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component } from '@angular/core';
-import { NavController, NavParams, IonicPage, ModalController, } from 'ionic-angular';
-import { ClaService } from '../../services/cla.service';
+import { Component } from "@angular/core";
+import {
+  NavController,
+  NavParams,
+  IonicPage,
+  ModalController
+} from "ionic-angular";
+import { ClaService } from "../../services/cla.service";
 
 @IonicPage({
-  segment: 'cla/project/:projectId/user/:userId/individual'
+  segment: "cla/project/:projectId/user/:userId/individual"
 })
 @Component({
-  selector: 'cla-individual',
-  templateUrl: 'cla-individual.html'
+  selector: "cla-individual",
+  templateUrl: "cla-individual.html"
 })
 export class ClaIndividualPage {
   projectId: string;
@@ -22,24 +27,25 @@ export class ClaIndividualPage {
   activeSignatures: boolean = true; // we assume true until otherwise
   signature: any;
   loadingSignature: boolean = true;
+  error: any = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private modalCtrl: ModalController,
-    private claService: ClaService,
+    private claService: ClaService
   ) {
     this.getDefaults();
-    this.projectId = navParams.get('projectId');
-    this.userId = navParams.get('userId');
+    this.projectId = navParams.get("projectId");
+    this.userId = navParams.get("userId");
   }
 
   getDefaults() {
     this.project = {
-      project_name: "",
+      project_name: ""
     };
     this.signature = {
-      sign_url: "",
+      sign_url: ""
     };
   }
 
@@ -61,11 +67,11 @@ export class ClaIndividualPage {
     });
   }
 
-  getUserSignatureIntent(userId)  {
+  getUserSignatureIntent(userId) {
     this.loadingSignature = true;
     this.claService.getUserSignatureIntent(userId).subscribe(response => {
       this.signatureIntent = response;
-      if(this.signatureIntent !== null) {
+      if (this.signatureIntent !== null) {
         this.postSignatureRequest();
       } else {
         this.activeSignatures = false;
@@ -76,23 +82,30 @@ export class ClaIndividualPage {
 
   postSignatureRequest() {
     let signatureRequest = {
-      'project_id': this.projectId,
-      'user_id': this.userId,
+      project_id: this.projectId,
+      user_id: this.userId,
       // TODO: Switch this to intermediary loading screen as docusign postback has delay
-      'return_url_type': "Github",
-      'return_url': this.signatureIntent.return_url,
+      return_url_type: "Github",
+      return_url: this.signatureIntent.return_url
     };
 
-    this.claService.postIndividualSignatureRequest(signatureRequest).subscribe(response => {
-      // returns {
-      //   user_id:
-      //   signature_id:
-      //   project_id:
-      //   sign_url: docusign.com/some-docusign-url
-      // }
-      console.log(response);
-      this.signature = response;
-    });
+    this.claService.postIndividualSignatureRequest(signatureRequest).subscribe(
+      response => {
+        // returns {
+        //   user_id:
+        //   signature_id:
+        //   project_id:
+        //   sign_url: docusign.com/some-docusign-url
+        // }
+        if (response.errors) {
+          this.error = response;
+        }
+        this.signature = response;
+      },
+      err => {
+        this.error = err;
+      }
+    );
   }
 
   openClaAgreement() {
@@ -100,7 +113,6 @@ export class ClaIndividualPage {
       // Can't open agreement if we don't have a sign_url yet
       return;
     }
-    window.open(this.signature.sign_url, '_blank');
+    window.open(this.signature.sign_url, "_blank");
   }
-
 }
