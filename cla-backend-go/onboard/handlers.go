3267607cbf091788ea4740f40e5fdff482ce3ebb
David Deal <dealako@gmail.com>
2019-12-12 19:40:10 -0800

    Added v3 messaging service.
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/onboard/handlers.go
+++ cla-backend-go/onboard/handlers.go
@@ -5,6 +5,9 @@ package onboard
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/communitybridge/easycla/cla-backend-go/gen/models"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/restapi/operations"
@@ -28,7 +31,7 @@ func Configure(api *operations.ClaAPI, service Service) {
 				msg := fmt.Sprintf("Bad Request - unable to create CLA Manager request using LFID: %s, error: %v", *params.Body.LfID, err)
 				log.Warnf(msg)
 				return onboard.NewCreateCLAManagerRequestBadRequest().WithPayload(&models.ErrorResponse{
-					Code:    "400",
+					Code:    strconv.Itoa(onboard.CreateCLAManagerRequestBadRequestCode),
 					Message: msg,
 				})
 			}
@@ -44,7 +47,7 @@ func Configure(api *operations.ClaAPI, service Service) {
 				msg := fmt.Sprintf("Bad Request - unable to query CLA Manager requests using LFID: %s, error: %v", params.LfID, err)
 				log.Warnf(msg)
 				return onboard.NewGetCLAManagerRequestsByLFIDBadRequest().WithPayload(&models.ErrorResponse{
-					Code:    "400",
+					Code:    strconv.Itoa(onboard.GetCLAManagerRequestsByLFIDBadRequestCode),
 					Message: msg,
 				})
 			}
@@ -53,7 +56,7 @@ func Configure(api *operations.ClaAPI, service Service) {
 				msg := fmt.Sprintf("No requests found for lfid: %s", params.LfID)
 				log.Warnf(msg)
 				return onboard.NewGetCLAManagerRequestsByLFIDNotFound().WithPayload(&models.ErrorResponse{
-					Code:    "400",
+					Code:    strconv.Itoa(onboard.GetCLAManagerRequestsByLFIDNotFoundCode),
 					Message: msg,
 				})
 			}
@@ -69,11 +72,40 @@ func Configure(api *operations.ClaAPI, service Service) {
 				msg := fmt.Sprintf("Bad Request - unable to delete CLA Manager requests using request id: %s, error: %v", params.RequestID, err)
 				log.Warnf(msg)
 				return onboard.NewDeleteCLAManagerRequestsByRequestIDBadRequest().WithPayload(&models.ErrorResponse{
-					Code:    "400",
+					Code:    strconv.Itoa(onboard.DeleteCLAManagerRequestsByRequestIDBadRequestCode),
 					Message: msg,
 				})
 			}
 
 			return onboard.NewDeleteCLAManagerRequestsByRequestIDNoContent()
+		})
+
+	api.OnboardSendNotificationHandler = onboard.SendNotificationHandlerFunc(
+		//func(params onboard.SendNotificationParams, user *user.CLAUser) middleware.Responder {
+		func(params onboard.SendNotificationParams) middleware.Responder {
+			/*
+				// Check the user details
+				if user == nil {
+					msg := "Access control check - CLA User object not provided for send message request"
+					log.Warn(msg)
+					return onboard.NewSendNotificationUnauthorized().WithPayload(&models.ErrorResponse{
+						Code:    strconv.Itoa(onboard.SendNotificationUnauthorizedCode),
+						Message: msg,
+					})
+				}
+			*/
+
+			// sender string, recipients []string, subject string, emailBody string
+			messageID, err := service.SendNotification(params.Body.SenderEmail, params.Body.RecipientEmails, params.Body.Subject, params.Body.EmailBody)
+			if err != nil {
+				msg := fmt.Sprintf("Bad Request - unable to send notification to recipients: %+v, error: %+v", params.Body.RecipientEmails, err)
+				log.Warnf(msg)
+				return onboard.NewSendNotificationBadRequest().WithPayload(&models.ErrorResponse{
+					Code:    "400",
+					Message: msg,
+				})
+			}
+			return onboard.NewSendNotificationOK().WithPayload(&models.OnboardNotificationResponse{
+				Message: aws.String(fmt.Sprintf("Message sent - messageID: %s", *messageID))})
 		})
 }
---
+++ cla-backend-go/utils/events.go
@@ -0,0 +1,70 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
+package utils
+
+import (
+	"time"
+
+	emailevent "github.com/LF-Engineering/lfx-models/models/email"
+	"github.com/LF-Engineering/lfx-models/models/event"
+	log "github.com/communitybridge/easycla/cla-backend-go/logging"
+	"github.com/go-openapi/strfmt"
+	"github.com/google/uuid"
+)
+
+// CreateEventWrapper creates a new event with the specified event type
+func CreateEventWrapper(eventType string) *event.Event {
+	now := time.Now().UTC()
+	openAPITime := getCurrentDateTime()
+	err := openAPITime.Scan(now)
+	if err != nil {
+		log.Warnf("error while converting/scanning strfmt time: %v", err)
+		return nil
+	}
+
+	return &event.Event{
+		ID:      uuid.New().String(),
+		Version: "0.1.0",
+		Type:    eventType,
+		Created: openAPITime,
+		SourceID: &event.Source{
+			Name:        "EasyCLA Service",
+			Description: "EasyCLA Service",
+			ClientID:    "easycla-service",
+		},
+	}
+}
+
+// ToEmailEvent converts an organization model into a create org event model
+func ToEmailEvent(sender *string, recipients []string, subject *string, emailBody *string) *emailevent.EmailEvent {
+
+	// Convert the string array to an array of strfmt email recipients
+	var emailRecipients = make([]strfmt.Email, len(recipients))
+	for i, recipient := range recipients {
+		emailRecipients[i] = strfmt.Email(recipient)
+	}
+
+	log.Debug("Generating email event")
+	from := strfmt.Email(*sender)
+	return &emailevent.EmailEvent{
+		From:       &from,
+		Recipients: emailRecipients,
+		Subject:    subject,
+		Body:       emailBody,
+		Type:       "cla-email-event",
+	}
+}
+
+// getCurrentDateTime returns the current date time as a strfmt.DateTime object
+func getCurrentDateTime() strfmt.DateTime {
+	// Grab the current time as a strfmt date/time
+	now := time.Now().UTC()
+	openAPITime := strfmt.NewDateTime()
+	err := openAPITime.Scan(now)
+	if err != nil {
+		log.Warnf("error while converting/scanning strfmt time: %v", err)
+	}
+
+	return openAPITime
+}
