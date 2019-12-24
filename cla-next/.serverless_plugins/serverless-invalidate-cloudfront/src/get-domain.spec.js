79255efc8a767f784ae5f6b98e879ab69c8d145c
David Deal <dealako@gmail.com>
2019-12-23 18:02:02 -0800

    Working to resolve cla-next landing page deployment. (#445)
    Signed-off-by: David Deal <dealako@gmail.com>

---
+++ cla-next/.serverless_plugins/serverless-invalidate-cloudfront/src/get-domain.spec.js
@@ -0,0 +1,39 @@
+const getDomain = require('./get-domain');
+
+describe('getDomain', () => {
+  let aws, cloudfront, promise;
+  beforeEach(() => {
+    const stacks = {
+      StackResources: [
+        {
+          ResourceType: 'AWS::CloudFront::Distribution',
+          LogicalResourceId: 'dist1',
+          PhysicalResourceId: 'physical1'
+        }
+      ]
+    };
+    const stackName = 'stack';
+    aws = {
+      naming: jasmine.createSpyObj('naming', {
+        getStackName: jasmine.createSpy('getStackName').and.returnValue(stackName)
+      }),
+      request: jasmine.createSpy('request').and.returnValue(Promise.resolve(stacks))
+    };
+
+    cloudfront = {
+      getDistribution: jasmine.createSpy('getDistribution').and.returnValue({
+        promise: () =>
+          Promise.resolve({
+            DomainName: 'www.somedomain.com'
+          })
+      })
+    };
+  });
+
+  it('gets a domain name from a distribution name', done => {
+    getDomain('dist1', aws, cloudfront).then(name => {
+      expect(name).toBe('www.somedomain.com');
+      done();
+    });
+  });
+});
---
+++ cla-next/.serverless_plugins/serverless-invalidate-cloudfront/src/match-distributions.spec.js
@@ -0,0 +1,68 @@
+const matchDistributions = require('./match-distributions');
+
+describe('matchDistributions', () => {
+  let stacks;
+
+  beforeEach(() => {
+    stacks = [
+      {
+        ResourceType: 'AWS::CloudFront::Distribution',
+        LogicalResourceId: 'dist1',
+        PhysicalResourceId: 'physical1'
+      },
+      {
+        ResourceType: 'AWS::CloudFront::Distribution',
+        LogicalResourceId: 'dist2',
+        PhysicalResourceId: 'physical2'
+      },
+      {
+        ResourceType: 'AWS::Lambda',
+        LogicalResourceId: 'nondistribution',
+        PhysicalResourceId: 'physical3'
+      }
+    ];
+  });
+
+  it('can match a single distribution to a stack resource', () => {
+    const distributions = {
+      dist1: {
+        paths: ['a', 'b', 'c']
+      }
+    };
+    const result = matchDistributions(distributions, stacks, 'staging');
+    expect(result).toEqual([{ distribution: distributions.dist1, distributionId: 'physical1', name: 'dist1' }]);
+  });
+  it('can match multiple distributions to a stack resource', () => {
+    const distributions = {
+      dist1: {
+        paths: ['a', 'b', 'c']
+      },
+      dist2: {
+        paths: ['e', 'f', 'g']
+      }
+    };
+    const result = matchDistributions(distributions, stacks, 'staging');
+    expect(result).toEqual([
+      { distribution: distributions.dist1, distributionId: 'physical1', name: 'dist1' },
+      { distribution: distributions.dist2, distributionId: 'physical2', name: 'dist2' }
+    ]);
+  });
+
+  it("throws an error when a distribution doesn't have a matching stack resource", () => {
+    const distributions = {
+      dist3: {
+        paths: ['a', 'b', 'c']
+      }
+    };
+    expect(() => matchDistributions(distributions, stacks, 'staging')).toThrow();
+  });
+
+  it("throws an error when a distribution matches a stack resource which isn't a cloudfront distribution", () => {
+    const distributions = {
+      nondistribution: {
+        paths: ['a', 'b', 'c']
+      }
+    };
+    expect(() => matchDistributions(distributions, stacks, 'staging')).toThrow();
+  });
+});
