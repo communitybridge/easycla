65a3e82c636ac30b731f95cb122031e7836bc866
David Deal <dealako@gmail.com>
2020-01-10 12:25:13 -0800

    Updated Signature Query
    - Existing query searching by project id (CNCF project with 1000's of
    signatures) and then sub-filters by other criteria resulting in the code
    needing to iterate over potentially thousands of records.  This resulted
    in unexpected errors in the python pynamodb library when retrieving the
    results. This patch is to add a specific signature query to search
    by company_id then filter by project which, in one case, reduces the
    number of records returned from 1000's to less than 10.  As a result the
    query is much faster and avoids the volume/generator/pagination issue we
    are experiencing.
    
    Resolves: #486
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- scripts/install-backend.sh
+++ scripts/install-backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copyright The Linux Foundation and each contributor to CommunityBridge.
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 cd cla-backend/
 npm install
