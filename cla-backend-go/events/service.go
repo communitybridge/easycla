7eafc590d7efa60fd68c0d14065ebd0baba54cc2
prasanna.mahajan <mahajanprasannap@gmail.com>
2020-01-24 17:15:16 +0530

    making pageSize as integer
    Signed-off-by: prasanna.mahajan <mahajanprasannap@gmail.com>

--- cla-backend-go/events/service.go
+++ cla-backend-go/events/service.go
@@ -2,11 +2,9 @@ package events
 
 import (
 	"context"
-	"strconv"
 
 	"github.com/communitybridge/easycla/cla-backend-go/gen/models"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/restapi/operations/events"
-	log "github.com/communitybridge/easycla/cla-backend-go/logging"
 )
 
 // Service interface defines methods of event service
@@ -31,21 +29,8 @@ func (s *service) CreateEvent(event models.Event) error {
 func (s *service) SearchEvents(ctx context.Context, params *events.SearchEventsParams) (*models.EventList, error) {
 	const defaultPageSize int64 = 50
 	var pageSize = defaultPageSize
-
 	if params.PageSize != nil {
-		var err error
-		pageSize, err = strconv.ParseInt(*params.PageSize, 10, 64)
-		if err != nil {
-			log.Warnf("error parsing pageSize parameter to int64 - using default size of %d - error: %v",
-				defaultPageSize, err)
-		}
-
-		// Make sure it's positive
-		if pageSize < 1 {
-			log.Warnf("invalid page size of %d - must be a positive value - using default size of %d",
-				pageSize, defaultPageSize)
-			pageSize = defaultPageSize
-		}
+		pageSize = *params.PageSize
 	}
 	return s.repo.SearchEvents(ctx, params, pageSize)
 }
