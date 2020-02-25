6da744905f8e02235b0e630cffc12f541964f72b
Ngozi Ekekwe <11598255+ngozi-ekekwe@users.noreply.github.com>
2020-02-25 21:03:42 +0100

    Add a  message to inform that we need to login to connect to Git… (#614)
    Signed-off-by: ngozi-ekekwe <rose.ekekwe@gmail.com>
    
    Co-authored-by: David Deal <dealako@gmail.com>

--- cla-frontend-project-console/src/ionic/modals/cla-organization-app-modal/cla-organization-app-modal.ts
+++ cla-frontend-project-console/src/ionic/modals/cla-organization-app-modal/cla-organization-app-modal.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 import { Component } from '@angular/core';
-import { NavController, ModalController, NavParams, ViewController, IonicPage, Events } from 'ionic-angular';
+import { NavController, ModalController, NavParams, ViewController, IonicPage, Events, AlertController } from 'ionic-angular';
 import { PopoverController } from 'ionic-angular';
 import { EnvConfig } from '../../services/cla.env.utils';
 import { ClaService } from '../../services/cla.service';
@@ -21,6 +21,7 @@ export class ClaOrganizationAppModal {
     public viewCtrl: ViewController,
     private popoverCtrl: PopoverController,
     public modalCtrl: ModalController,
+    public alertCtrl: AlertController,
     public events: Events
   ) {
     this.getDefaults();
@@ -37,9 +38,25 @@ export class ClaOrganizationAppModal {
   // TODO: Do we want a call to cla that polls for the installation status?
   // UH YEA?
 
+
+  openMessageDialog() {
+    let alert = this.alertCtrl.create({
+      title: 'You are exiting EasyCLA and going to GitHub site. Please make sure you are already logged into GitHub so that you can install EasyCLA app',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.viewCtrl.dismiss();
+            window.open(EnvConfig['gh-app-public-link'], '_blank');
+          }
+        },
+      ]
+    });
+    alert.present();
+  }
+
   openAppPage() {
-    this.viewCtrl.dismiss();
-    window.open(EnvConfig['gh-app-public-link'], '_blank');
+    this.openMessageDialog()
   }
 
   dismiss() {
