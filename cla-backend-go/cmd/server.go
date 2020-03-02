9048c3ab984859794970caef1695cee474fa56c9
David Deal <dealako@gmail.com>
2020-03-02 13:45:37 -0800

    Allows Non-HTTPS for Localhost CORS
    - Allows CORS localhost for HTTP
    - Allows CORS hosts for HTTPS (non-localhost)
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/cmd/server.go
+++ cla-backend-go/cmd/server.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/communitybridge/easycla/cla-backend-go/docs"
 	"github.com/communitybridge/easycla/cla-backend-go/metrics"
@@ -226,19 +227,27 @@ func setupCORSHandler(handler http.Handler, allowedOrigins []string) http.Handle
 				return false
 			}
 
-			if u.Scheme != "https" {
-				log.Warnf("non-https scheme: %s - blocking origin: %s", u.Scheme, origin)
-				return false
-			}
-
 			// Ensure the origin is in our allowed list
 			allowedOrigin := stringInSlice(u.Hostname(), allowedOrigins)
 			if allowedOrigin {
-				log.Debugf("origin %s is allowed", u.Hostname())
+				// localhost with HTTP is allowed
+				if strings.HasPrefix(u.Hostname(), "localhost") && u.Scheme == "http" {
+					log.Debugf("origin %s with protocol is allowed", u.Hostname(), u.Scheme)
+					return true
+				}
+
+				// non-localhost with HTTPS is allowed
+				if !strings.HasPrefix(u.Hostname(), "localhost") && u.Scheme == "https" {
+					log.Debugf("origin %s with protocol is allowed", u.Hostname(), u.Scheme)
+					return true
+				}
+
+				log.Debugf("origin %s with protocol %s is NOT allowed", u.Hostname(), u.Scheme)
+				return false
 			} else {
 				log.Warnf("origin %s is NOT allowed - not in allowed list: %v", u.Hostname(), allowedOrigins)
+				return false
 			}
-			return allowedOrigin
 		},
 		// Enable Debugging for testing, consider disabling in production
 		Debug: false,
