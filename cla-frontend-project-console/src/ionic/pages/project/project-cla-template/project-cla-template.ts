09fdeeb85ebc10b1d0b8b4b0c1d55a562c397e6c
Amol Sontakke <amols@proximabiz.com>
2020-03-11 19:49:56 +0530

    Added dynamic valdation

--- cla-frontend-project-console/src/ionic/pages/project/project-cla-template/project-cla-template.ts
+++ cla-frontend-project-console/src/ionic/pages/project/project-cla-template/project-cla-template.ts
@@ -1,12 +1,13 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
 import { IonicPage, Nav, NavController, NavParams } from 'ionic-angular';
 import { ClaService } from '../../../services/cla.service';
 import { Restricted } from '../../../decorators/restricted';
 import { DomSanitizer } from '@angular/platform-browser';
 import { RolesService } from '../../../services/roles.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Restricted({
   roles: ['isAuthenticated', 'isPmcUser']
@@ -35,9 +36,9 @@ export class ProjectClaTemplatePage {
   loading = {
     documents: false
   };
-
+  @Input() form: FormGroup;
   @ViewChild(Nav) nav: Nav;
-
+  submitAttempt = false;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -79,39 +80,68 @@ export class ProjectClaTemplatePage {
 
   selectTemplate(template) {
     this.selectedTemplate = template;
+    this.generateFormGroup();
     this.step = 'values';
   }
 
-  reviewSelectedTemplate() {
-    this.setLoadingSpinner(true);
-    this.buttonGenerateEnabled = false;
-    this.message = 'Generating PDFs...';
-
+  generateFormGroup() {
+    let group = {};
     const metaFields = this.selectedTemplate.metaFields;
-    metaFields.forEach((metaField) => {
-      if (this.templateValues.hasOwnProperty(metaField.templateVariable)) {
-        metaField.value = this.templateValues[metaField.templateVariable];
-      }
-    });
-    let data = {
-      templateID: this.selectedTemplate.ID,
-      metaFields: metaFields
-    };
+    metaFields.forEach(template => {
+      let field = this.getFieldName(template);
+      group[field] = new FormControl('', Validators.required);
+    })
+    this.form = new FormGroup(group);
+  }
 
-    this.claService.postClaGroupTemplate(this.projectId, data).subscribe(
-      (response) => {
-        this.setLoadingSpinner(false);
-        this.buttonGenerateEnabled = true;
-        this.message = null;
-        this.pdfPath = response;
-        this.goToStep('review');
-      },
-      (error) => {
-        this.setLoadingSpinner(false);
-        this.buttonGenerateEnabled = true;
-        this.message = 'Error creating PDFs: ' + error;
+  getFieldName(template) {
+    return template.name.split(' ').join('');
+  }
+
+  validateEmail(template: string) {
+    if (template.toLowerCase().indexOf('email') >= 0) {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(template)) {
+        return true
+      } else {
+        return false;
       }
-    );
+    }
+    return null;
+  }
+
+  reviewSelectedTemplate() {
+    this.submitAttempt = true;
+    if (this.form.valid) {
+      this.setLoadingSpinner(true);
+      this.buttonGenerateEnabled = false;
+      this.message = 'Generating PDFs...';
+
+      const metaFields = this.selectedTemplate.metaFields;
+      metaFields.forEach((metaField) => {
+        if (this.templateValues.hasOwnProperty(metaField.templateVariable)) {
+          metaField.value = this.templateValues[metaField.templateVariable];
+        }
+      });
+      let data = {
+        templateID: this.selectedTemplate.ID,
+        metaFields: metaFields
+      };
+
+      this.claService.postClaGroupTemplate(this.projectId, data).subscribe(
+        (response) => {
+          this.setLoadingSpinner(false);
+          this.buttonGenerateEnabled = true;
+          this.message = null;
+          this.pdfPath = response;
+          this.goToStep('review');
+        },
+        (error) => {
+          this.setLoadingSpinner(false);
+          this.buttonGenerateEnabled = true;
+          this.message = 'Error creating PDFs: ' + error;
+        }
+      );
+    }
   }
 
   goToStep(step) {
