9a57a48ec43158b682abb9c422ce0373d1c022f4
David Deal <dealako@gmail.com>
2020-03-13 11:23:35 -0700

    Deployment Updates + LFX API Fix (#694)
    - Moved deployment routines to circle CI config (away from separate script)
    - Fixed infra index update script (needed to declare the column)
    - Updated the security context for the LFX v2 APIs
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- infra/index.ts
+++ infra/index.ts
@@ -628,6 +628,7 @@ function buildEventsTable(importResources: boolean): aws.dynamodb.Table {
         { name: 'event_type', type: 'S' },
         { name: 'event_user_id', type: 'S' },
         { name: 'event_project_id', type: 'S' },
+        { name: 'event_date_and_contains_pii', type: 'B' },
         { name: 'event_time_epoch', type: 'N' },
       ],
       hashKey: 'event_id',
