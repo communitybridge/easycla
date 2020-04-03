70c9d8e1f1f73bff3b63ba26b28471fd1b4a3234
Prasanna Mahajan <mahajanprasannap@gmail.com>
2020-04-03 22:26:11 +0530

    Feature/GitHub repositories api migration (#815)
    * adding swagger spec for github repositories api
    * adding add/delete/list method for github repository
    * Adding repositories add/delete/list api
    * Returning extra info about github repositories of github organization
    installation
    Checking if github repo already exist or not before inserting github
    repo
    * checking that user have access to perform operation on project
    * Adding repositories api to v4
    * adding audit events for github repositories api in v2
    * Adding functional test for github repositories
    * Adding audit events in github organization
    * Updating v4 github_organizations test
    
    Signed-off-by: prasanna.mahajan <mahajanprasannap@gmail.com>

---
+++ cla-backend-go/utils/auth_user.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/LF-Engineering/lfx-kit/auth"
+	log "github.com/communitybridge/easycla/cla-backend-go/logging"
+)
+
+// SetAuthUserProperties adds username and email to auth user
+func SetAuthUserProperties(authUser *auth.User, xUserName *string, xEmail *string) {
+
+	if xUserName != nil {
+		authUser.UserName = *xUserName
+	}
+	if xEmail != nil {
+		authUser.Email = *xEmail
+	}
+	log.Infof(fmt.Sprintf("authuser x-username:%s and x-email:%s", authUser.UserName, authUser.Email))
+}
--- cla-backend-go/v2/github_organizations/handlers.go
+++ cla-backend-go/v2/github_organizations/handlers.go
@@ -5,18 +5,21 @@ package github_organizations
 
 import (
 	"github.com/LF-Engineering/lfx-kit/auth"
+	"github.com/communitybridge/easycla/cla-backend-go/events"
 	v1Models "github.com/communitybridge/easycla/cla-backend-go/gen/models"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/models"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/restapi/operations"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/restapi/operations/github_organizations"
 	v1GithubOrganizations "github.com/communitybridge/easycla/cla-backend-go/github_organizations"
+	"github.com/communitybridge/easycla/cla-backend-go/utils"
 	"github.com/go-openapi/runtime/middleware"
 )
 
 // Configure setups handlers on api with service
-func Configure(api *operations.EasyclaAPI, service v1GithubOrganizations.Service) {
+func Configure(api *operations.EasyclaAPI, service v1GithubOrganizations.Service, eventService events.Service) {
 	api.GithubOrganizationsGetProjectGithubOrganizationsHandler = github_organizations.GetProjectGithubOrganizationsHandlerFunc(
-		func(params github_organizations.GetProjectGithubOrganizationsParams, claUser *auth.User) middleware.Responder {
+		func(params github_organizations.GetProjectGithubOrganizationsParams, authUser *auth.User) middleware.Responder {
+			utils.SetAuthUserProperties(authUser, params.XUSERNAME, params.XEMAIL)
 			result, err := service.GetGithubOrganizations(params.ProjectSFID)
 			if err != nil {
 				return github_organizations.NewGetProjectGithubOrganizationsBadRequest().WithPayload(errorResponse(err))
@@ -24,22 +27,26 @@ func Configure(api *operations.EasyclaAPI, service v1GithubOrganizations.Service
 			return github_organizations.NewGetProjectGithubOrganizationsOK().WithPayload(*result)
 		})
 	api.GithubOrganizationsAddProjectGithubOrganizationHandler = github_organizations.AddProjectGithubOrganizationHandlerFunc(
-		func(params github_organizations.AddProjectGithubOrganizationParams, claUser *auth.User) middleware.Responder {
+		func(params github_organizations.AddProjectGithubOrganizationParams, authUser *auth.User) middleware.Responder {
+			utils.SetAuthUserProperties(authUser, params.XUSERNAME, params.XEMAIL)
 			result, err := service.AddGithubOrganization(params.ProjectSFID, &v1Models.CreateGithubOrganization{
 				OrganizationName: params.Body.OrganizationName,
 			})
 			if err != nil {
 				return github_organizations.NewAddProjectGithubOrganizationBadRequest().WithPayload(errorResponse(err))
 			}
+			addGithubOrganizationEvent(eventService, authUser, params.Body.OrganizationName)
 			return github_organizations.NewAddProjectGithubOrganizationOK().WithPayload(*result)
 		})
 
 	api.GithubOrganizationsDeleteProjectGithubOrganizationHandler = github_organizations.DeleteProjectGithubOrganizationHandlerFunc(
-		func(params github_organizations.DeleteProjectGithubOrganizationParams, claUser *auth.User) middleware.Responder {
+		func(params github_organizations.DeleteProjectGithubOrganizationParams, authUser *auth.User) middleware.Responder {
+			utils.SetAuthUserProperties(authUser, params.XUSERNAME, params.XEMAIL)
 			err := service.DeleteGithubOrganization(params.ProjectSFID, params.OrgName)
 			if err != nil {
 				return github_organizations.NewDeleteProjectGithubOrganizationBadRequest().WithPayload(errorResponse(err))
 			}
+			deleteGithubOrganizationEvent(eventService, authUser, params.OrgName)
 			return github_organizations.NewDeleteProjectGithubOrganizationOK()
 		})
 }
