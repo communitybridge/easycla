733f4eceabd432ecbf98721426c6c4f836f9a220
Amol Sontakke <amols@proximabiz.com>
2020-04-17 13:26:58 +0530

    Removed unused files and clean the code by removing unwanted logs and commented code. Issue: #871
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-frontend-project-console/src/ionic/modals/cla-contract-version-modal/cla-contract-version-modal.ts
+++ cla-frontend-project-console/src/ionic/modals/cla-contract-version-modal/cla-contract-version-modal.ts
@@ -8,10 +8,8 @@ import {
   ModalController,
   NavController,
   NavParams,
-  PopoverController,
   ViewController
 } from 'ionic-angular';
-import { ClaService } from '../../services/cla.service';
 
 @IonicPage({
   segment: 'cla-contract-version-modal'
@@ -21,7 +19,6 @@ import { ClaService } from '../../services/cla.service';
   templateUrl: 'cla-contract-version-modal.html'
 })
 export class ClaContractVersionModal {
-  claProjectId: string;
   documentType: string; // individual | corporate
   documents: any;
   currentDocument: any;
@@ -31,33 +28,22 @@ export class ClaContractVersionModal {
     public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController,
-    private popoverCtrl: PopoverController,
     public modalCtrl: ModalController,
-    private claService: ClaService,
     public events: Events
   ) {
-    this.claProjectId = this.navParams.get('claProjectId');
     this.documentType = this.navParams.get('documentType');
     this.documents = this.navParams.get('documents').reverse();
     if (this.documents.length > 0) {
       this.currentDocument = this.documents.slice(0, 1);
-      console.log(this.documentType + ' document:');
-      console.log(this.currentDocument);
       if (this.documents.length > 1) {
         this.previousDocuments = this.documents.slice(1);
       }
-    } else {
-      console.log('No documents for project: ' + this.claProjectId);
     }
 
     events.subscribe('modal:close', () => {
       this.dismiss();
     });
-
-    this.getDefaults();
   }
-
-  getDefaults() {}
 
   ngOnInit() {}
 
@@ -80,7 +66,6 @@ export class ClaContractVersionModal {
         ' - document URL is empty.' +
         ' For further assistance with EasyCLA, please ' +
         '<a href="https://jira.linuxfoundation.org/servicedesk/customer/portal/4">submit a support request ticket</a>.';
-      console.log(msg);
       const height = Math.floor(window.innerHeight * 0.8);
       const width = Math.floor(window.innerWidth * 0.8);
       const myWindow = window.open('', '_blank', 'titlebar=yes,width=' + width + ',height=' + height);
