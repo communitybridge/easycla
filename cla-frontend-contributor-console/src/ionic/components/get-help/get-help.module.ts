d7196cb9f46ad2d205739b2d1e88d9fc4e926ae7
hnexokonkwo <henryokonkwo01@gmail.com>
2020-04-19 12:29:38 +0100

    [#907 / #818] - Help Ribbon - Fixed closing tag issue
    Signed-off-by: hnexokonkwo <henryokonkwo01@gmail.com>

---
+++ cla-frontend-contributor-console/src/ionic/components/get-help/get-help.module.ts
@@ -0,0 +1,13 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { GetHelpComponent } from './get-help';
+
+@NgModule({
+  declarations: [GetHelpComponent],
+  imports: [IonicModule],
+  exports: [GetHelpComponent]
+})
+export class GetHelpComponentModule {}
---
+++ cla-frontend-contributor-console/src/ionic/components/get-help/get-help.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the GetHelpComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'get-help',
+  templateUrl: 'get-help.html'
+})
+export class GetHelpComponent {
+
+  constructor() {
+  }
+
+}
