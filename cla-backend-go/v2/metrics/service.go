7a97cd8fadcc35922c062fac9e253d34253da41c
Prasanna Mahajan <mahajanprasannap@gmail.com>
2020-04-30 23:48:14 +0530

    cleaning the swagger spec (#1035)

--- cla-backend-go/v2/metrics/service.go
+++ cla-backend-go/v2/metrics/service.go
@@ -9,7 +9,7 @@ import (
 
 	log "github.com/communitybridge/easycla/cla-backend-go/logging"
 
-	"github.com/communitybridge/easycla/cla-backend-go/gen/models"
+	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/models"
 )
 
 // Service interface defines function of Metrics service
