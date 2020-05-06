31d18ce17cd84473dc948ea441d406aab084c51c
Prasanna Mahajan <mahajanprasannap@gmail.com>
2020-05-06 20:43:10 +0530

    [#930] Using companySFID for GetRecentCompanyProjectEvents api (#1069)

--- cla-backend-go/v2/events/handlers.go
+++ cla-backend-go/v2/events/handlers.go
@@ -2,15 +2,27 @@ package events
 
 import (
 	"github.com/LF-Engineering/lfx-kit/auth"
+	v1Company "github.com/communitybridge/easycla/cla-backend-go/company"
 	v1Events "github.com/communitybridge/easycla/cla-backend-go/events"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/models"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/restapi/operations"
+	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/restapi/operations/company"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/restapi/operations/events"
+	"github.com/communitybridge/easycla/cla-backend-go/utils"
 	"github.com/go-openapi/runtime/middleware"
 )
 
+func isUserAuthorizedForOrganization(user *auth.User, externalCompanyID string) bool {
+	if !user.Admin {
+		if !user.Allowed || !user.IsUserAuthorized(auth.Organization, externalCompanyID) {
+			return false
+		}
+	}
+	return true
+}
+
 // Configure setups handlers on api with service
-func Configure(api *operations.EasyclaAPI, service v1Events.Service) {
+func Configure(api *operations.EasyclaAPI, service v1Events.Service, v1CompanyRepo v1Company.IRepository) {
 	api.EventsGetRecentEventsHandler = events.GetRecentEventsHandlerFunc(
 		func(params events.GetRecentEventsParams, user *auth.User) middleware.Responder {
 			result, err := service.GetRecentEvents(params.PageSize)
@@ -21,8 +33,18 @@ func Configure(api *operations.EasyclaAPI, service v1Events.Service) {
 		})
 
 	api.EventsGetRecentCompanyProjectEventsHandler = events.GetRecentCompanyProjectEventsHandlerFunc(
-		func(params events.GetRecentCompanyProjectEventsParams, user *auth.User) middleware.Responder {
-			result, err := service.GetRecentEventsForCompanyProject(params.CompanyID, params.ProjectSFID, params.PageSize)
+		func(params events.GetRecentCompanyProjectEventsParams, authUser *auth.User) middleware.Responder {
+			utils.SetAuthUserProperties(authUser, params.XUSERNAME, params.XEMAIL)
+			if !isUserAuthorizedForOrganization(authUser, params.CompanySFID) {
+				return events.NewGetRecentCompanyProjectEventsUnauthorized()
+			}
+			comp, err := v1CompanyRepo.GetCompanyByExternalID(params.CompanySFID)
+			if err != nil {
+				if err == v1Company.ErrCompanyDoesNotExist {
+					return company.NewGetCompanyClaManagersNotFound()
+				}
+			}
+			result, err := service.GetRecentEventsForCompanyProject(comp.CompanyID, params.ProjectSFID, params.PageSize)
 			if err != nil {
 				return events.NewGetRecentEventsBadRequest().WithPayload(errorResponse(err))
 			}
