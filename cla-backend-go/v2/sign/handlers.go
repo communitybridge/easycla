d6cf8d0336d84ee7efd3220f92e3c4a87c1df514
Prasanna Mahajan <mahajanprasannap@gmail.com>
2020-05-20 22:17:42 +0530

    Feature/1104 v2 request corporate signature (#1125)

---
+++ cla-backend-go/v2/sign/handlers.go
@@ -0,0 +1,52 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
+package sign
+
+import (
+	"strings"
+
+	"github.com/LF-Engineering/lfx-kit/auth"
+	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/models"
+	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/restapi/operations"
+	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/restapi/operations/sign"
+	"github.com/communitybridge/easycla/cla-backend-go/utils"
+	"github.com/go-openapi/runtime/middleware"
+)
+
+// Configure API call
+func Configure(api *operations.EasyclaAPI, service Service) {
+	// Retrieve a list of available templates
+	api.SignRequestCorporateSignatureHandler = sign.RequestCorporateSignatureHandlerFunc(
+		func(params sign.RequestCorporateSignatureParams, user *auth.User) middleware.Responder {
+			if !user.IsUserAuthorizedByProject(utils.StringValue(params.Input.ProjectSfid), utils.StringValue(params.Input.CompanySfid)) {
+				return sign.NewRequestCorporateSignatureUnauthorized()
+			}
+			resp, err := service.RequestCorporateSignature(params.Authorization, params.Input)
+			if err != nil {
+				if strings.Contains(err.Error(), "does not exist") {
+					return sign.NewRequestCorporateSignatureNotFound().WithPayload(errorResponse(err))
+				}
+				return sign.NewRequestCorporateSignatureBadRequest().WithPayload(errorResponse(err))
+			}
+			return sign.NewRequestCorporateSignatureOK().WithPayload(resp)
+		})
+}
+
+type codedResponse interface {
+	Code() string
+}
+
+func errorResponse(err error) *models.ErrorResponse {
+	code := ""
+	if e, ok := err.(codedResponse); ok {
+		code = e.Code()
+	}
+
+	e := models.ErrorResponse{
+		Code:    code,
+		Message: err.Error(),
+	}
+
+	return &e
+}
