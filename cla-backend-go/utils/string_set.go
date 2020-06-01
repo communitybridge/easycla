3affe0013088fdb13465d54c21e53163f03d725b
Prasanna Mahajan <mahajanprasannap@gmail.com>
2020-06-01 23:08:53 +0530

    adding projectSFID in the input for getActiveCLA api and  getCLAManagers api (#1151)
    Co-authored-by: David Deal <dealako@gmail.com>

--- cla-backend-go/utils/string_set.go
+++ cla-backend-go/utils/string_set.go
@@ -15,6 +15,17 @@ func (ss *StringSet) Add(v string) {
 	ss.set[v] = nil
 }
 
+// Length is the length of string set
+func (ss *StringSet) Length() int {
+	return len(ss.set)
+}
+
+// Include tells if the string to string set
+func (ss *StringSet) Include(v string) bool {
+	_, ok := ss.set[v]
+	return ok
+}
+
 // List returns list of strings in StringSet
 func (ss *StringSet) List() []string {
 	var list []string
