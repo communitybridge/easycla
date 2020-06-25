2bed23fa9ab090f5102ec449e27a89b301d3865e
David Deal <dealako@gmail.com>
2020-06-24 18:29:35 -0700

    [#1308] Updated CLA Enabled Endpoint (#1312)
    - Added additional flags to indicate if ICLA, CCLA and ICLA enabled with CCLA flags
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/v2/project/service.go
+++ cla-backend-go/v2/project/service.go
@@ -4,69 +4,57 @@ import (
 	"sort"
 
 	"github.com/aws/aws-sdk-go/aws"
-	v1Models "github.com/communitybridge/easycla/cla-backend-go/gen/models"
 	v1ProjectParams "github.com/communitybridge/easycla/cla-backend-go/gen/restapi/operations/project"
 	"github.com/communitybridge/easycla/cla-backend-go/gen/v2/models"
 	log "github.com/communitybridge/easycla/cla-backend-go/logging"
+	v1Project "github.com/communitybridge/easycla/cla-backend-go/project"
 	v2ProjectService "github.com/communitybridge/easycla/cla-backend-go/v2/project-service"
 	v2ProjectServiceModels "github.com/communitybridge/easycla/cla-backend-go/v2/project-service/models"
 )
 
 // Service interface defines the v2 project service methods
 type Service interface {
-	GetCLAProjectsByID(projectSfdcID string) (*models.EnabledClaList, error)
-}
-
-//Repo interface defines Project repository methods in v1
-type Repo interface {
-	GetProjectsByExternalID(params *v1ProjectParams.GetProjectsByExternalIDParams, loadRepoDetails bool) (*v1Models.Projects, error)
+	GetCLAProjectsByID(foundationSFID string) (*models.EnabledClaList, error)
 }
 
 // service
 type service struct {
-	projectRepo Repo
+	projectRepo v1Project.ProjectRepository
 }
 
 // NewService returns an instance of v2 project service
-func NewService(projectRepo Repo) Service {
+func NewService(projectRepo v1Project.ProjectRepository) Service {
 	return &service{
 		projectRepo: projectRepo,
 	}
 }
-func (s *service) GetCLAProjectsByID(projectSfdcID string) (*models.EnabledClaList, error) {
+
+func (s *service) GetCLAProjectsByID(foundationSFID string) (*models.EnabledClaList, error) {
 	var enabledClas []*models.EnabledCla
 	psc := v2ProjectService.GetClient()
-	projectDetails, err := psc.GetProject(projectSfdcID)
-	enabledClas = make([]*models.EnabledCla, 0, len(projectDetails.Projects))
+	projectDetails, err := psc.GetProject(foundationSFID)
 	if err != nil {
 		log.Error("Unable to get project details", err)
 		return nil, err
 	}
 
-	// Filter claProjects from project service Projects
-	subProjects := s.filterClaProjects(projectDetails.Projects)
-	for _, subProject := range subProjects {
-		sp := &models.EnabledCla{
-			ProjectName: subProject.Name,
-			ProjectSfid: subProject.ID,
-			ProjectLogo: subProject.ProjectLogo,
-			ProjectType: subProject.ProjectType,
-		}
-		enabledClas = append(enabledClas, sp)
-	}
+	// Filter claProjects and convert to response model
+	enabledClas = s.filterClaProjects(projectDetails.Projects)
+	log.Warnf("output: %+v", enabledClas)
 
 	// sort by project names
 	sort.Slice(enabledClas, func(i, j int) bool {
 		return enabledClas[i].ProjectName < enabledClas[j].ProjectName
 	})
-	return &models.EnabledClaList{List: enabledClas}, nil
 
+	return &models.EnabledClaList{List: enabledClas}, nil
 }
 
-// return projects output for which cla_group is present in cla
-func (s *service) filterClaProjects(projects []*v2ProjectServiceModels.ProjectOutput) []*v2ProjectServiceModels.ProjectOutput {
-	results := make([]*v2ProjectServiceModels.ProjectOutput, 0)
-	prChan := make(chan *v2ProjectServiceModels.ProjectOutput)
+// filterClaProjects return projects output for which cla_group is present in cla
+func (s *service) filterClaProjects(projects []*v2ProjectServiceModels.ProjectOutput) []*models.EnabledCla {
+	results := make([]*models.EnabledCla, 0)
+	prChan := make(chan *models.EnabledCla)
+
 	for _, v := range projects {
 		go func(projectOutput *v2ProjectServiceModels.ProjectOutput) {
 			project, err := s.projectRepo.GetProjectsByExternalID(&v1ProjectParams.GetProjectsByExternalIDParams{
@@ -88,14 +76,26 @@ func (s *service) filterClaProjects(projects []*v2ProjectServiceModels.ProjectOu
 				prChan <- nil
 				return
 			}
-			prChan <- projectOutput
+
+			prChan <- &models.EnabledCla{
+				ProjectName:      projectOutput.Name,
+				ProjectSfid:      projectOutput.ID,
+				ProjectLogo:      projectOutput.ProjectLogo,
+				ProjectType:      projectOutput.ProjectType,
+				CclaEnabled:      aws.Bool(claProject.ProjectCCLAEnabled),
+				IclaEnabled:      aws.Bool(claProject.ProjectICLAEnabled),
+				CclaRequiresIcla: aws.Bool(claProject.ProjectCCLARequiresICLA),
+			}
 		}(v)
 	}
+
 	for range projects {
 		project := <-prChan
 		if project != nil {
+			log.Warnf("Adding project to channel: %+v", project)
 			results = append(results, project)
 		}
 	}
+
 	return results
 }
