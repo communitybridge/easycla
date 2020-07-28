cc6e7b281d9b8a10c4ae0015b231e1d8000097f4
David Deal <dealako@gmail.com>
2020-07-28 13:31:36 -0700

    CCLA PDF Download Email Link Update
    - Added support for both ICLA and CCLA download via email
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/signatures/handlers.go
+++ cla-backend-go/signatures/handlers.go
@@ -21,19 +21,18 @@ import (
 )
 
 // Configure setups handlers on api with service
-func Configure(api *operations.ClaAPI, service SignatureService, sessionStore *dynastore.Store, eventsService events.Service) {
+func Configure(api *operations.ClaAPI, service SignatureService, sessionStore *dynastore.Store, eventsService events.Service) { // nolint
 
 	api.SignaturesGetSignedICLADocumentHandler = signatures.GetSignedICLADocumentHandlerFunc(func(params signatures.GetSignedICLADocumentParams) middleware.Responder {
 		signatureModel, sigErr := service.GetIndividualSignature(params.ClaGroupID, params.UserID)
 		if sigErr != nil {
-			msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error -  error retrieving signature using claGroupID: %s, userID: %s, error: %+v",
+			msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error -  error retrieving signature using ClaGroupID: %s, userID: %s, error: %+v",
 				params.ClaGroupID, params.UserID, sigErr)
 			log.Warn(msg)
 			return signatures.NewGetSignedICLADocumentInternalServerError().WithPayload(&models.ErrorResponse{
 				Code:    "500",
 				Message: msg,
 			})
-
 		}
 
 		if signatureModel == nil {
@@ -51,7 +50,7 @@ func Configure(api *operations.ClaAPI, service SignatureService, sessionStore *d
 		log.Debugf("Retrieving PDF from path: %s", downloadURL)
 		downloadLink, s3Err := utils.GetDownloadLink(downloadURL)
 		if s3Err != nil {
-			msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error -  unable to locate PDF from source using claGroupID: %s, userID: %s, s3 error: %+v",
+			msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error -  unable to locate PDF from source using ClaGroupID: %s, userID: %s, s3 error: %+v",
 				params.ClaGroupID, params.UserID, s3Err)
 			log.Warn(msg)
 			return signatures.NewGetSignedICLADocumentInternalServerError().WithPayload(&models.ErrorResponse{
@@ -63,17 +62,62 @@ func Configure(api *operations.ClaAPI, service SignatureService, sessionStore *d
 		return middleware.ResponderFunc(func(rw http.ResponseWriter, p runtime.Producer) {
 			rw.Header().Set("Content-type", "text/html")
 			rw.WriteHeader(200)
-			redirectDocument := fmt.Sprintf(
-				`<html>
-							<head><meta http-equiv="Refresh" content="0; url='%s'"/></head>
-                            <body>
-                              <p>Please follow <a href="%s">this link</a>.</p>
-                            </body>
-                        </html>`, downloadLink, downloadLink)
+			redirectDocument := generateHTMLRedirectPage(downloadLink, "ICLA")
 			bytesWritten, writeErr := rw.Write([]byte(redirectDocument))
 			if writeErr != nil {
 				msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error - generating s3 redirect for the client client using source using claGroupID: %s, userID: %s, error: %+v",
 					params.ClaGroupID, params.UserID, s3Err)
+				log.Warn(msg)
+			}
+			log.Debugf("SignaturesGetSignedICLADocumentHandler - wrote %d bytes", bytesWritten)
+		})
+	})
+
+	api.SignaturesGetSignedCCLADocumentHandler = signatures.GetSignedCCLADocumentHandlerFunc(func(params signatures.GetSignedCCLADocumentParams) middleware.Responder {
+		signatureModel, sigErr := service.GetCorporateSignature(params.ClaGroupID, params.CompanyID)
+		if sigErr != nil {
+			msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error -  error retrieving signature using ClaGroupID: %s, CompanyID: %s, error: %+v",
+				params.ClaGroupID, params.CompanyID, sigErr)
+			log.Warn(msg)
+			return signatures.NewGetSignedCCLADocumentInternalServerError().WithPayload(&models.ErrorResponse{
+				Code:    "500",
+				Message: msg,
+			})
+
+		}
+
+		if signatureModel == nil {
+			msg := fmt.Sprintf("EasyCLA - 404 Not Found - -  error retrieving signature using ClaGroupID: %s, CompanyID: %s",
+				params.ClaGroupID, params.CompanyID)
+			log.Warn(msg)
+			return signatures.NewGetSignedCCLADocumentNotFound().WithPayload(&models.ErrorResponse{
+				Code:    "404",
+				Message: msg,
+			})
+		}
+
+		downloadURL := fmt.Sprintf("contract-group/%s/ccla/%s/%s.pdf",
+			params.ClaGroupID, params.CompanyID, signatureModel.SignatureID)
+		log.Debugf("Retrieving PDF from path: %s", downloadURL)
+		downloadLink, s3Err := utils.GetDownloadLink(downloadURL)
+		if s3Err != nil {
+			msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error -  unable to locate PDF from source using ClaGroupID: %s, CompanyID: %s, s3 error: %+v",
+				params.ClaGroupID, params.CompanyID, s3Err)
+			log.Warn(msg)
+			return signatures.NewGetSignedCCLADocumentInternalServerError().WithPayload(&models.ErrorResponse{
+				Code:    "500",
+				Message: msg,
+			})
+		}
+
+		return middleware.ResponderFunc(func(rw http.ResponseWriter, p runtime.Producer) {
+			rw.Header().Set("Content-type", "text/html")
+			rw.WriteHeader(200)
+			redirectDocument := generateHTMLRedirectPage(downloadLink, "CCLA")
+			bytesWritten, writeErr := rw.Write([]byte(redirectDocument))
+			if writeErr != nil {
+				msg := fmt.Sprintf("EasyCLA - 500 Internal Server Error - generating s3 redirect for the client client using source using ClaGroupID: %s, CompanyID: %s, error: %+v",
+					params.ClaGroupID, params.CompanyID, s3Err)
 				log.Warn(msg)
 			}
 			log.Debugf("SignaturesGetSignedICLADocumentHandler - wrote %d bytes", bytesWritten)
@@ -301,4 +345,28 @@ func errorResponse(err error) *models.ErrorResponse {
 	}
 
 	return &e
+}
+
+func generateHTMLRedirectPage(downloadLink, claType string) string {
+	return fmt.Sprintf(
+		`<html lang="en">
+							<head>
+                               <title>The Linux Foundation – EasyCLA %s PDF Redirect</title>
+                               <meta http-equiv="Refresh" content="0; url='%s'"/>
+                               <meta charset="utf-8">
+                               <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                               <link rel="shortcut icon" href="https://www.linuxfoundation.org/wp-content/uploads/2017/08/favicon.png">
+                               <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+                               <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                            </head>
+                            <body style='margin-top:20;margin-left:0;margin-right:0;'>
+                              <div class="text-center">
+                                <img width=300px" src="https://cla-project-logo-prod.s3.amazonaws.com/lf-horizontal-color.svg" alt="community bridge logo"/>
+                              </div>
+                              <h2 class="text-center">EasyCLA %s PDF Redirect Authorization</h2>
+                              <p class="text-center">
+                                 <a href="%s" class="btn btn-primary" role="button">Proceed To Download</a>
+                              </p>
+                            </body>
+                        </html>`, claType, downloadLink, claType, downloadLink)
 }
