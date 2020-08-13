9304adeaa36899b348bd430d894902a97f701d8a
Amol Sontakke <amols@proximabiz.com>
2020-08-13 17:58:08 +0530

    [#1555] Added regex validation for company name and url
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-frontend-project-console/src/ionic/constants/general.ts
+++ cla-frontend-project-console/src/ionic/constants/general.ts
@@ -10,4 +10,6 @@ export const generalConstants = {
     createTicketURL: 'https://jira.linuxfoundation.org/servicedesk/customer/portal/4',
     easyCLAHelpURL: 'https://lf-docs-linux-foundation.gitbook.io/easycla/getting-started/easycla-faqs',
     getAccessURL: 'https://jira.linuxfoundation.org/servicedesk/customer/portal/4',
+    CLA_GROUP_NAME_REGEX: /^([\w\p{L}][\w\s\p{L}()\[\]\.\,+-_]*)$/,
+    CLA_GROUP_DESCRIPTION_REGEX: /^([\w\p{L}][\w\s\p{L}()\[\]\.\,+-_]*)$/,
 }
