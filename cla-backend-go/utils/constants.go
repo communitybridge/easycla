d1692033fc234ca73f08a54972820b4507e2002e
David Deal <dealako@gmail.com>
2020-09-09 21:11:48 -0700

    [#1503] CLA Group Delete Updates
    - Refactored CLA Group delete logic to specify the correct Project SFID
      when removing roles
    - Added go routines to do a number of cleanup activities concurrently
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/utils/constants.go
+++ cla-backend-go/utils/constants.go
@@ -8,3 +8,15 @@ const (
 	// TheLinuxFoundation is the name of the super parent for many Salesforce Foundations/Project Groups
 	TheLinuxFoundation = "The Linux Foundation"
 )
+
+// CLADesigneeRole CLA manager designee role identifier
+const CLADesigneeRole = "cla-manager-designee"
+
+// CLAManagerRole CLA manager role identifier
+const CLAManagerRole = "cla-manager"
+
+// CLASignatoryRole CLA signatory role identifier
+const CLASignatoryRole = "cla-signatory"
+
+// Lead representing type of user
+const Lead = "lead"
