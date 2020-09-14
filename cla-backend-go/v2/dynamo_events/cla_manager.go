f4e9303f6511d144f2d60ea7d352734e9d7134e5
David Deal <dealako@gmail.com>
2020-09-14 15:12:00 -0700

    [#1762] Added Phase 1 of CLA Permissions Cleanup
    - Added additional logging
    - Added hooks for querying and removing CLA Group permissions for
      assigned roles (cla-manager, cla-manager-designee, and cla-signatory).
      Waiting on feedback to determine API for this.
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/v2/dynamo_events/cla_manager.go
+++ cla-backend-go/v2/dynamo_events/cla_manager.go
@@ -17,27 +17,33 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// constants
-const (
-	RoleCLAManager         = "cla-manager"
-	RoleCLAManagerDesignee = "cla-manager-designee"
-)
-
+// SetInitialCLAManagerACSPermissions
 func (s *service) SetInitialCLAManagerACSPermissions(signatureID string) error {
+	f := logrus.Fields{
+		"functionName": "SetInitialCLAManagerACSPermissions",
+		"signatureID":  signatureID,
+	}
+
 	sig, err := s.signatureRepo.GetSignature(signatureID)
 	if err != nil {
+		log.WithFields(f).Warnf("problem locating signature by ID, error: %+v", err)
 		return err
 	}
+
+	f["signatureType"] = sig.SignatureType
 	if sig.SignatureType != CCLASignatureType {
-		return fmt.Errorf("invalid signature set initial cla manager request. %s", signatureID)
+		log.WithFields(f).Warn("invalid signature type for setting initial cla manager request")
+		return fmt.Errorf("invalid signature type for setting initial cla manager request. %s", signatureID)
 	}
 	if len(sig.SignatureACL) == 0 {
+		log.WithFields(f).Warn("initial cla manager details not found")
 		return errors.New("initial cla manager details not found")
 	}
 	// get user details
 	userServiceClient := v2UserService.GetClient()
 	claManager, err := userServiceClient.GetUserByUsername(sig.SignatureACL[0].LfUsername)
 	if err != nil {
+		log.WithFields(f).Warnf("unable to lookup user by username: %s", sig.SignatureACL[0].LfUsername)
 		return err
 	}
 	// get email id from the user details
@@ -49,18 +55,24 @@ func (s *service) SetInitialCLAManagerACSPermissions(signatureID string) error {
 	}
 	company, err := s.companyRepo.GetCompany(sig.SignatureReferenceID.String())
 	if err != nil {
+		log.WithFields(f).Warnf("unable to lookup company by ID: %s, error: %+v",
+			sig.SignatureReferenceID.String(), err)
 		return err
 	}
 
 	// fetch list of projects under cla group
 	projectList, err := s.projectsClaGroupRepo.GetProjectsIdsForClaGroup(sig.ProjectID)
 	if err != nil {
+		log.WithFields(f).Warnf("unable to fetch list of projects associated with CLA Group: %s, error: %+v",
+			sig.ProjectID, err)
 		return err
 	}
 
 	// Assign cla manager role based on level
 	err = s.assignCLAManager(email, claManager.Username, company.CompanyExternalID, projectList)
 	if err != nil {
+		log.WithFields(f).Warnf("unable to assign CLA Manager %s for company: %s, error: %+v",
+			claManager.Username, company.CompanyExternalID, err)
 		return err
 	}
 
@@ -68,6 +80,13 @@ func (s *service) SetInitialCLAManagerACSPermissions(signatureID string) error {
 }
 
 func (s service) assignCLAManager(email, username, companySFID string, projectList []*projects_cla_groups.ProjectClaGroup) error {
+	f := logrus.Fields{
+		"functionName": "assignCLAManager",
+		"email":        email,
+		"username":     username,
+		"companySFID":  companySFID,
+	}
+
 	// check if project is signed at foundation level
 	foundationID := projectList[0].FoundationSFID
 	signedAtFoundation, signedErr := s.projectService.SignedAtFoundationLevel(foundationID)
@@ -75,14 +94,17 @@ func (s service) assignCLAManager(email, username, companySFID string, projectLi
 		return signedErr
 	}
 	acsClient := v2AcsService.GetClient()
-	claManagerRoleID, roleErr := acsClient.GetRoleID(RoleCLAManager)
+	claManagerRoleID, roleErr := acsClient.GetRoleID(utils.CLAManagerRole)
 	if roleErr != nil {
+		log.WithFields(f).Warnf("problem getting role for %s, error: %+v", utils.CLAManagerRole, roleErr)
 		return roleErr
 	}
 	orgService := v2OrgService.GetClient()
 
-	scopes, err := orgService.ListOrgUserScopes(companySFID, []string{RoleCLAManager, RoleCLAManagerDesignee})
+	scopes, err := orgService.ListOrgUserScopes(companySFID, []string{utils.CLAManagerRole, utils.CLADesigneeRole})
 	if err != nil {
+		log.WithFields(f).Warnf("problem listing organization user scopes for company: %s, error: %+v",
+			companySFID, err)
 		return err
 	}
 
@@ -101,7 +123,7 @@ func (s service) assignCLAManager(email, username, companySFID string, projectLi
 		// delete cla manager designee role for foundation
 		for _, userRole := range scopes.Userroles {
 			for _, roleScope := range userRole.RoleScopes {
-				if roleScope.RoleName == RoleCLAManagerDesignee {
+				if roleScope.RoleName == utils.CLADesigneeRole {
 					for _, scope := range roleScope.Scopes {
 						tmp := strings.Split(scope.ObjectID, "|")
 						projectSFID := tmp[0]
@@ -137,7 +159,7 @@ func (s service) assignCLAManager(email, username, companySFID string, projectLi
 		// delete all cla-manager designee for all project of cla-group
 		for _, userRole := range scopes.Userroles {
 			for _, roleScope := range userRole.RoleScopes {
-				if roleScope.RoleName == RoleCLAManagerDesignee {
+				if roleScope.RoleName == utils.CLADesigneeRole {
 					for _, scope := range roleScope.Scopes {
 						tmp := strings.Split(scope.ObjectID, "|")
 						projectSFID := tmp[0]
