7fc25ff1bc5d2a2dc4b71b8f43332055616e72b8
Amol Sontakke <amols@proximabiz.com>
2020-09-24 14:01:18 +0530

    [#1813] Added linux foundation copyright in each file
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-landing-page/src/environments/environment.dev.ts
+++ cla-landing-page/src/environments/environment.dev.ts
@@ -1,3 +1,6 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
 export const environment = {
     production: false,
     LFX_HEADER_URL: 'https://cdn.dev.platform.linuxfoundation.org/lfx-header.js'
--- cla-landing-page/src/environments/environment.ts
+++ cla-landing-page/src/environments/environment.ts
@@ -1,3 +1,6 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
