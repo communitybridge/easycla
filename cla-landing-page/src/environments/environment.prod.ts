8c34d502999a1b96ee0c9d04db98a47eab215a87
Amol Sontakke <amols@proximabiz.com>
2020-09-30 16:10:21 +0530

    [#1813] Changed UI as per new wireframe and added environment specific URLs
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-landing-page/src/environments/environment.prod.ts
+++ cla-landing-page/src/environments/environment.prod.ts
@@ -3,5 +3,8 @@
 
 export const environment = {
   production: true,
-  LFX_HEADER_URL: 'https://cdn.platform.linuxfoundation.org/lfx-header.js'
+  LFX_HEADER_URL: 'https://cdn.platform.linuxfoundation.org/lfx-header.js',
+  ACCEPTABLE_USER_POLICY: 'https://communitybridge.platform.linuxfoundation.org/acceptable-use/',
+  SERVICE_SPECIFIC_TERM: 'https://communitybridge.platform.linuxfoundation.org/service-terms/',
+  PLATEFORM_USER_AGREEMENT: 'https://communitybridge.platform.linuxfoundation.org/platform-use-agreement/'
 };
--- cla-landing-page/src/environments/environment.staging.ts
+++ cla-landing-page/src/environments/environment.staging.ts
@@ -4,5 +4,8 @@
 
 export const environment = {
   production: true,
-  LFX_HEADER_URL: 'https://cdn.staging.platform.linuxfoundation.org/lfx-header.js'
+  LFX_HEADER_URL: 'https://cdn.staging.platform.linuxfoundation.org/lfx-header.js',
+  ACCEPTABLE_USER_POLICY: 'https://communitybridge.staging.platform.linuxfoundation.org/acceptable-use/',
+  SERVICE_SPECIFIC_TERM: 'https://communitybridge.staging.platform.linuxfoundation.org/service-terms/',
+  PLATEFORM_USER_AGREEMENT: 'https://communitybridge.staging.platform.linuxfoundation.org/platform-use-agreement/'
 };
