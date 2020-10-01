c13be856768b11af921dd720b01c4c8f06808495
Amol Sontakke <amols@proximabiz.com>
2020-10-01 12:50:04 +0530

    [#1813] Added auth0 redirection as per project or corporate console click
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-landing-page/src/app/components/cla-console-section/cla-console-section.component.ts
+++ cla-landing-page/src/app/components/cla-console-section/cla-console-section.component.ts
@@ -1,32 +1,24 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { EnvConfig } from 'src/app/config/cla-env-utils';
 import { AppSettings } from 'src/app/config/app-settings';
-
 
 @Component({
   selector: 'app-cla-console-section',
   templateUrl: './cla-console-section.component.html',
   styleUrls: ['./cla-console-section.component.scss']
 })
-export class ClaConsoleSectionComponent implements OnInit {
+export class ClaConsoleSectionComponent {
   @Input() consoleMetadata: any;
 
   constructor(
     private authService: AuthService
   ) { }
 
-  ngOnInit(): void {
-  }
-
-
   onClickProceed(type: string) {
-    const consoleType = type === 'Projects' ? AppSettings.PROJECT_CONSOLE_LINK : AppSettings.CORPORATE_CONSOLE_LINK;
-    const url = EnvConfig.default[consoleType];
-    window.open(url, '_self');
+    this.authService.login(type);
   }
 
   onClickRequestAccess() {
@@ -36,5 +28,4 @@ export class ClaConsoleSectionComponent implements OnInit {
   onClickLearnMore() {
     window.open(AppSettings.LEARN_MORE, '_blank');
   }
-
 }
--- cla-landing-page/src/environments/environment.staging.ts
+++ cla-landing-page/src/environments/environment.staging.ts
@@ -4,6 +4,9 @@
 
 export const environment = {
   production: true,
+  PROJECT_LOGIN_URL: 'https://project.staging.lfcla.com/',
+  CORPORATE_LOGIN_URL: 'https://corporate.staging.lfcla.com/',
+  CONTRIBUTOR_LOGIN_URL: 'https://contributor.staging.lfcla.com/',
   LFX_HEADER_URL: 'https://cdn.staging.platform.linuxfoundation.org/lfx-header.js',
   ACCEPTABLE_USER_POLICY: 'https://communitybridge.staging.platform.linuxfoundation.org/acceptable-use/',
   SERVICE_SPECIFIC_TERM: 'https://communitybridge.staging.platform.linuxfoundation.org/service-terms/',
