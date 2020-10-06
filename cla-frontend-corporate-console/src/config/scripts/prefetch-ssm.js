65ad6635e388f3265514e75783d8cd318d65f625
Amol Sontakke <amols@proximabiz.com>
2020-10-06 11:01:21 +0530

    [#1813] Working on corporate console header repalcement in internal pages
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-frontend-corporate-console/src/config/scripts/prefetch-ssm.js
+++ cla-frontend-corporate-console/src/config/scripts/prefetch-ssm.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 const RetrieveSSMValues = require('./read-ssm');
-const configVarArray = ['auth0-clientId', 'auth0-domain', 'cla-api-url', 'cla-logo-s3-url', 'corp-console-link'];
+const configVarArray = ['auth0-clientId', 'auth0-domain', 'cla-api-url', 'cla-logo-s3-url', 'corp-console-link', 'cla-lfx-header', 'cla-landing-page'];
 const region = 'us-east-1';
 const profile = process.env.AWS_PROFILE;
 const stageEnv = process.env.STAGE_ENV;
@@ -14,7 +14,7 @@ async function prefetchSSM() {
   result = await RetrieveSSMValues(configVarArray, stageEnv, region, profile);
 
   // result['cla-api-url'] = 'http://localhost:5000';
-  fs.writeFile(`./config/cla-env-config.json`, JSON.stringify(result), function(err) {
+  fs.writeFile(`./config/cla-env-config.json`, JSON.stringify(result), function (err) {
     if (err) throw new Error(`Couldn't save SSM parameters to disk with error ${err}`);
     console.log('Fetching completed...');
   });
