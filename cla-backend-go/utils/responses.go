c8e29df8338d48e6cbbdff358e47d0411e83380f
David Deal <dealako@gmail.com>
2020-10-12 14:52:06 -0700

    Added X-Request-ID to error response model
    - Added reqID from the request to the JSON resposne model for v2 APIs
    - Added IsUUIDv4 utility method to detect if the client provided a UUIDv4 or SFID
    - Added IsUUIDv4 unit test method
    - Added logic on employee signature to lookup the company ID via internal or external ID
    - Refactored AWS setup in CircleCI config
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/utils/responses.go
+++ cla-backend-go/utils/responses.go
@@ -10,65 +10,73 @@ import (
 )
 
 // ErrorResponseBadRequest Helper function to generate a bad request error response
-func ErrorResponseBadRequest(msg string) *models.ErrorResponse {
+func ErrorResponseBadRequest(reqID, msg string) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String400,
-		Message: fmt.Sprintf("%s - %s", EasyCLA400BadRequest, msg),
+		Code:       String400,
+		Message:    fmt.Sprintf("%s - %s", EasyCLA400BadRequest, msg),
+		XRequestID: reqID,
 	}
 }
 
 // ErrorResponseBadRequestWithError Helper function to generate a bad request error response
-func ErrorResponseBadRequestWithError(msg string, err error) *models.ErrorResponse {
+func ErrorResponseBadRequestWithError(reqID, msg string, err error) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String400,
-		Message: fmt.Sprintf("%s - %s - error: %+v", EasyCLA400BadRequest, msg, err),
+		Code:       String400,
+		Message:    fmt.Sprintf("%s - %s - error: %+v", EasyCLA400BadRequest, msg, err),
+		XRequestID: reqID,
 	}
 }
 
 // ErrorResponseForbidden Helper function to generate a forbidden error response
-func ErrorResponseForbidden(msg string) *models.ErrorResponse {
+func ErrorResponseForbidden(reqID, msg string) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String403,
-		Message: fmt.Sprintf("%s - %s", EasyCLA403Forbidden, msg),
+		Code:       String403,
+		Message:    fmt.Sprintf("%s - %s", EasyCLA403Forbidden, msg),
+		XRequestID: reqID,
 	}
 }
 
 // ErrorResponseForbiddenWithError Helper function to generate a forbidden error response
-func ErrorResponseForbiddenWithError(msg string, err error) *models.ErrorResponse {
+func ErrorResponseForbiddenWithError(reqID, msg string, err error) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String403,
-		Message: fmt.Sprintf("%s - %s - error: %+v", EasyCLA403Forbidden, msg, err),
+		Code:       String403,
+		Message:    fmt.Sprintf("%s - %s - error: %+v", EasyCLA403Forbidden, msg, err),
+		XRequestID: reqID,
 	}
 }
 
 // ErrorResponseNotFound Helper function to generate a not found error response
-func ErrorResponseNotFound(msg string) *models.ErrorResponse {
+func ErrorResponseNotFound(reqID, msg string) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String404,
-		Message: fmt.Sprintf("%s - %s", EasyCLA404NotFound, msg),
+		Code:       String404,
+		Message:    fmt.Sprintf("%s - %s", EasyCLA404NotFound, msg),
+		XRequestID: reqID,
 	}
 }
 
 // ErrorResponseNotFoundWithError Helper function to generate a not found error response
-func ErrorResponseNotFoundWithError(msg string, err error) *models.ErrorResponse {
+func ErrorResponseNotFoundWithError(reqID, msg string, err error) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String404,
-		Message: fmt.Sprintf("%s - %s - error: %+v", EasyCLA404NotFound, msg, err),
+		Code:       String404,
+		Message:    fmt.Sprintf("%s - %s - error: %+v", EasyCLA404NotFound, msg, err),
+		XRequestID: reqID,
 	}
 }
 
 // ErrorResponseInternalServerError Helper function to generate an internal server error response
-func ErrorResponseInternalServerError(msg string) *models.ErrorResponse {
+func ErrorResponseInternalServerError(reqID, msg string) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String500,
-		Message: fmt.Sprintf("%s - %s", EasyCLA500InternalServerError, msg),
+		Code:       String500,
+		Message:    fmt.Sprintf("%s - %s", EasyCLA500InternalServerError, msg),
+		XRequestID: reqID,
 	}
 }
 
 // ErrorResponseInternalServerErrorWithError Helper function to generate an internal server error response
-func ErrorResponseInternalServerErrorWithError(msg string, err error) *models.ErrorResponse {
+func ErrorResponseInternalServerErrorWithError(reqID, msg string, err error) *models.ErrorResponse {
 	return &models.ErrorResponse{
-		Code:    String500,
-		Message: fmt.Sprintf("%s - %s - error: %+v", EasyCLA500InternalServerError, msg, err),
+		Code:       String500,
+		Message:    fmt.Sprintf("%s - %s - error: %+v", EasyCLA500InternalServerError, msg, err),
+		XRequestID: reqID,
 	}
 }
