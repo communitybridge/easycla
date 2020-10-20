bb9d9259de0b12f24315557f7467bc1a4bae7954
Amol Sontakke <amols@proximabiz.com>
2020-10-20 17:34:28 +0530

    Solved redirection issue
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-frontend-corporate-console/src/ionic/pages/auth/auth.ts
+++ cla-frontend-corporate-console/src/ionic/pages/auth/auth.ts
@@ -26,12 +26,10 @@ export class AuthPage implements OnInit {
 
   ngOnInit() {
     this.authService.redirectRoot.subscribe((target) => {
-      if (EnvConfig['lfx-header-enabled'] === "true") {
-        this.navCtrl.setRoot('CompaniesPage');
-      } else {
-        // Redirected forcefully for without LFX-header due to auth token persist in URL. 
-        window.open(target, '_self');
-      }
+      window.history.replaceState(null, null, window.location.pathname);
+      this.navCtrl.setRoot('CompaniesPage');
     });
   }
 }
+
+
