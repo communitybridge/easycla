aa6ebf9457ad004b8adbf52a8c1c2d7b85fe0cea
Rinkesh Bhutwala <rbhutwal@proximabiz.com>
2020-10-27 15:40:50 +0530

    Added global init function in main file

--- cla-backend-go/main.go
+++ cla-backend-go/main.go
@@ -4,7 +4,13 @@
 package main
 
 import (
+	"log"
+
 	"github.com/communitybridge/easycla/cla-backend-go/cmd"
+	"github.com/communitybridge/easycla/cla-backend-go/config"
+	ini "github.com/communitybridge/easycla/cla-backend-go/init"
+	token "github.com/communitybridge/easycla/cla-backend-go/token"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -19,7 +25,24 @@ var (
 
 	// build date
 	buildDate string
+
+	configFile = ""
 )
+
+func init() {
+	stage := viper.GetString("STAGE")
+
+	awsSession, err := ini.GetAWSSession()
+	if err != nil {
+		log.Panicf("Unable to load AWS session - Error: %v", err)
+	}
+
+	configFile, err := config.LoadConfig(configFile, awsSession, stage)
+	if err != nil {
+		log.Panicf("Unable to load config - Error: %v", err)
+	}
+	token.Init(configFile.Auth0Platform.ClientID, configFile.Auth0Platform.ClientSecret, configFile.Auth0Platform.URL, configFile.Auth0Platform.Audience)
+}
 
 func main() {
 	cmd.Version = version
