07187545362d9987ddeac864eb963bd1e0e84476
David Deal <dealako@gmail.com>
2020-10-28 14:08:53 -0700

    Added gravatar to approved image source list
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-project-console/edge/security-headers.js
+++ cla-frontend-project-console/edge/security-headers.js
@@ -65,6 +65,7 @@ function generateCSP(env, isDevServer) {
       'https://s3.amazonaws.com/cla-project-logo-prod/',
       'https://s3.amazonaws.com/lf-master-project-logos-prod/',
       'https://lf-master-project-logos-prod.s3.us-east-2.amazonaws.com/',
+      'https://s.gravatar.com/',
       'https://lh3.googleusercontent.com/',
       'https://platform-logos-myprofile-api-dev.s3.us-east-2.amazonaws.com/'
     ],
