eea2df70db53c1e5ea0996a3d555828ee9645db6
Amol Sontakke <amols@proximabiz.com>
2020-11-03 14:07:05 +0530

    [#2097] Added lfx footer in landing page and removed unwanted conde
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-landing-page/src/app/components/cla-footer/cla-footer.component.ts
+++ cla-landing-page/src/app/components/cla-footer/cla-footer.component.ts
@@ -1,26 +1,14 @@
 // Copyright The Linux Foundation and each contributor to CommunityBridge.
 // SPDX-License-Identifier: MIT
 
-import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-cla-footer',
+  selector: 'lfx-footer',
   templateUrl: './cla-footer.component.html',
   styleUrls: ['./cla-footer.component.scss']
 })
-export class ClaFooterComponent implements OnInit {
-
-  COMMUNITY_PEOPLE_GUIDE = 'https://communitybridge.org/guide/';
-  PRIVACY_POLICY = 'https://www.linuxfoundation.org/privacy/';
-  TRADEMARK_USEAGE = 'https://www.linuxfoundation.org/trademark-usage/';
-  ACCEPTABLE_USER_POLICY = environment.ACCEPTABLE_USER_POLICY;
-  SERVICE_SPECIFIC_TERM = environment.SERVICE_SPECIFIC_TERM
-  PLATEFORM_USER_AGREEMENT = environment.PLATEFORM_USER_AGREEMENT;
-
+export class ClaFooterComponent {
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
--- cla-landing-page/src/environments/environment.ts
+++ cla-landing-page/src/environments/environment.ts
@@ -6,11 +6,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  LFX_HEADER_URL: 'https://cdn.dev.platform.linuxfoundation.org/lfx-header.js',
-  ACCEPTABLE_USER_POLICY: 'https://communitybridge.dev.platform.linuxfoundation.org/acceptable-use/',
-  SERVICE_SPECIFIC_TERM: 'https://communitybridge.dev.platform.linuxfoundation.org/service-terms/',
-  PLATEFORM_USER_AGREEMENT: 'https://communitybridge.dev.platform.linuxfoundation.org/platform-use-agreement/'
+  production: false
 }
 /*
  * For easier debugging in development mode, you can import the following file
