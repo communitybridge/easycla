d91e47af70fef49bd0c962685e5cbaf04312e46b
David Deal <dealako@gmail.com>
2020-11-05 11:36:55 -0800

    Updated List CLA Groups Under Foundation Response
    - Return 200 empty list when no mappins are found
    - Resolved Project Service API update issue (auth update)
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/repositories/mock/mock_service.go
+++ cla-backend-go/repositories/mock/mock_service.go
@@ -10,9 +10,10 @@ package mock
 
 import (
 	context "context"
+	reflect "reflect"
+
 	models "github.com/communitybridge/easycla/cla-backend-go/gen/models"
 	gomock "github.com/golang/mock/gomock"
-	reflect "reflect"
 )
 
 // MockService is a mock of Service interface
