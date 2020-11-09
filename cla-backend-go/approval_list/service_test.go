98f2f736bb32ebe3c04676378a7440f8bbc9b6e4
wanyaland <wanyaland@gmail.com>
2020-11-09 15:35:11 +0300

    [#2162]Feature/Approval Email
    - Changed email content for approval list requests (Accept/Deny)
    
    Signed-off-by: wanyaland <wanyaland@gmail.com>

--- cla-backend-go/approval_list/service_test.go
+++ cla-backend-go/approval_list/service_test.go
@@ -18,9 +18,9 @@ func TestRequestApprovedEmailToRecipientContent(t *testing.T) {
 		"john",
 		"john@john.com")
 
-	assert.Equal(t, "EasyCLA: Company Manager Access Approved for gardenerLtd", subject)
+	assert.Equal(t, "EasyCLA: Approved List Request Accepted for gardenerLtd", subject)
 	assert.Equal(t, []string{"john@john.com"}, recipients)
 	assert.Contains(t, body, "Hello john,")
 	assert.Contains(t, body, "This is a notification email from EasyCLA regarding the company gardenerLtd")
-	assert.Contains(t, body, "You have now been approved as a Company Manager for gardenerLtd")
+	assert.Contains(t, body, "You have now been added to the approval list for gardenerLtd")
 }
