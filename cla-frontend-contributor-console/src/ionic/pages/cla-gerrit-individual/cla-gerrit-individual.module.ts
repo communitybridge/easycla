e5ac0634f2e3f0c1b43e1e54ca364140b3724d91
Amol Sontakke <amols@proximabiz.com>
2020-11-12 17:27:55 +0530

    [1468] Removed Gerritid and it's related method
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- cla-frontend-contributor-console/src/ionic/pages/cla-gerrit-individual/cla-gerrit-individual.module.ts
+++ cla-frontend-contributor-console/src/ionic/pages/cla-gerrit-individual/cla-gerrit-individual.module.ts
@@ -3,12 +3,14 @@
 
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { LoadingSpinnerComponentModule } from '../../components/loading-spinner/loading-spinner.module';
 import { LayoutModule } from '../../layout/layout.module';
 import { ClaGerritIndividualPage } from './cla-gerrit-individual';
 
 @NgModule({
   declarations: [ClaGerritIndividualPage],
-  imports: [IonicPageModule.forChild(ClaGerritIndividualPage), LayoutModule],
+  imports: [IonicPageModule.forChild(ClaGerritIndividualPage),
+    LayoutModule, LoadingSpinnerComponentModule],
   entryComponents: [ClaGerritIndividualPage]
 })
 export class ClaGerritIndividualPageModule { }
