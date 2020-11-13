076d1a6b9f52dc70fa7812b1d91243c3334a8ee6
David Deal <dealako@gmail.com>
2020-11-12 16:07:59 -0800

    GitHub Organization Response Model Returns All Repos
    - Updated logic to return all public repos for each organization
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/github/github_repository.go
+++ cla-backend-go/github/github_repository.go
@@ -6,6 +6,11 @@ package github
 import (
 	"context"
 	"errors"
+	"fmt"
+
+	"github.com/communitybridge/easycla/cla-backend-go/utils"
+	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/communitybridge/easycla/cla-backend-go/logging"
 	"github.com/google/go-github/v32/github"
@@ -31,4 +36,42 @@ func GetRepositoryByExternalID(ctx context.Context, installationID, id int64) (*
 		return nil, err
 	}
 	return org, nil
+}
+
+// GetRepositories gets github repositories by organization
+func GetRepositories(ctx context.Context, organizationName string) ([]*github.Repository, error) {
+	f := logrus.Fields{
+		"functionName":     "GetRepositories",
+		utils.XREQUESTID:   ctx.Value(utils.XREQUESTID),
+		"organizationName": organizationName,
+	}
+
+	// Get the client with token
+	client := NewGithubOauthClient()
+
+	// API https://docs.github.com/en/free-pro-team@latest/rest/reference/repos
+	repoList, resp, err := client.Repositories.ListByOrg(ctx, organizationName, &github.RepositoryListByOrgOptions{
+		Type:      "public",
+		Sort:      "full_name",
+		Direction: "asc",
+		ListOptions: github.ListOptions{
+			Page:    0,
+			PerPage: 100,
+		},
+	})
+	if err != nil {
+		log.WithFields(f).WithError(err).Warn("unable to list repositories for organization")
+		if resp != nil && resp.StatusCode == 404 {
+			return nil, ErrGithubOrganizationNotFound
+		}
+		return nil, err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		msg := fmt.Sprintf("GetRepositories %s failed with no success response code %d. error = %s", organizationName, resp.StatusCode, err.Error())
+		log.WithFields(f).Warnf(msg)
+		return nil, errors.New(msg)
+	}
+
+	return repoList, nil
 }
