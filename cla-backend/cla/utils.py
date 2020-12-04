4448fb38c08d5568661c4eef033c22b8fae5e08e
David Deal <dealako@gmail.com>
2020-12-04 09:11:37 -0800

    Resolved email typo in footer (#2272)
    - resolve email formatting, removed semi-colon
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend/cla/utils.py
+++ cla-backend/cla/utils.py
@@ -1445,7 +1445,7 @@ def get_email_help_content(show_v2_help_link: bool) -> str:
         # v2 help link
         help_link = 'https://docs.linuxfoundation.org/lfx/v/v2/communitybridge/easycla'
 
-    return f'<p>If you need help or have questions about EasyCLA, you can <a href="{help_link}" target="_blank">read the documentation</a> or <a href="https://jira.linuxfoundation.org/servicedesk/customer/portal/4/create/143" target="_blank">reach out; to us for support</a>.</p>'
+    return f'<p>If you need help or have questions about EasyCLA, you can <a href="{help_link}" target="_blank">read the documentation</a> or <a href="https://jira.linuxfoundation.org/servicedesk/customer/portal/4/create/143" target="_blank">reach out to us for support</a>.</p>'
 
 
 def get_email_sign_off_content() -> str:
