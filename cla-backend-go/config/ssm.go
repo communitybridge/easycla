6d9e21f93a6ec7a944f3d808c4ba238151b13d33
Harold Wanyama <wanyaland@gmail.com>
2021-01-13 18:56:16 +0300

    [#2443] Bug/Sign Coporate signature (#2445)
    - Resolved issue raised by empty config key for v1 api base url
    
    Signed-off-by: wanyaland <wanyaland@gmail.com>

--- cla-backend-go/config/ssm.go
+++ cla-backend-go/config/ssm.go
@@ -178,7 +178,7 @@ func loadSSMConfig(awsSession *session.Session, stage string) Config { //nolint
 			config.LFGroup.ClientURL = resp.value
 		case fmt.Sprintf("cla-lf-group-refresh-token-%s", stage):
 			config.LFGroup.RefreshToken = resp.value
-		case fmt.Sprintf("cla-api-base-%s", stage):
+		case fmt.Sprintf("cla-v1-api-url-%s", stage):
 			config.ClaV1ApiURL = resp.value
 		case fmt.Sprintf("cla-acs-api-key-%s", stage):
 			config.AcsAPIKey = resp.value
