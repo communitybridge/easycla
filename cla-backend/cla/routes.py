562347ccc0a6992f43d76447028ae4ae395a4c4c
David Deal <dealako@gmail.com>
2021-01-20 16:29:34 -0500

    Resolved Python Function Parameters Issue (#2488)
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend/cla/routes.py
+++ cla-backend/cla/routes.py
@@ -1268,8 +1268,8 @@ def request_corporate_signature(
         company_id=company_id,
         signing_entity_name=signing_entity_name,
         send_as_email=send_as_email,
-        signatory_name=authority_name,
-        signatory_email=authority_email,
+        authority_name=authority_name,
+        authority_email=authority_email,
         return_url_type=return_url_type,
         return_url=return_url,
     )
