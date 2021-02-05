e09ce46ac748aac9ace35931296af55eb4f0fe4a
David Deal <dealako@gmail.com>
2021-02-05 08:42:37 -0800

    Resolved #2576 Updated V1 Email Regex - Allow 12 characters in domain suffix
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-frontend-contributor-console/src/ionic/validators/email.ts
+++ cla-frontend-contributor-console/src/ionic/validators/email.ts
@@ -5,7 +5,7 @@ import { FormControl } from '@angular/forms';
 
 export class EmailValidator {
   static isValid(control: FormControl): any {
-    const EMAIL_PATTERN = new RegExp(/^([a-zA-Z0-9_\-\.\+]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
+    const EMAIL_PATTERN = new RegExp(/^([a-zA-Z0-9_\-\.\+]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,12})$/);
     let email = control.value;
     let isValid = EMAIL_PATTERN.test(email);
     if (!isValid) {
