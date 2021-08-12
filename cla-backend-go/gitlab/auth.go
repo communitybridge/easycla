368bb503e0809979dd7c8c02720cf3df40a45635
David Deal <dealako@gmail.com>
2021-08-12 12:46:13 -0700

    Added GitLab Auth Debug (#3146)

--- cla-backend-go/gitlab/auth.go
+++ cla-backend-go/gitlab/auth.go
@@ -16,11 +16,14 @@ import (
 // FetchOauthCredentials is responsible for fetching the credentials from gitlab for alredy started Oauth process (access_token, refresh_token)
 func FetchOauthCredentials(code string) (*OauthSuccessResponse, error) {
 	f := logrus.Fields{
-		"functionName": "gitlab.auth.FetchOauthCredentials",
-		"code":         code,
-		"redirectURI":  config.GetConfig().Gitlab.RedirectURI,
+		"functionName":       "gitlab.auth.FetchOauthCredentials",
+		"code":               code,
+		"redirectURI":        config.GetConfig().Gitlab.RedirectURI,
+		"gitLabClientID":     fmt.Sprintf("%s...", config.GetConfig().Gitlab.AppID[0:6]),
+		"gitLabClientSecret": fmt.Sprintf("%s...", config.GetConfig().Gitlab.ClientSecret[0:6]),
 	}
 
+	// For info on this authorization flow, see: https://docs.gitlab.com/ee/api/oauth2.html#authorization-code-flow
 	client := resty.New()
 	params := map[string]string{
 		"client_id":     config.GetConfig().Gitlab.AppID,
@@ -42,7 +45,7 @@ func FetchOauthCredentials(code string) (*OauthSuccessResponse, error) {
 	}
 
 	if resp.StatusCode() < 200 || resp.StatusCode() > 299 {
-		msg := fmt.Sprintf("problem invoking GitLab auth token exchange to: %s with status code: %d", url, resp.StatusCode())
+		msg := fmt.Sprintf("problem invoking GitLab auth token exchange to: %s with status code: %d, response: %s", url, resp.StatusCode(), string(resp.Body()))
 		log.WithFields(f).Warn(msg)
 		return nil, errors.New(msg)
 	}
