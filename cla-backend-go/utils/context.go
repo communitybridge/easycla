482e3eceae0d8fb19fa0af5b42c7441261115f46
David Deal <dealako@gmail.com>
2021-08-18 18:58:11 -0700

    Added Context Helper Functions (#3170)
    - Added helper functions for adding authUser to the context and fetching the values
    
    Signed-off-by: David Deal <dealako@gmail.com>

--- cla-backend-go/utils/context.go
+++ cla-backend-go/utils/context.go
@@ -6,6 +6,8 @@ package utils
 import (
 	"context"
 
+	"github.com/LF-Engineering/lfx-kit/auth"
+
 	"github.com/sirupsen/logrus"
 
 	log "github.com/communitybridge/easycla/cla-backend-go/logging"
@@ -24,4 +26,54 @@ func NewContext() context.Context {
 	}
 
 	return context.WithValue(context.Background(), XREQUESTID, requestID.String()) // nolint
+}
+
+// NewContextWithUser returns a new context with a newly generated request ID and the specified user
+func NewContextWithUser(authUser *auth.User) context.Context {
+	f := logrus.Fields{
+		"functionName": "utils.NewContextWithUser",
+	}
+	requestID, err := uuid.NewV4()
+	if err != nil {
+		log.WithFields(f).WithError(err).Warn("unable to generate a UUID for x-request-id")
+		return context.Background()
+	}
+
+	return context.WithValue(context.WithValue(context.Background(), XREQUESTID, requestID), "authUser", authUser) // nolint
+}
+
+// ContextWithRequestAndUser returns a new context with the specified request ID and user
+func ContextWithRequestAndUser(ctx context.Context, reqID string, authUser *auth.User) context.Context {
+	return context.WithValue(context.WithValue(ctx, XREQUESTID, reqID), "authUser", authUser) // nolint
+}
+
+// ContextWithUser returns a new context with the specified user
+func ContextWithUser(ctx context.Context, authUser *auth.User) context.Context {
+	return context.WithValue(ctx, "authUser", authUser) // nolint
+}
+
+// GetUserNameFromContext returns the user's name from the context
+func GetUserNameFromContext(ctx context.Context) string {
+	val := ctx.Value(CtxAuthUser)
+	if val != nil {
+		authUser := val.(*auth.User) // nolint
+		if authUser != nil {
+			return authUser.UserName
+		}
+	}
+
+	return ""
+}
+
+// GetUserEmailFromContext returns the user's email from the context
+func GetUserEmailFromContext(ctx context.Context) string {
+	val := ctx.Value(CtxAuthUser)
+	if val != nil {
+		authUser := val.(*auth.User) // nolint
+		if authUser != nil {
+			return authUser.Email
+		}
+	}
+
+	return ""
 }
