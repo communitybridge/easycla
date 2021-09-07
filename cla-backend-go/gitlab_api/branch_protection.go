742c4f4e79315192aa8a79583ff6dbd7ba928dce
Denis Kyorov <makkalot@gmail.com>
2021-09-07 18:36:55 +0300

    gitlab branch protection dynamodb event handlers (#3246)

---
+++ cla-backend-go/gitlab_api/branch_protection.go
@@ -0,0 +1,120 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
+package gitlab
+
+import (
+	"context"
+	"fmt"
+
+	log "github.com/communitybridge/easycla/cla-backend-go/logging"
+	"github.com/communitybridge/easycla/cla-backend-go/utils"
+	"github.com/sirupsen/logrus"
+	"github.com/xanzy/go-gitlab"
+)
+
+// SetOrCreateBranchProtection sets the required parameters if existing pattern exists or creates a new one
+func SetOrCreateBranchProtection(ctx context.Context, client *gitlab.Client, projectID int, protectionPattern string) error {
+	var err error
+	f := logrus.Fields{
+		"functionName":      "gitlab_api.SetOrCreateBranchProtection",
+		utils.XREQUESTID:    ctx.Value(utils.XREQUESTID),
+		"gitlabProjectID":   projectID,
+		"protectionPattern": protectionPattern,
+	}
+
+	log.WithFields(f).Debugf("setting branch protection...")
+
+	protectedBranch, resp, err := client.ProtectedBranches.GetProtectedBranch(projectID, protectionPattern)
+	if err != nil && resp.StatusCode != 404 {
+		return fmt.Errorf("fetching existing branch failed : %v", err)
+	}
+
+	if protectedBranch != nil {
+		if isProtectedBranchSet(protectedBranch) {
+			log.WithFields(f).Debugf("branch protection already set nothing to do")
+			return nil
+		}
+
+		//it's an existing one try to remove it first and re-create it
+		log.WithFields(f).Debugf("removing old branch protection")
+		_, err = client.ProtectedBranches.UnprotectRepositoryBranches(projectID, protectionPattern)
+		if err != nil {
+			return fmt.Errorf("removing protection for existing branch failed : %v", err)
+		}
+	}
+
+	log.WithFields(f).Debugf("re-creating branch protection ")
+	if _, err = createBranchProtection(client, projectID, protectionPattern); err != nil {
+		return fmt.Errorf("recreating branch protection failed : %v", err)
+	}
+	return nil
+}
+
+func createBranchProtection(client *gitlab.Client, projectID int, name string) (*gitlab.ProtectedBranch, error) {
+	protectedBranch, _, err := client.ProtectedBranches.ProtectRepositoryBranches(projectID, &gitlab.ProtectRepositoryBranchesOptions{
+		Name:                      gitlab.String(name),
+		PushAccessLevel:           gitlab.AccessLevel(gitlab.NoPermissions),
+		MergeAccessLevel:          gitlab.AccessLevel(gitlab.MaintainerPermissions),
+		UnprotectAccessLevel:      nil,
+		AllowForcePush:            gitlab.Bool(false),
+		AllowedToPush:             nil,
+		AllowedToMerge:            nil,
+		AllowedToUnprotect:        nil,
+		CodeOwnerApprovalRequired: nil,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("creating new branch protection failed : %v", err)
+	}
+	return protectedBranch, nil
+}
+
+func isProtectedBranchSet(protectedBranch *gitlab.ProtectedBranch) bool {
+	if protectedBranch.AllowForcePush {
+		return false
+	}
+
+	if len(protectedBranch.PushAccessLevels) != 1 {
+		return false
+	}
+
+	if protectedBranch.PushAccessLevels[0].AccessLevel != gitlab.NoPermissions {
+		return false
+	}
+
+	if len(protectedBranch.MergeAccessLevels) != 1 {
+		return false
+	}
+
+	if protectedBranch.MergeAccessLevels[0].AccessLevel != gitlab.MaintainerPermissions {
+		return false
+	}
+
+	if len(protectedBranch.UnprotectAccessLevels) != 1 {
+		return false
+	}
+
+	if protectedBranch.UnprotectAccessLevels[0].AccessLevel != gitlab.MaintainerPermissions {
+		return false
+	}
+
+	return true
+}
+
+// GetDefaultBranch finds the default branch for the given project
+func GetDefaultBranch(client *gitlab.Client, projectID int) (*gitlab.Branch, error) {
+	project, _, err := client.Projects.GetProject(projectID, &gitlab.GetProjectOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("fetching project failed : %v", err)
+	}
+
+	defaultBranch := project.DefaultBranch
+
+	//	first try with the possible option
+	branch, _, err := client.Branches.GetBranch(projectID, defaultBranch)
+	if err != nil {
+		return nil, fmt.Errorf("fetching default branch failed : %v", err)
+	}
+
+	return branch, nil
+}
---
+++ cla-backend-go/v2/dynamo_events/gitlab_branch_protection.go
@@ -0,0 +1,140 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
+package dynamo_events
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	gitlab_api "github.com/communitybridge/easycla/cla-backend-go/gitlab_api"
+	log "github.com/communitybridge/easycla/cla-backend-go/logging"
+	"github.com/communitybridge/easycla/cla-backend-go/utils"
+	"github.com/communitybridge/easycla/cla-backend-go/v2/common"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+)
+
+// GitLabOrgUpdatedEvent handles branch protection functionality
+func (s *service) GitLabOrgUpdatedEvent(event events.DynamoDBEventRecord) error {
+	ctx := utils.NewContext()
+	f := logrus.Fields{
+		"functionName":   "dynamodb_events.gitlab_organization.GitLabOrgUpdatedEvent",
+		utils.XREQUESTID: ctx.Value(utils.XREQUESTID),
+		"eventName":      event.EventName,
+		"eventSource":    event.EventSource,
+		"eventID":        event.EventID,
+	}
+
+	log.WithFields(f).Debug("processing event")
+	var newGitLabOrg, oldGitLabOrg common.GitLabOrganization
+	err := unmarshalStreamImage(event.Change.NewImage, &newGitLabOrg)
+	if err != nil {
+		log.WithFields(f).Warnf("problem unmarshalling the new gitlab organization model from the updated event, error: %+v", err)
+		return err
+	}
+	err = unmarshalStreamImage(event.Change.OldImage, &oldGitLabOrg)
+	if err != nil {
+		log.WithFields(f).Warnf("problem unmarshalling the old gitlab organization model from the updated event, error: %+v", err)
+		return err
+	}
+
+	f["gitlabOrgID"] = newGitLabOrg.OrganizationID
+	f["gitlabOrgName"] = newGitLabOrg.OrganizationName
+
+	if !newGitLabOrg.Enabled {
+		log.WithFields(f).Debugf("gitlab org is not enabled, nothing to do this time")
+		return nil
+	}
+
+	// If the branch protection value was updated from false to true....
+	if !oldGitLabOrg.BranchProtectionEnabled && newGitLabOrg.BranchProtectionEnabled {
+		log.WithFields(f).Debug("transition of branchProtectionEnabled false => true - processing...")
+		return s.enableBranchProtectionForGitLabOrg(ctx, newGitLabOrg)
+	}
+
+	// it might be a new gitlab org that was just authenticated
+	if oldGitLabOrg.AuthInfo != newGitLabOrg.AuthInfo && newGitLabOrg.BranchProtectionEnabled {
+		log.WithFields(f).Debug("auth info was set for the org, processing the branch protection")
+		return s.enableBranchProtectionForGitLabOrg(ctx, newGitLabOrg)
+	}
+
+	log.WithFields(f).Debug("no transition of branchProtectionEnabled false => true - ignoring...")
+	return nil
+}
+
+func (s *service) enableBranchProtectionForGitLabOrg(ctx context.Context, newGitLabOrg common.GitLabOrganization) error {
+	f := logrus.Fields{
+		"functionName":            "dynamo_events.gitlab_organization.enableBranchProtectionForGitLabOrg",
+		utils.XREQUESTID:          ctx.Value(utils.XREQUESTID),
+		"projectSFID":             newGitLabOrg.ProjectSFID,
+		"organizationName":        newGitLabOrg.OrganizationName,
+		"organizationSFID":        newGitLabOrg.OrganizationSFID,
+		"autoEnabled":             newGitLabOrg.AutoEnabled,
+		"branchProtectionEnabled": newGitLabOrg.BranchProtectionEnabled,
+	}
+
+	gitlabOrg, err := s.gitLabOrgRepo.GetGitLabOrganizationByName(ctx, newGitLabOrg.OrganizationName)
+	if err != nil {
+		return fmt.Errorf("fetching gitlab org : %s failed : %v", newGitLabOrg.OrganizationName, err)
+	}
+
+	log.WithFields(f).Debugf("creating a new gitlab client object for org: %s...", newGitLabOrg.OrganizationName)
+	gitLabClient, err := gitlab_api.NewGitlabOauthClient(gitlabOrg.AuthInfo, s.gitLabApp)
+	if err != nil {
+		return fmt.Errorf("initializing GitLab client failed : %v", err)
+	}
+
+	// Locate the repositories already saved under this organization
+	log.WithFields(f).Debugf("loading repositories under the organization : %s", newGitLabOrg.OrganizationName)
+	repos, err := s.v2Repository.GitLabGetRepositoriesByOrganizationName(context.Background(), newGitLabOrg.OrganizationName)
+	if err != nil {
+		log.WithFields(f).Warnf("problem locating repositories by organization name, error: %+v", err)
+		return err
+	}
+
+	var eg errgroup.Group
+	// a pool of 5 concurrent workers
+	var workerTokens = make(chan struct{}, 5)
+	for _, repo := range repos {
+		// this is for goroutine local variables
+		repo := repo
+		// acquire a worker token to create a new goroutine
+		workerTokens <- struct{}{}
+		// Update the branch protection in a go routine...
+		eg.Go(func() error {
+			defer func() {
+				<-workerTokens // release the workerToken
+			}()
+			log.WithFields(f).Debugf("enabling branch protection for repository: %s", repo.RepositoryName)
+
+			repositoryExternalIDInt, err := strconv.Atoi(repo.RepositoryExternalID)
+			if err != nil {
+				return fmt.Errorf("parsing external repository id failed : %v", err)
+			}
+
+			gitlabDefaultBranch, err := gitlab_api.GetDefaultBranch(gitLabClient, repositoryExternalIDInt)
+			if err != nil {
+				return fmt.Errorf("fetching default branch failed : %v", err)
+			}
+
+			err = gitlab_api.SetOrCreateBranchProtection(ctx, gitLabClient, repositoryExternalIDInt, gitlabDefaultBranch.Name)
+			if err != nil {
+				return fmt.Errorf("enabling branch protection for pattern : %s, failed : %v", gitlabDefaultBranch.Name, err)
+			}
+			return nil
+		})
+	}
+
+	// Wait for the go routines to finish
+	log.WithFields(f).Debugf("waiting for %d repositories to complete...", len(repos))
+	var branchProtectionErr error
+	if loadErr := eg.Wait(); loadErr != nil {
+		log.WithFields(f).Warnf("encountered branch protection setup error: %+v", loadErr)
+		branchProtectionErr = loadErr
+	}
+
+	return branchProtectionErr
+}
