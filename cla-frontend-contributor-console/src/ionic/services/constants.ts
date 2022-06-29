ea64a5e11b816de28a1a8ece0769e7fc47c1a849
David Deal <ddeal@linuxfoundation.org>
2022-06-29 13:12:43 -0700

    Removed Old V1 Consoles
    - removed old v1 project console
    - removed old v1 corporate console
    - removed old v1 contributor console
    - updated CI/CD CircleCI configuration/deployment
    
    Signed-off-by: David Deal <ddeal@linuxfoundation.org>

--- cla-frontend-contributor-console/src/ionic/services/constants.ts
+++
@@ -1,6 +0,0 @@
-// Copyright The Linux Foundation and each contributor to CommunityBridge.
-// SPDX-License-Identifier: MIT
-
-export const CINCO_API_URL: string = 'https://cinco_api_endpoint';
-export const CLA_API_URL: string = 'https://cla_api_endpoint/dev-runze';
-export const ANALYTICS_API_URL: string = 'https://analytics_api_endpoint';
