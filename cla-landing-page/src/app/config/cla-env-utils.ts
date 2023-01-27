b62cfb7584c34b23333f546f5e921e2f468b9788
David Deal <ddeal@linuxfoundation.org>
2023-01-27 14:33:49 -0800

    CI/CD - Update CircleCI Base Images
    - removed landing page
    
    Signed-off-by: David Deal <ddeal@linuxfoundation.org>

--- cla-landing-page/src/app/config/cla-env-utils.ts
+++
@@ -1,5 +0,0 @@
-// Copyright The Linux Foundation and each contributor to CommunityBridge.
-// SPDX-License-Identifier: MIT
-
-import * as env from './cla-env-config.json';
-export const EnvConfig = env as any;
--- cla-landing-page/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-// Copyright The Linux Foundation and each contributor to CommunityBridge.
-// SPDX-License-Identifier: MIT
-
-export const environment = {
-  environment: 'prod',
-  lfxHeader: 'https://cdn.platform.linuxfoundation.org/lfx-header-v2-no-zone.js',
-  production: true
-};
