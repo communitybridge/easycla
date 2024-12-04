359e54b1d1fd5ddeb578dfc3d1f2addc8c73fcc6
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-12-04 07:17:09 +0000

    Example tests in comment
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -4,6 +4,7 @@
 if [ ! -z "$PY" ]
 then
   cd cla-backend && pytest "cla/tests" -p no:warnings
+  # pytest -vvv -s cla/tests/unit/test_docusign_models.py -p no:warnings -k test_request_individual_signature
   cd ..
 else
   echo "$0: skipping python backend tests, specify PY=1 to run them"
@@ -12,6 +13,7 @@ fi
 if [ ! -z "$GO" ]
 then
   cd cla-backend-go && make test
+  # go test github.com/communitybridge/easycla/cla-backend-go/signatures
   cd ..
 else
   echo "$0: skipping golang backend tests, specify GO=1 to run them"
