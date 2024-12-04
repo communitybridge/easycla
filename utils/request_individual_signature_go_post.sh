ab386de2a3dc6e4f22e4c5a9dba66b7a245f2cb4
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-12-04 06:33:28 +0000

    Updaste util script
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- utils/request_individual_signature_go_post.sh
+++ utils/request_individual_signature_go_post.sh
@@ -4,33 +4,58 @@
 # project_id='88ee12de-122b-4c46-9046-19422054ed8d'
 # return_url_type='github'
 # return_url='http://localhost'
-# DEBUG=1 ./utils/request_individual_signature_go_post.sh 9dcf5bbc-2492-11ed-97c7-3e2a23ea20b5 88ee12de-122b-4c46-9046-19422054ed8d github 'http://localhost'
+# TOKEN='...' - Auth0 JWT bearer token
+# XACL='...' - X-ACL header
+# DEBUG=1 TOKEN="$(cat ./auth0.token.secret)" XACL="$(cat ./x-acl.secret)" ./utils/request_individual_signature_go_post.sh 9dcf5bbc-2492-11ed-97c7-3e2a23ea20b5 88ee12de-122b-4c46-9046-19422054ed8d github 'http://localhost'
+
+if [ -z "$TOKEN" ]
+then
+  # source ./auth0_token.secret
+  TOKEN="$(cat ./auth0.token.secret)"
+fi
+
+if [ -z "$TOKEN" ]
+then
+  echo "$0: TOKEN not specified and unable to obtain one"
+  exit 1
+fi
+
+if [ -z "$XACL" ]
+then
+  XACL="$(cat ./x-acl.secret)"
+fi
+
+if [ -z "$XACL" ]
+then
+  echo "$0: XACL not specified and unable to obtain one"
+  exit 2
+fi
 
 if [ -z "$1" ]
 then
   echo "$0: you need to specify user_id as a 1st parameter"
-  exit 1
+  exit 3
 fi
 export user_id="$1"
 
 if [ -z "$2" ]
 then
   echo "$0: you need to specify project_id as a 2nd parameter"
-  exit 2
+  exit 4
 fi
 export project_id="$2"
 
 if [ -z "$3" ]
 then
   echo "$0: you need to specify return_url_type as a 3rd parameter: github|gitlab|gerrit"
-  exit 3
+  exit 5
 fi
 export return_url_type="$3"
 
 if [ -z "$4" ]
 then
   echo "$0: you need to specify return_url as a 4th parameter"
-  exit 4
+  exit 6
 fi
 export return_url="$4"
 
@@ -41,6 +66,6 @@ fi
 
 if [ ! -z "$DEBUG" ]
 then
-  echo "curl -s -XPOST -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' '${API_URL}/v4/request-individual-signature' -d '{\"project_id\":\"${project_id}\",\"user_id\":\"${user_id}\",\"return_url_type\":\"${return_url_type}\",\"return_url\":\"${return_url}\"}' | jq -r '.'"
+  echo "curl -s -XPOST -H 'X-ACL: ${XACL}' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' '${API_URL}/v4/request-individual-signature' -d '{\"project_id\":\"${project_id}\",\"user_id\":\"${user_id}\",\"return_url_type\":\"${return_url_type}\",\"return_url\":\"${return_url}\"}' | jq -r '.'"
 fi
-curl -s -XPOST -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" "${API_URL}/v4/request-individual-signature" -d "{\"project_id\":\"${project_id}\",\"user_id\":\"${user_id}\",\"return_url_type\":\"${return_url_type}\",\"return_url\":\"${return_url}\"}" | jq -r '.'
+curl -s -XPOST -H "X-ACL: ${XACL}" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" "${API_URL}/v4/request-individual-signature" -d "{\"project_id\":\"${project_id}\",\"user_id\":\"${user_id}\",\"return_url_type\":\"${return_url_type}\",\"return_url\":\"${return_url}\"}" | jq -r '.'
